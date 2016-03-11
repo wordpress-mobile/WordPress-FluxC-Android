f0cec20a32510dc5c09a7dda5780cd7b0ddf2119
Maxime Biais <maxime.biais@gmail.com>
2016-03-11 18:32:08 +0100

    update to okhttp 3.2.0 and setup OkHttpClient to use our custom Trust Manager

--- WordPressStores/src/main/java/org/wordpress/android/stores/module/ReleaseNetworkModule.java
+++ WordPressStores/src/main/java/org/wordpress/android/stores/module/ReleaseNetworkModule.java
@@ -4,11 +4,10 @@ import android.content.Context;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.OkUrlFactory;
 
 import org.wordpress.android.stores.Dispatcher;
 import org.wordpress.android.stores.network.HTTPAuthManager;
+import org.wordpress.android.stores.network.MemorizingTrustManager;
 import org.wordpress.android.stores.network.OkHttpStack;
 import org.wordpress.android.stores.network.UserAgent;
 import org.wordpress.android.stores.network.rest.wpcom.account.AccountRestClient;
@@ -18,18 +17,45 @@ import org.wordpress.android.stores.network.rest.wpcom.auth.Authenticator;
 import org.wordpress.android.stores.network.rest.wpcom.site.SiteRestClient;
 import org.wordpress.android.stores.network.xmlrpc.site.SiteXMLRPCClient;
 
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
 import javax.inject.Singleton;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
+import okhttp3.OkUrlFactory;
 
 @Module
 public class ReleaseNetworkModule {
+
     @Provides
-    public OkHttpClient provideOkHttpClient() {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        // FIXME: okHttpClient.setSslSocketFactory()
-        return new OkHttpClient();
+    public OkHttpClient provideOkHttpClient(MemorizingTrustManager memorizingTrustManager) {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        // TODO: We should provide a "normal" client (or with a pinned certificate trust manager) for wpcom requests
+        try {
+            final SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{memorizingTrustManager}, new java.security.SecureRandom());
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            builder.sslSocketFactory(sslSocketFactory);
+            builder.hostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+        } catch (NoSuchAlgorithmException e) {
+            // noop
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        return builder.build();
     }
 
     @Singleton
@@ -87,5 +113,11 @@ public class ReleaseNetworkModule {
     @Provides
     public HTTPAuthManager provideHTTPAuthManager() {
         return new HTTPAuthManager();
+    }
+
+    @Singleton
+    @Provides
+    public MemorizingTrustManager provideMemorizingTrustManager(Context appContext) {
+        return new MemorizingTrustManager(appContext);
     }
 }
