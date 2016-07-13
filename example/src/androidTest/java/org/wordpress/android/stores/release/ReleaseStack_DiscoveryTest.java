57aa3a4e3cda7a595a1b1bffd5482a2af1688d11
Alex Forcier <alex.forcier@automattic.com>
2016-07-13 09:36:15 -0400

    Updated tests with changes to endpoint discovery

--- example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_DiscoveryTest.java
+++ example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_DiscoveryTest.java
@@ -5,11 +5,10 @@ import com.squareup.otto.Subscribe;
 import org.wordpress.android.stores.Dispatcher;
 import org.wordpress.android.stores.TestUtils;
 import org.wordpress.android.stores.example.BuildConfig;
+import org.wordpress.android.stores.generated.AuthenticationActionBuilder;
 import org.wordpress.android.stores.generated.SiteActionBuilder;
-import org.wordpress.android.stores.network.AuthError;
 import org.wordpress.android.stores.network.HTTPAuthManager;
 import org.wordpress.android.stores.network.MemorizingTrustManager;
-import org.wordpress.android.stores.network.discovery.SelfHostedEndpointFinder;
 import org.wordpress.android.stores.network.discovery.SelfHostedEndpointFinder.DiscoveryError;
 import org.wordpress.android.stores.store.AccountStore;
 import org.wordpress.android.stores.store.AccountStore.OnAuthenticationChanged;
@@ -34,17 +33,21 @@ public class ReleaseStack_DiscoveryTest extends ReleaseStack_Base {
     @Inject AccountStore mAccountStore;
     @Inject HTTPAuthManager mHTTPAuthManager;
     @Inject MemorizingTrustManager mMemorizingTrustManager;
-    @Inject SelfHostedEndpointFinder mSelfHostedEndpointFinder;
 
+    RefreshSitesXMLRPCPayload mPayload;
     CountDownLatch mCountDownLatch;
-
-    String mLastEndpoint;
 
     enum TEST_EVENTS {
         NONE,
+        DISCOVERY_SUCCEEDED,
+        NO_SITE_ERROR,
+        WORDPRESS_COM_SITE,
+        ERRONEOUS_SSL_CERTIFICATE,
+        HTTP_AUTH_REQUIRED,
         SITE_CHANGED,
         SITE_REMOVED
     }
+
     private TEST_EVENTS mNextEvent;
 
     @Override
@@ -58,74 +61,46 @@ public class ReleaseStack_DiscoveryTest extends ReleaseStack_Base {
     }
 
     public void testInvalidUrlFetchSites() throws InterruptedException {
-        final RefreshSitesXMLRPCPayload payload = new RefreshSitesXMLRPCPayload();
-        payload.username = BuildConfig.TEST_WPORG_USERNAME_SH_SIMPLE;
-        payload.password = BuildConfig.TEST_WPORG_PASSWORD_SH_SIMPLE;
+        mPayload = new RefreshSitesXMLRPCPayload();
+        mPayload.xmlrpcEndpoint = "notaurl&*@";
+        mPayload.username = BuildConfig.TEST_WPORG_USERNAME_SH_SIMPLE;
+        mPayload.password = BuildConfig.TEST_WPORG_PASSWORD_SH_SIMPLE;
 
+        mNextEvent = TEST_EVENTS.NO_SITE_ERROR;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint("notaurl&*@", payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        assertEquals(DiscoveryError.NO_SITE_ERROR, error);
-                        mCountDownLatch.countDown();
-                    }
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        throw new AssertionError("Expected failure due to invalid URL error but discovery succeeded");
-                    }
-                });
         // Wait for a network response / onChanged event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 
     public void testNonWordPressFetchSites() throws InterruptedException {
-        final RefreshSitesXMLRPCPayload payload = new RefreshSitesXMLRPCPayload();
-        payload.username = BuildConfig.TEST_WPORG_USERNAME_SH_SIMPLE;
-        payload.password = BuildConfig.TEST_WPORG_PASSWORD_SH_SIMPLE;
+        mPayload = new RefreshSitesXMLRPCPayload();
+        mPayload.xmlrpcEndpoint = "example.com";
+        mPayload.username = BuildConfig.TEST_WPORG_USERNAME_SH_SIMPLE;
+        mPayload.password = BuildConfig.TEST_WPORG_PASSWORD_SH_SIMPLE;
 
+        mNextEvent = TEST_EVENTS.NO_SITE_ERROR;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint("example.com", payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        assertEquals(DiscoveryError.NO_SITE_ERROR, error);
-                        mCountDownLatch.countDown();
-                    }
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        throw new AssertionError("Expected failure due to invalid URL error but discovery succeeded");
-                    }
-                });
         // Wait for a network response / onChanged event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 
     public void testWordPressCOMUrlFetchSites() throws InterruptedException {
-        final RefreshSitesXMLRPCPayload payload = new RefreshSitesXMLRPCPayload();
-        payload.username = BuildConfig.TEST_WPCOM_USERNAME_TEST1;
-        payload.password = BuildConfig.TEST_WPCOM_PASSWORD_TEST1;
+        mPayload = new RefreshSitesXMLRPCPayload();
+        mPayload.xmlrpcEndpoint = "mysite.wordpress.com";
+        mPayload.username = BuildConfig.TEST_WPCOM_USERNAME_TEST1;
+        mPayload.password = BuildConfig.TEST_WPCOM_PASSWORD_TEST1;
 
+        mNextEvent = TEST_EVENTS.WORDPRESS_COM_SITE;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint("mysite.wordpress.com", payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        assertEquals(DiscoveryError.WORDPRESS_COM_SITE, error);
-                        mCountDownLatch.countDown();
-                    }
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        throw new AssertionError(
-                                "Expected failure due to WORDPRESS_COM_SITE error but discovery succeeded");
-                    }
-                });
         // Wait for a network response / onChanged event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
@@ -238,131 +213,90 @@ public class ReleaseStack_DiscoveryTest extends ReleaseStack_Base {
 
     private void checkSelfHostedSimpleFetchForSite(String url, String username, String password)
             throws InterruptedException {
-        final RefreshSitesXMLRPCPayload payload = new RefreshSitesXMLRPCPayload();
-        payload.username = username;
-        payload.password = password;
+        mPayload = new RefreshSitesXMLRPCPayload();
+        mPayload.xmlrpcEndpoint = url;
+        mPayload.username = username;
+        mPayload.password = password;
 
-        mNextEvent = TEST_EVENTS.SITE_CHANGED;
+        mNextEvent = TEST_EVENTS.DISCOVERY_SUCCEEDED;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint(url, payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        throw new AssertionError("Expected successful discovery but got error: " + error);
-                    }
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        payload.xmlrpcEndpoint = xmlrpcEndpoint;
-                        mDispatcher.dispatch(SiteActionBuilder.newFetchSitesXmlRpcAction(payload));
-                    }
-                });
         // Wait for a network response / onChanged event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        fetchSites();
     }
 
     private void checkSelfHostedSelfSignedSSLFetchForSite(String url, String username, String password)
             throws InterruptedException {
-        final RefreshSitesXMLRPCPayload payload = new RefreshSitesXMLRPCPayload();
-        payload.username = username;
-        payload.password = password;
+        mPayload = new RefreshSitesXMLRPCPayload();
+        mPayload.xmlrpcEndpoint = url;
+        mPayload.username = username;
+        mPayload.password = password;
 
+        mNextEvent = TEST_EVENTS.ERRONEOUS_SSL_CERTIFICATE;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint(url, payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        assertEquals(DiscoveryError.ERRONEOUS_SSL_CERTIFICATE, error);
-                        mLastEndpoint = lastEndpoint;
-                        mCountDownLatch.countDown();
-                    }
-
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        throw new AssertionError("Expected failure due to SSL error but discovery succeeded");
-                    }
-                });
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
         // Wait for a network response / onAuthenticationChanged error event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
         // Add an exception for the last certificate
         mMemorizingTrustManager.storeLastFailure();
 
         // Retry endpoint discovery, and attempt to fetch sites
-        mNextEvent = TEST_EVENTS.SITE_CHANGED;
+        mNextEvent = TEST_EVENTS.DISCOVERY_SUCCEEDED;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint(mLastEndpoint,
-                payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        throw new AssertionError("Expected successful discovery but got error: " + error);
-                    }
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        payload.xmlrpcEndpoint = xmlrpcEndpoint;
-                        mDispatcher.dispatch(SiteActionBuilder.newFetchSitesXmlRpcAction(payload));
-                    }
-                });
-
-        // Wait for a network response / onChanged event
+        // Wait for a network response / onAuthenticationChanged error event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        fetchSites();
 
         mMemorizingTrustManager.clearLocalTrustStore();
     }
 
     private void checkSelfHostedHTTPAuthFetchForSite(String url, String username, String password, String auth_username,
                                                     String auth_password) throws InterruptedException {
-        final RefreshSitesXMLRPCPayload payload = new RefreshSitesXMLRPCPayload();
-        payload.username = username;
-        payload.password = password;
+        mPayload = new RefreshSitesXMLRPCPayload();
+        mPayload.xmlrpcEndpoint = url;
+        mPayload.username = username;
+        mPayload.password = password;
 
+        mNextEvent = TEST_EVENTS.HTTP_AUTH_REQUIRED;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint(url, payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        assertEquals(DiscoveryError.HTTP_AUTH_REQUIRED, error);
-                        mLastEndpoint = lastEndpoint;
-                        mCountDownLatch.countDown();
-                    }
-
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        throw new AssertionError("Expected failure due to HTTP AUTH error but discovery succeeded");
-                    }
-                });
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
 
         // Wait for a network response / onAuthenticationChanged error event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
 
         // Set known HTTP Auth credentials
-        mHTTPAuthManager.addHTTPAuthCredentials(auth_username, auth_password, mLastEndpoint, null);
+        mHTTPAuthManager.addHTTPAuthCredentials(auth_username, auth_password, mPayload.xmlrpcEndpoint, null);
 
         // Retry endpoint discovery, and attempt to fetch sites
+        mNextEvent = TEST_EVENTS.DISCOVERY_SUCCEEDED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        mDispatcher.dispatch(AuthenticationActionBuilder.newDiscoverEndpointAction(mPayload));
+
+        // Wait for a network response / onChanged event
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        fetchSites();
+    }
+
+    private void fetchSites() throws InterruptedException {
         mNextEvent = TEST_EVENTS.SITE_CHANGED;
         mCountDownLatch = new CountDownLatch(1);
 
-        mSelfHostedEndpointFinder.findEndpoint(mLastEndpoint, payload.username, payload.password,
-                new SelfHostedEndpointFinder.DiscoveryCallback() {
-                    @Override
-                    public void onError(DiscoveryError error, String lastEndpoint) {
-                        throw new AssertionError("Expected successful discovery but got error: " + error);
-                    }
+        mDispatcher.dispatch(SiteActionBuilder.newFetchSitesXmlRpcAction(mPayload));
 
-                    @Override
-                    public void onSuccess(String xmlrpcEndpoint, String restEndpoint) {
-                        payload.xmlrpcEndpoint = xmlrpcEndpoint;
-                        mDispatcher.dispatch(SiteActionBuilder.newFetchSitesXmlRpcAction(payload));
-                    }
-                });
-
-        // Wait for a network response / onChanged event
         assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 
@@ -391,5 +325,31 @@ public class ReleaseStack_DiscoveryTest extends ReleaseStack_Base {
     @Subscribe
     public void onAuthenticationChanged(OnAuthenticationChanged event) {
         throw new AssertionError("OnAuthenticationChanged called - that's not supposed to happen for discovery");
+    }
+
+    @Subscribe
+    public void onDiscoveryCompleted(AccountStore.OnDiscoveryCompleted event) {
+        if (event.isError) {
+            AppLog.i(T.API, "Discovery error: " + event.error);
+            if (event.error == DiscoveryError.NO_SITE_ERROR) {
+                assertEquals(TEST_EVENTS.NO_SITE_ERROR, mNextEvent);
+            } else if (event.error == DiscoveryError.WORDPRESS_COM_SITE) {
+                assertEquals(TEST_EVENTS.WORDPRESS_COM_SITE, mNextEvent);
+            } else if (event.error == DiscoveryError.HTTP_AUTH_REQUIRED) {
+                assertEquals(TEST_EVENTS.HTTP_AUTH_REQUIRED, mNextEvent);
+            } else if (event.error == DiscoveryError.ERRONEOUS_SSL_CERTIFICATE) {
+                assertEquals(TEST_EVENTS.ERRONEOUS_SSL_CERTIFICATE, mNextEvent);
+            } else {
+                throw new AssertionError("Didn't get the correct error, expected: " + mNextEvent + ", and got: "
+                        + event.error);
+            }
+            mPayload.xmlrpcEndpoint = event.failedEndpoint;
+            mCountDownLatch.countDown();
+        } else {
+            AppLog.i(T.API, "Discovery succeeded, endpoint: " + event.xmlRpcEndpoint);
+            assertEquals(TEST_EVENTS.DISCOVERY_SUCCEEDED, mNextEvent);
+            mPayload.xmlrpcEndpoint = event.xmlRpcEndpoint;
+            mCountDownLatch.countDown();
+        }
     }
 }
