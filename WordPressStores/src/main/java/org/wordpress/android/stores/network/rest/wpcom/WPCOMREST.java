b6fd3c63498fbe33cbaff56bad134728a2250aef
Tony Rankin <tonyr@tonyrankin.com>
2016-07-27 19:21:08 +0200

    Adding WPcom REST endpoints for media methods

--- WordPressStores/src/main/java/org/wordpress/android/stores/network/rest/wpcom/WPCOMREST.java
+++ WordPressStores/src/main/java/org/wordpress/android/stores/network/rest/wpcom/WPCOMREST.java
@@ -11,7 +11,13 @@ public enum WPCOMREST {
     SITES_NEW("/sites/new"),
 
     // Users
-    USERS_NEW("/users/new");
+    USERS_NEW("/users/new"),
+
+    // Media
+    MEDIA_ALL("/sites/%s/media"),
+    MEDIA_ITEM("/sites/%s/media/%s"),
+    MEDIA_NEW("/sites/%s/media/new"),
+    MEDIA_DELETE("/sites/%s/media/%s/delete");
 
     private static final String WPCOM_REST_PREFIX = "https://public-api.wordpress.com/rest";
     private static final String WPCOM_PREFIX_V1 = WPCOM_REST_PREFIX + "/v1";
