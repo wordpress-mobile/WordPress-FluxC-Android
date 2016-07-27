68e9a92d09ec489bac229f3918a019b05a80cd0c
Alex Forcier <alex.forcier@automattic.com>
2016-07-27 19:22:06 +0200

    Added instrumentation test class for XML-RPC posts
    - First test checks new post creation and uploading

--- example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_AppComponent.java
+++ example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_AppComponent.java
@@ -21,7 +21,8 @@ import dagger.Component;
 })
 public interface ReleaseStack_AppComponent {
     void inject(ReleaseStack_AccountTest test);
+    void inject(ReleaseStack_DiscoveryTest test);
+    void inject(ReleaseStack_PostTestXMLRPC test);
     void inject(ReleaseStack_SiteTest test);
     void inject(ReleaseStack_SiteTestWPCOM test);
-    void inject(ReleaseStack_DiscoveryTest test);
 }
---
+++ example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_PostTestXMLRPC.java
@@ -0,0 +1,97 @@
+package org.wordpress.android.stores.release;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.wordpress.android.stores.Dispatcher;
+import org.wordpress.android.stores.TestUtils;
+import org.wordpress.android.stores.example.BuildConfig;
+import org.wordpress.android.stores.generated.PostActionBuilder;
+import org.wordpress.android.stores.model.PostModel;
+import org.wordpress.android.stores.model.SiteModel;
+import org.wordpress.android.stores.store.PostStore;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+public class ReleaseStack_PostTestXMLRPC extends ReleaseStack_Base {
+    @Inject Dispatcher mDispatcher;
+    @Inject PostStore mPostStore;
+
+    CountDownLatch mCountDownLatch;
+
+    enum TEST_EVENTS {
+        NONE,
+        POST_INSTANTIATED,
+        POST_UPLOADED
+    }
+    private TEST_EVENTS mNextEvent;
+
+    private PostModel mPost;
+    private SiteModel mSite;
+
+    {
+        mSite = new SiteModel();
+        mSite.setId(1);
+        mSite.setDotOrgSiteId(0);
+        mSite.setUsername(BuildConfig.TEST_WPORG_USERNAME_SH_SIMPLE);
+        mSite.setPassword(BuildConfig.TEST_WPORG_PASSWORD_SH_SIMPLE);
+        mSite.setXmlRpcUrl(BuildConfig.TEST_WPORG_URL_SH_SIMPLE_ENDPOINT);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mReleaseStackAppComponent.inject(this);
+        // Register
+        mDispatcher.register(this);
+        // Reset expected test event
+        mNextEvent = TEST_EVENTS.NONE;
+
+        mPost = null;
+    }
+
+    public void testUploadNewPost() throws InterruptedException {
+        // Instantiate new post
+        mNextEvent = TEST_EVENTS.POST_INSTANTIATED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        PostStore.InstantiatePostPayload initPayload = new PostStore.InstantiatePostPayload(mSite, false);
+        mDispatcher.dispatch(PostActionBuilder.newInstantiatePostAction(initPayload));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        // Upload new post to site
+        mPost.setTitle("From example activity");
+        mPost.setDescription("Hi there, I'm a post from WordPress-{Placeholder Name}-Android!");
+        mPost.setFeaturedImageId(0);
+
+        mNextEvent = TEST_EVENTS.POST_UPLOADED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        PostStore.ChangePostPayload pushPayload = new PostStore.ChangePostPayload(mPost, mSite);
+        mDispatcher.dispatch(PostActionBuilder.newPushPostAction(pushPayload));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    @Subscribe
+    public void OnPostInstantiated(PostStore.OnPostInstantiated event) {
+        assertEquals(TEST_EVENTS.POST_INSTANTIATED, mNextEvent);
+
+        assertEquals(0, event.post.getRemotePostId());
+        assertNotSame(0, event.post.getId());
+        assertNotSame(0, event.post.getLocalSiteId());
+
+        mPost = event.post;
+        mCountDownLatch.countDown();
+    }
+
+    // TODO: Test making local changes, then reverting them, and check that (FETCH_POST) works correctly and syncs with the server
+
+    @Subscribe
+    public void onPostUploaded(PostStore.OnPostUploaded event) {
+        assertEquals(TEST_EVENTS.POST_UPLOADED, mNextEvent);
+        mCountDownLatch.countDown();
+    }
+}
