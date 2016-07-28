969620ee89b2ee798467afe2b4fe14a8d440a272
Alex Forcier <alex.forcier@automattic.com>
2016-07-28 11:01:23 +0200

    Added tests for editing remote posts and reverting posts with local changes

--- example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_PostTestXMLRPC.java
+++ example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_PostTestXMLRPC.java
@@ -3,6 +3,7 @@ package org.wordpress.android.stores.release;
 import org.greenrobot.eventbus.Subscribe;
 import org.wordpress.android.stores.Dispatcher;
 import org.wordpress.android.stores.TestUtils;
+import org.wordpress.android.stores.action.PostAction;
 import org.wordpress.android.stores.example.BuildConfig;
 import org.wordpress.android.stores.generated.PostActionBuilder;
 import org.wordpress.android.stores.model.PostModel;
@@ -23,7 +24,8 @@ public class ReleaseStack_PostTestXMLRPC extends ReleaseStack_Base {
     enum TEST_EVENTS {
         NONE,
         POST_INSTANTIATED,
-        POST_UPLOADED
+        POST_UPLOADED,
+        POST_UPDATED
     }
     private TEST_EVENTS mNextEvent;
 
@@ -80,6 +82,65 @@ public class ReleaseStack_PostTestXMLRPC extends ReleaseStack_Base {
         assertEquals(false, uploadedPost.isLocalDraft());
     }
 
+    public void testEditingRemotePost() throws InterruptedException {
+        createNewPost();
+
+        mPost.setTitle("PostTextXMLRPC base post");
+        mPost.setDescription("Hi there, I'm a post from FluxC!");
+        mPost.setFeaturedImageId(0);
+
+        uploadPost(mPost);
+
+        mPost.setTitle("From testEditingRemotePost");
+        mPost.setIsLocallyChanged(true);
+
+        // Upload edited post
+        uploadPost(mPost);
+
+        assertEquals("From testEditingRemotePost", mPost.getTitle());
+    }
+
+    public void testRevertingLocallyChangedPost() throws InterruptedException {
+        createNewPost();
+
+        mPost.setTitle("PostTextXMLRPC base post");
+        mPost.setDescription("Hi there, I'm a post from FluxC!");
+        mPost.setFeaturedImageId(0);
+
+        uploadPost(mPost);
+
+        mPost.setTitle("From testRevertingLocallyChangedPost");
+        mPost.setIsLocallyChanged(true);
+
+        // Revert changes to post
+        mNextEvent = TEST_EVENTS.POST_UPDATED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        mDispatcher.dispatch(PostActionBuilder.newFetchPostAction(new PostStore.FetchPostPayload(mPost, mSite)));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        // Get the current copy of the post from the PostStore
+        PostModel latestPost = mPostStore.getPostByLocalPostId(mPost.getId());
+
+        assertEquals("PostTextXMLRPC base post", latestPost.getTitle());
+        assertEquals(false, latestPost.isLocallyChanged());
+    }
+
+    // TEST overwriting local saved post with UPDATE
+
+    // TEST full set of post params assigned, uploaded, and verified after fetch
+
+    @Subscribe
+    public void onPostChanged(PostStore.OnPostChanged event) {
+        if (event.causeOfChange.equals(PostAction.UPDATE_POST)) {
+            if (mCountDownLatch.getCount() > 0) {
+                assertEquals(TEST_EVENTS.POST_UPDATED, mNextEvent);
+                mCountDownLatch.countDown();
+            }
+        }
+    }
+
     @Subscribe
     public void OnPostInstantiated(PostStore.OnPostInstantiated event) {
         assertEquals(TEST_EVENTS.POST_INSTANTIATED, mNextEvent);
@@ -93,8 +154,6 @@ public class ReleaseStack_PostTestXMLRPC extends ReleaseStack_Base {
         mCountDownLatch.countDown();
     }
 
-    // TODO: Test making local changes, then reverting them, and check that (FETCH_POST) works correctly and syncs with the server
-
     @Subscribe
     public void onPostUploaded(PostStore.OnPostUploaded event) {
         assertEquals(TEST_EVENTS.POST_UPLOADED, mNextEvent);
@@ -105,5 +164,26 @@ public class ReleaseStack_PostTestXMLRPC extends ReleaseStack_Base {
         mPost = event.post;
 
         mCountDownLatch.countDown();
+    }
+
+    private void createNewPost() throws InterruptedException {
+        // Instantiate new post
+        mNextEvent = TEST_EVENTS.POST_INSTANTIATED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        PostStore.InstantiatePostPayload initPayload = new PostStore.InstantiatePostPayload(mSite, false);
+        mDispatcher.dispatch(PostActionBuilder.newInstantiatePostAction(initPayload));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private void uploadPost(PostModel post) throws InterruptedException {
+        mNextEvent = TEST_EVENTS.POST_UPLOADED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        PostStore.ChangePostPayload pushPayload = new PostStore.ChangePostPayload(mPost, mSite);
+        mDispatcher.dispatch(PostActionBuilder.newPushPostAction(pushPayload));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 }
