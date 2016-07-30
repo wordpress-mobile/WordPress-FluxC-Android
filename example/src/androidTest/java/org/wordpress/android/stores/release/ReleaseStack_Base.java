c369b95da4b1caf1881983539d67a8b556c6686f
Tony Rankin <tonyr@tonyrankin.com>
2016-07-30 11:44:20 +0200

    Clarifying Store injection requirements for ReleaseStack test classes

--- example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_Base.java
+++ example/src/androidTest/java/org/wordpress/android/stores/release/ReleaseStack_Base.java
@@ -8,6 +8,17 @@ import com.yarolegovich.wellsql.WellSql;
 import org.wordpress.android.stores.module.AppContextModule;
 import org.wordpress.android.stores.persistence.WellSqlConfig;
 
+/**
+ * NOTE:
+ * Stores are not instantiated by default when running tests. If a test sub-class dispatches an
+ * event or listens for an event from a Store, that test class MUST INJECT THE STORE even if the
+ * Store is not explicitly used.
+ *
+ * For example:
+ *  ReleaseStack_SiteTestWPCOM dispatches an authentication event that is handled by AccountStore.
+ *  Therefore the test class must provide an injected AccountStore member, even though
+ *  methods/properties from the AccountStore are never explicitly invoked.
+ */
 public class ReleaseStack_Base extends InstrumentationTestCase {
     Context mAppContext;
     ReleaseStack_AppComponent mReleaseStackAppComponent;
