1e3a4365d61670c066e12453d2408eb077b86078
Oguz Kocer <oguz.kocer@automattic.com>
2016-08-09 17:56:59 +0300

    Adds login button & dialog

--- instaflux/src/main/java/org/wordpress/android/fluxc/instaflux/MainInstafluxActivity.java
+++ instaflux/src/main/java/org/wordpress/android/fluxc/instaflux/MainInstafluxActivity.java
@@ -1,12 +1,35 @@
 package org.wordpress.android.fluxc.instaflux;
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainInstafluxActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button signInBtn = (Button) findViewById(R.id.sign_in_button);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignInDialog();
+            }
+        });
+    }
+
+    private void showSignInDialog() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        DialogFragment newFragment = ThreeEditTextDialog.newInstance(new ThreeEditTextDialog.Listener() {
+            @Override
+            public void onClick(String username, String password, String url) {
+                // sign in action
+            }
+        }, "Username", "Password", "XMLRPC Url");
+        newFragment.show(ft, "dialog");
     }
 }
