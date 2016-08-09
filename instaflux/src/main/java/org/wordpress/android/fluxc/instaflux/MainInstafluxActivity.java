4a56c9c021ab1f95ba25f753bf3517b45869cd27
Oguz Kocer <oguz.kocer@automattic.com>
2016-08-09 19:27:05 +0300

    wpcom login flow implemented

--- instaflux/src/main/java/org/wordpress/android/fluxc/instaflux/MainInstafluxActivity.java
+++ instaflux/src/main/java/org/wordpress/android/fluxc/instaflux/MainInstafluxActivity.java
@@ -4,15 +4,25 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
+import org.wordpress.android.fluxc.generated.SiteActionBuilder;
+import org.wordpress.android.fluxc.store.AccountStore;
 
 import javax.inject.Inject;
 
 public class MainInstafluxActivity extends AppCompatActivity {
+    @Inject AccountStore mAccountStore;
     @Inject Dispatcher mDispatcher;
+
+    private static String TAG = "MainInstafluxActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +58,42 @@ public class MainInstafluxActivity extends AppCompatActivity {
         DialogFragment newFragment = ThreeEditTextDialog.newInstance(new ThreeEditTextDialog.Listener() {
             @Override
             public void onClick(String username, String password, String url) {
-                // sign in action
+                signInAction(username, password, url);
             }
         }, "Username", "Password", "XMLRPC Url");
         newFragment.show(ft, "dialog");
+    }
+
+    private void signInAction(final String username, final String password, final String url) {
+        if (TextUtils.isEmpty(url)) {
+            wpcomFetchSites(username, password);
+        }
+    }
+
+    private void wpcomFetchSites(String username, String password) {
+        AccountStore.AuthenticatePayload payload = new AccountStore.AuthenticatePayload(username, password);
+        // Next action will be dispatched if authentication is successful
+        payload.nextAction = SiteActionBuilder.newFetchSitesAction();
+        mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateAction(payload));
+    }
+
+    // Event listeners
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onAccountChanged(AccountStore.OnAccountChanged event) {
+        if (!mAccountStore.hasAccessToken()) {
+            Log.i(TAG, "Signed out!");
+        } else {
+            if (event.accountInfosChanged) {
+                Log.i(TAG, "Display Name: " + mAccountStore.getAccount().getDisplayName());
+            }
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onAuthenticationChanged(AccountStore.OnAuthenticationChanged event) {
+        if (event.isError) {
+            Log.e(TAG, "Authentication error: " + event.errorType + " - " + event.errorMessage);
+        }
     }
 }
