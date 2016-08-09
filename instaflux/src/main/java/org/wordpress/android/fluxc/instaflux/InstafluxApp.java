4797a46889aea5c930bcf87dad1384778b410daa
Oguz Kocer <oguz.kocer@automattic.com>
2016-08-09 17:02:22 +0300

    Instaflux sample app introduced

---
+++ instaflux/src/main/java/org/wordpress/android/fluxc/instaflux/InstafluxApp.java
@@ -0,0 +1,11 @@
+package org.wordpress.android.fluxc.instaflux;
+
+import android.app.Application;
+
+public class InstafluxApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ instaflux/src/main/java/org/wordpress/android/fluxc/instaflux/MainInstafluxActivity.java
@@ -0,0 +1,12 @@
+package org.wordpress.android.fluxc.instaflux;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainInstafluxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
