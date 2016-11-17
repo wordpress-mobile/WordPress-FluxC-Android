a8cdfbac2e1b4475d181a9d10dd2a06bbc636680
Alex Forcier <alex.forcier@automattic.com>
2016-11-17 13:40:47 -0500

    Add support for uploading terms (XML-RPC)

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TaxonomyTestXMLRPC.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TaxonomyTestXMLRPC.java
@@ -1,14 +1,20 @@
 package org.wordpress.android.fluxc.release;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.greenrobot.eventbus.Subscribe;
 import org.wordpress.android.fluxc.TestUtils;
 import org.wordpress.android.fluxc.generated.TaxonomyActionBuilder;
 import org.wordpress.android.fluxc.model.TaxonomyModel;
 import org.wordpress.android.fluxc.model.TermModel;
 import org.wordpress.android.fluxc.store.TaxonomyStore;
+import org.wordpress.android.fluxc.store.TaxonomyStore.FetchTermsPayload;
+import org.wordpress.android.fluxc.store.TaxonomyStore.InstantiateTermPayload;
 import org.wordpress.android.fluxc.store.TaxonomyStore.OnTaxonomyChanged;
+import org.wordpress.android.fluxc.store.TaxonomyStore.OnTermInstantiated;
+import org.wordpress.android.fluxc.store.TaxonomyStore.OnTermUploaded;
 import org.wordpress.android.fluxc.store.TaxonomyStore.RemoteTermPayload;
 import org.wordpress.android.fluxc.store.TaxonomyStore.TaxonomyError;
+import org.wordpress.android.fluxc.store.TaxonomyStore.TaxonomyErrorType;
 import org.wordpress.android.fluxc.utils.WellSqlUtils;
 import org.wordpress.android.util.AppLog;
 import org.wordpress.android.util.AppLog.T;
@@ -22,12 +28,17 @@ import javax.inject.Inject;
 public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
     @Inject TaxonomyStore mTaxonomyStore;
 
+    private static final String TERM_DEFAULT_NAME = "fluxc-example";
+    private static final String TERM_DEFAULT_DESCRIPTION = "A term from FluxC";
+
     private enum TestEvents {
         NONE,
         CATEGORIES_FETCHED,
         TAGS_FETCHED,
         TERMS_FETCHED,
         TERM_UPDATED,
+        TERM_INSTANTIATED,
+        TERM_UPLOADED,
         ERROR_INVALID_TAXONOMY,
         ERROR_UNAUTHORIZED,
         ERROR_GENERIC
@@ -35,6 +46,7 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
 
     private TestEvents mNextEvent;
     private TaxonomyError mLastTaxonomyError;
+    private TermModel mTerm;
 
     @Override
     protected void setUp() throws Exception {
@@ -80,7 +92,7 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
         TaxonomyModel taxonomyModel = new TaxonomyModel();
         taxonomyModel.setName("roads");
 
-        TaxonomyStore.FetchTermsPayload payload = new TaxonomyStore.FetchTermsPayload(sSite, taxonomyModel);
+        FetchTermsPayload payload = new FetchTermsPayload(sSite, taxonomyModel);
         mDispatcher.dispatch(TaxonomyActionBuilder.newFetchTermsAction(payload));
 
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
@@ -109,6 +121,57 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
         assertNotSame(0, fetchedTerm.getRemoteTermId());
     }
 
+    public void testUploadNewCategory() throws InterruptedException {
+        // Instantiate new category
+        createNewCategory();
+        setupTermAttributes();
+
+        // Upload new term to site
+        uploadTerm(mTerm);
+
+        TermModel uploadedTerm = mTaxonomyStore.getTermsForSite(sSite, "category").get(0);
+
+        assertEquals(1, WellSqlUtils.getTotalTermsCount());
+        assertEquals(1, mTaxonomyStore.getTermsForSite(sSite, "category").size());
+
+        assertNotSame(0, uploadedTerm.getRemoteTermId());
+    }
+
+    public void testUploadNewTag() throws InterruptedException {
+        // Instantiate new tag
+        createNewTag();
+        setupTermAttributes();
+
+        // Upload new term to site
+        uploadTerm(mTerm);
+
+        TermModel uploadedTerm = mTaxonomyStore.getTermsForSite(sSite, "post_tag").get(0);
+
+        assertEquals(1, WellSqlUtils.getTotalTermsCount());
+        assertEquals(1, mTaxonomyStore.getTermsForSite(sSite, "post_tag").size());
+
+        assertNotSame(0, uploadedTerm.getRemoteTermId());
+    }
+
+    public void testUploadNewCategoryAsTerm() throws InterruptedException {
+        TaxonomyModel taxonomyModel = new TaxonomyModel();
+        taxonomyModel.setName("category");
+
+        // Instantiate new term
+        createNewTerm(taxonomyModel);
+        setupTermAttributes();
+
+        // Upload new term to site
+        uploadTerm(mTerm);
+
+        TermModel uploadedTerm = mTaxonomyStore.getTermsForSite(sSite, "category").get(0);
+
+        assertEquals(1, WellSqlUtils.getTotalTermsCount());
+        assertEquals(1, mTaxonomyStore.getTermsForSite(sSite, "category").size());
+
+        assertNotSame(0, uploadedTerm.getRemoteTermId());
+    }
+
     @Subscribe
     public void onTaxonomyChanged(OnTaxonomyChanged event) {
         AppLog.i(T.API, "Received OnTaxonomyChanged, causeOfChange: " + event.causeOfChange);
@@ -116,10 +179,10 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
             AppLog.i(T.API, "OnTaxonomyChanged has error: " + event.error.type + " - " + event.error.message);
             mLastTaxonomyError = event.error;
             if (mNextEvent.equals(TestEvents.ERROR_INVALID_TAXONOMY)) {
-                assertEquals(TaxonomyStore.TaxonomyErrorType.INVALID_TAXONOMY, event.error.type);
+                assertEquals(TaxonomyErrorType.INVALID_TAXONOMY, event.error.type);
                 mCountDownLatch.countDown();
             } else if (mNextEvent.equals(TestEvents.ERROR_GENERIC)) {
-                assertEquals(TaxonomyStore.TaxonomyErrorType.GENERIC_ERROR, event.error.type);
+                assertEquals(TaxonomyErrorType.GENERIC_ERROR, event.error.type);
                 mCountDownLatch.countDown();
             }
             return;
@@ -150,5 +213,86 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
                 }
                 break;
         }
+    }
+
+    @Subscribe
+    public void onTermUploaded(OnTermUploaded event) {
+        AppLog.i(T.API, "Received OnTermUploaded");
+        if (event.isError()) {
+            AppLog.i(T.API, "OnTermUploaded has error: " + event.error.type + " - " + event.error.message);
+            mLastTaxonomyError = event.error;
+            // TODO this case can't happen at the moment...but maybe should leave it in for future anyway
+            if (mNextEvent.equals(TestEvents.ERROR_INVALID_TAXONOMY)) {
+                assertEquals(TaxonomyErrorType.INVALID_TAXONOMY, event.error.type);
+                mCountDownLatch.countDown();
+            } else if (mNextEvent.equals(TestEvents.ERROR_GENERIC)) {
+                assertEquals(TaxonomyErrorType.GENERIC_ERROR, event.error.type);
+                mCountDownLatch.countDown();
+            }
+            return;
+        }
+        assertEquals(TestEvents.TERM_UPLOADED, mNextEvent);
+        assertNotSame(0, event.term.getRemoteTermId());
+
+        mCountDownLatch.countDown();
+    }
+
+    @Subscribe
+    public void onTermInstantiated(OnTermInstantiated event) {
+        AppLog.i(T.API, "Received OnTermInstantiated");
+        assertEquals(TestEvents.TERM_INSTANTIATED, mNextEvent);
+
+        assertEquals(0, event.term.getRemoteTermId());
+        assertNotSame(0, event.term.getId());
+        assertNotSame(0, event.term.getLocalSiteId());
+
+        mTerm = event.term;
+        mCountDownLatch.countDown();
+    }
+
+    private void setupTermAttributes() {
+        mTerm.setName(TERM_DEFAULT_NAME + "-" + RandomStringUtils.randomAlphanumeric(4));
+        mTerm.setDescription(TERM_DEFAULT_DESCRIPTION);
+    }
+
+    private void createNewCategory() throws InterruptedException {
+        // Instantiate new category
+        mNextEvent = TestEvents.TERM_INSTANTIATED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        mDispatcher.dispatch(TaxonomyActionBuilder.newInstantiateCategoryAction(sSite));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private void createNewTag() throws InterruptedException {
+        // Instantiate new tag
+        mNextEvent = TestEvents.TERM_INSTANTIATED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        mDispatcher.dispatch(TaxonomyActionBuilder.newInstantiateTagAction(sSite));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private void createNewTerm(TaxonomyModel taxonomy) throws InterruptedException {
+        // Instantiate new term
+        mNextEvent = TestEvents.TERM_INSTANTIATED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        InstantiateTermPayload payload = new InstantiateTermPayload(sSite, taxonomy);
+        mDispatcher.dispatch(TaxonomyActionBuilder.newInstantiateTermAction(payload));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private void uploadTerm(TermModel term) throws InterruptedException {
+        mNextEvent = TestEvents.TERM_UPLOADED;
+        mCountDownLatch = new CountDownLatch(1);
+
+        RemoteTermPayload pushPayload = new RemoteTermPayload(term, sSite);
+        mDispatcher.dispatch(TaxonomyActionBuilder.newPushTermAction(pushPayload));
+
+        assertEquals(true, mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 }
