e72e51e0ccd714e6c922d0d1047ba667b41060ac
Alex Forcier <alex.forcier@automattic.com>
2016-11-24 10:48:24 -0500

    Use XMLRPCBase class for taxonomy tests

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TaxonomyTestXMLRPC.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TaxonomyTestXMLRPC.java
@@ -1,14 +1,12 @@
 package org.wordpress.android.fluxc.release;
 
 import org.greenrobot.eventbus.Subscribe;
-import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.TestUtils;
-import org.wordpress.android.fluxc.example.BuildConfig;
 import org.wordpress.android.fluxc.generated.TaxonomyActionBuilder;
-import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.model.TaxonomyModel;
 import org.wordpress.android.fluxc.model.TermModel;
 import org.wordpress.android.fluxc.store.TaxonomyStore;
+import org.wordpress.android.fluxc.store.TaxonomyStore.OnTaxonomyChanged;
 import org.wordpress.android.fluxc.store.TaxonomyStore.RemoteTermPayload;
 import org.wordpress.android.fluxc.store.TaxonomyStore.TaxonomyError;
 import org.wordpress.android.fluxc.utils.WellSqlUtils;
@@ -21,16 +19,10 @@ import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
-public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
-    @Inject Dispatcher mDispatcher;
+public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_XMLRPCBase {
     @Inject TaxonomyStore mTaxonomyStore;
 
-    private CountDownLatch mCountDownLatch;
-    private static SiteModel sSite;
-
-    private TaxonomyError mLastTaxonomyError;
-
-    private enum TEST_EVENTS {
+    private enum TestEvents {
         NONE,
         CATEGORIES_FETCHED,
         TAGS_FETCHED,
@@ -40,31 +32,25 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
         ERROR_UNAUTHORIZED,
         ERROR_GENERIC
     }
-    private TEST_EVENTS mNextEvent;
 
-    {
-        sSite = new SiteModel();
-        sSite.setId(1);
-        sSite.setSelfHostedSiteId(0);
-        sSite.setUsername(BuildConfig.TEST_WPORG_USERNAME_SH_SIMPLE);
-        sSite.setPassword(BuildConfig.TEST_WPORG_PASSWORD_SH_SIMPLE);
-        sSite.setXmlRpcUrl(BuildConfig.TEST_WPORG_URL_SH_SIMPLE_ENDPOINT);
-    }
+    private TestEvents mNextEvent;
+    private TaxonomyError mLastTaxonomyError;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mReleaseStackAppComponent.inject(this);
-        // Register
-        mDispatcher.register(this);
+
+        // Fetch sites and initialize sSite
+        init();
         // Reset expected test event
-        mNextEvent = TEST_EVENTS.NONE;
+        mNextEvent = TestEvents.NONE;
 
         mLastTaxonomyError = null;
     }
 
     public void testFetchCategories() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.CATEGORIES_FETCHED;
+        mNextEvent = TestEvents.CATEGORIES_FETCHED;
         mCountDownLatch = new CountDownLatch(1);
 
         mDispatcher.dispatch(TaxonomyActionBuilder.newFetchCategoriesAction(sSite));
@@ -76,7 +62,7 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
     }
 
     public void testFetchTags() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.TAGS_FETCHED;
+        mNextEvent = TestEvents.TAGS_FETCHED;
         mCountDownLatch = new CountDownLatch(1);
 
         mDispatcher.dispatch(TaxonomyActionBuilder.newFetchTagsAction(sSite));
@@ -88,7 +74,7 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
     }
 
     public void testFetchTermsForInvalidTaxonomy() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.ERROR_GENERIC;
+        mNextEvent = TestEvents.ERROR_GENERIC;
         mCountDownLatch = new CountDownLatch(1);
 
         TaxonomyModel taxonomyModel = new TaxonomyModel();
@@ -105,7 +91,7 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
     }
 
     public void testFetchSingleCategory() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.TERM_UPDATED;
+        mNextEvent = TestEvents.TERM_UPDATED;
         mCountDownLatch = new CountDownLatch(1);
 
         TermModel term = new TermModel();
@@ -124,15 +110,15 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
     }
 
     @Subscribe
-    public void onTaxonomyChanged(TaxonomyStore.OnTaxonomyChanged event) {
+    public void onTaxonomyChanged(OnTaxonomyChanged event) {
         AppLog.i(T.API, "Received OnTaxonomyChanged, causeOfChange: " + event.causeOfChange);
         if (event.isError()) {
             AppLog.i(T.API, "OnTaxonomyChanged has error: " + event.error.type + " - " + event.error.message);
             mLastTaxonomyError = event.error;
-            if (mNextEvent.equals(TEST_EVENTS.ERROR_INVALID_TAXONOMY)) {
+            if (mNextEvent.equals(TestEvents.ERROR_INVALID_TAXONOMY)) {
                 assertEquals(TaxonomyStore.TaxonomyErrorType.INVALID_TAXONOMY, event.error.type);
                 mCountDownLatch.countDown();
-            } else if (mNextEvent.equals(TEST_EVENTS.ERROR_GENERIC)) {
+            } else if (mNextEvent.equals(TestEvents.ERROR_GENERIC)) {
                 assertEquals(TaxonomyStore.TaxonomyErrorType.GENERIC_ERROR, event.error.type);
                 mCountDownLatch.countDown();
             }
@@ -140,24 +126,25 @@ public class ReleaseStack_TaxonomyTestXMLRPC extends ReleaseStack_Base {
         }
         switch (event.causeOfChange) {
             case FETCH_CATEGORIES:
-                if (mNextEvent.equals(TEST_EVENTS.CATEGORIES_FETCHED)) {
+                if (mNextEvent.equals(TestEvents.CATEGORIES_FETCHED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " categories");
                     mCountDownLatch.countDown();
                 }
                 break;
             case FETCH_TAGS:
-                if (mNextEvent.equals(TEST_EVENTS.TAGS_FETCHED)) {
+                if (mNextEvent.equals(TestEvents.TAGS_FETCHED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " tags");
                     mCountDownLatch.countDown();
                 }
                 break;
             case FETCH_TERMS:
-                if (mNextEvent.equals(TEST_EVENTS.TERMS_FETCHED)) {
+                if (mNextEvent.equals(TestEvents.TERMS_FETCHED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " " + event.taxonomyName + " terms");
                     mCountDownLatch.countDown();
                 }
+                break;
             case UPDATE_TERM:
-                if (mNextEvent.equals(TEST_EVENTS.TERM_UPDATED)) {
+                if (mNextEvent.equals(TestEvents.TERM_UPDATED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " term");
                     mCountDownLatch.countDown();
                 }
