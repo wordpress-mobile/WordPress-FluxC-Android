576c9ad806427c03bcaef14b709505e246857a72
Alex Forcier <alex.forcier@automattic.com>
2016-11-24 10:49:39 -0500

    Use WPComBase class for taxonomy tests

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TaxonomyTestWPCom.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TaxonomyTestWPCom.java
@@ -1,19 +1,13 @@
 package org.wordpress.android.fluxc.release;
 
 import org.greenrobot.eventbus.Subscribe;
-import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.TestUtils;
-import org.wordpress.android.fluxc.example.BuildConfig;
-import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
-import org.wordpress.android.fluxc.generated.SiteActionBuilder;
 import org.wordpress.android.fluxc.generated.TaxonomyActionBuilder;
-import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.model.TaxonomyModel;
 import org.wordpress.android.fluxc.model.TermModel;
-import org.wordpress.android.fluxc.store.AccountStore;
-import org.wordpress.android.fluxc.store.SiteStore;
 import org.wordpress.android.fluxc.store.TaxonomyStore;
 import org.wordpress.android.fluxc.store.TaxonomyStore.FetchTermsPayload;
+import org.wordpress.android.fluxc.store.TaxonomyStore.OnTaxonomyChanged;
 import org.wordpress.android.fluxc.store.TaxonomyStore.RemoteTermPayload;
 import org.wordpress.android.fluxc.store.TaxonomyStore.TaxonomyErrorType;
 import org.wordpress.android.fluxc.utils.WellSqlUtils;
@@ -26,18 +20,11 @@ import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
-public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
-    @Inject Dispatcher mDispatcher;
-    @Inject AccountStore mAccountStore;
-    @Inject SiteStore mSiteStore;
+public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_WPComBase {
     @Inject TaxonomyStore mTaxonomyStore;
 
-    private CountDownLatch mCountDownLatch;
-    private static SiteModel sSite;
-
-    private enum TEST_EVENTS {
+    private enum TestEvents {
         NONE,
-        SITE_CHANGED,
         CATEGORIES_FETCHED,
         TAGS_FETCHED,
         TERMS_FETCHED,
@@ -46,29 +33,22 @@ public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
         ERROR_UNAUTHORIZED,
         ERROR_GENERIC
     }
-    private TEST_EVENTS mNextEvent;
+
+    private TestEvents mNextEvent;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         mReleaseStackAppComponent.inject(this);
-        // Register
-        mDispatcher.register(this);
-        // Reset expected test event
-        mNextEvent = TEST_EVENTS.NONE;
 
-        if (mAccountStore.getAccessToken().isEmpty()) {
-            authenticate();
-        }
-
-        if (sSite == null) {
-            fetchSites();
-            sSite = mSiteStore.getSites().get(0);
-        }
+        // Authenticate, fetch sites and initialize sSite
+        init();
+        // Init mNextEvent
+        mNextEvent = TestEvents.NONE;
     }
 
     public void testFetchCategories() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.CATEGORIES_FETCHED;
+        mNextEvent = TestEvents.CATEGORIES_FETCHED;
         mCountDownLatch = new CountDownLatch(1);
 
         mDispatcher.dispatch(TaxonomyActionBuilder.newFetchCategoriesAction(sSite));
@@ -80,7 +60,7 @@ public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
     }
 
     public void testFetchTags() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.TAGS_FETCHED;
+        mNextEvent = TestEvents.TAGS_FETCHED;
         mCountDownLatch = new CountDownLatch(1);
 
         mDispatcher.dispatch(TaxonomyActionBuilder.newFetchTagsAction(sSite));
@@ -92,7 +72,7 @@ public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
     }
 
     public void testFetchTermsForInvalidTaxonomy() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.ERROR_INVALID_TAXONOMY;
+        mNextEvent = TestEvents.ERROR_INVALID_TAXONOMY;
         mCountDownLatch = new CountDownLatch(1);
 
         TaxonomyModel taxonomyModel = new TaxonomyModel();
@@ -105,7 +85,7 @@ public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
     }
 
     public void testFetchSingleCategory() throws InterruptedException {
-        mNextEvent = TEST_EVENTS.TERM_UPDATED;
+        mNextEvent = TestEvents.TERM_UPDATED;
         mCountDownLatch = new CountDownLatch(1);
 
         TermModel term = new TermModel();
@@ -122,56 +102,15 @@ public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
         assertNotSame(0, fetchedTerm.getRemoteTermId());
     }
 
-    private void authenticate() throws InterruptedException {
-        // Authenticate a test user (actual credentials declared in gradle.properties)
-        AccountStore.AuthenticatePayload payload =
-                new AccountStore.AuthenticatePayload(BuildConfig.TEST_WPCOM_USERNAME_TEST1,
-                        BuildConfig.TEST_WPCOM_PASSWORD_TEST1);
-        mCountDownLatch = new CountDownLatch(1);
-
-        // Correct user we should get an OnAuthenticationChanged message
-        mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateAction(payload));
-        // Wait for a network response / onChanged event
-        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
-    }
-
-    private void fetchSites() throws InterruptedException {
-        // Fetch sites from REST API, and wait for onSiteChanged event
-        mCountDownLatch = new CountDownLatch(1);
-        mNextEvent = TEST_EVENTS.SITE_CHANGED;
-        mDispatcher.dispatch(SiteActionBuilder.newFetchSitesAction());
-
-        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
-    }
-
     @Subscribe
-    public void onAuthenticationChanged(AccountStore.OnAuthenticationChanged event) {
-        assertFalse(event.isError());
-        mCountDownLatch.countDown();
-    }
-
-    @Subscribe
-    public void onSiteChanged(SiteStore.OnSiteChanged event) {
-        AppLog.i(T.TESTS, "site count " + mSiteStore.getSitesCount());
-        if (event.isError()) {
-            AppLog.i(T.TESTS, "event error type: " + event.error.type);
-            return;
-        }
-        assertTrue(mSiteStore.hasSite());
-        assertTrue(mSiteStore.hasWPComSite());
-        assertEquals(TEST_EVENTS.SITE_CHANGED, mNextEvent);
-        mCountDownLatch.countDown();
-    }
-
-    @Subscribe
-    public void onTaxonomyChanged(TaxonomyStore.OnTaxonomyChanged event) {
+    public void onTaxonomyChanged(OnTaxonomyChanged event) {
         AppLog.i(T.API, "Received OnTaxonomyChanged, causeOfChange: " + event.causeOfChange);
         if (event.isError()) {
             AppLog.i(T.API, "OnTaxonomyChanged has error: " + event.error.type + " - " + event.error.message);
-            if (mNextEvent.equals(TEST_EVENTS.ERROR_INVALID_TAXONOMY)) {
+            if (mNextEvent.equals(TestEvents.ERROR_INVALID_TAXONOMY)) {
                 assertEquals(TaxonomyErrorType.INVALID_TAXONOMY, event.error.type);
                 mCountDownLatch.countDown();
-            } else if (mNextEvent.equals(TEST_EVENTS.ERROR_GENERIC)) {
+            } else if (mNextEvent.equals(TestEvents.ERROR_GENERIC)) {
                 assertEquals(TaxonomyErrorType.GENERIC_ERROR, event.error.type);
                 mCountDownLatch.countDown();
             }
@@ -179,25 +118,25 @@ public class ReleaseStack_TaxonomyTestWPCom extends ReleaseStack_Base {
         }
         switch (event.causeOfChange) {
             case FETCH_CATEGORIES:
-                if (mNextEvent.equals(TEST_EVENTS.CATEGORIES_FETCHED)) {
+                if (mNextEvent.equals(TestEvents.CATEGORIES_FETCHED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " categories");
                     mCountDownLatch.countDown();
                 }
                 break;
             case FETCH_TAGS:
-                if (mNextEvent.equals(TEST_EVENTS.TAGS_FETCHED)) {
+                if (mNextEvent.equals(TestEvents.TAGS_FETCHED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " tags");
                     mCountDownLatch.countDown();
                 }
                 break;
             case FETCH_TERMS:
-                if (mNextEvent.equals(TEST_EVENTS.TERMS_FETCHED)) {
+                if (mNextEvent.equals(TestEvents.TERMS_FETCHED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " " + event.taxonomyName + " terms");
                     mCountDownLatch.countDown();
                 }
                 break;
             case UPDATE_TERM:
-                if (mNextEvent.equals(TEST_EVENTS.TERM_UPDATED)) {
+                if (mNextEvent.equals(TestEvents.TERM_UPDATED)) {
                     AppLog.i(T.API, "Fetched " + event.rowsAffected + " term");
                     mCountDownLatch.countDown();
                 }
