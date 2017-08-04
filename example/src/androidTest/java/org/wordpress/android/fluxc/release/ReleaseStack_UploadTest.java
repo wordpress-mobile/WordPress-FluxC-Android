8130612593b2e654f91937581df1617f05f2907a
Alex Forcier <alex.forcier@automattic.com>
2017-08-04 09:38:35 -0400

    Add connected test for UploadStore media handling

---
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_UploadTest.java
@@ -0,0 +1,167 @@
+package org.wordpress.android.fluxc.release;
+
+import android.annotation.SuppressLint;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.wordpress.android.fluxc.TestUtils;
+import org.wordpress.android.fluxc.action.MediaAction;
+import org.wordpress.android.fluxc.example.BuildConfig;
+import org.wordpress.android.fluxc.generated.MediaActionBuilder;
+import org.wordpress.android.fluxc.model.MediaModel;
+import org.wordpress.android.fluxc.model.MediaUploadModel;
+import org.wordpress.android.fluxc.store.MediaStore;
+import org.wordpress.android.fluxc.store.MediaStore.MediaPayload;
+import org.wordpress.android.fluxc.store.MediaStore.OnMediaChanged;
+import org.wordpress.android.fluxc.store.MediaStore.OnMediaUploaded;
+import org.wordpress.android.fluxc.store.UploadStore;
+import org.wordpress.android.fluxc.utils.MediaUtils;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+@SuppressLint("UseSparseArrays")
+public class ReleaseStack_UploadTest extends ReleaseStack_WPComBase {
+    @Inject MediaStore mMediaStore;
+    @Inject UploadStore mUploadStore;
+
+    private enum TestEvents {
+        CANCELED_MEDIA,
+        DELETED_MEDIA,
+        PUSHED_MEDIA,
+        REMOVED_MEDIA,
+        UPLOADED_MEDIA,
+    }
+
+    private TestEvents mNextEvent;
+    private long mLastUploadedId = -1L;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mReleaseStackAppComponent.inject(this);
+
+        // Authenticate, fetch sites and initialize sSite
+        init();
+    }
+
+    public void testUploadImage() throws InterruptedException {
+        // Start uploading media
+        MediaModel testMedia = newMediaModel(BuildConfig.TEST_LOCAL_IMAGE, MediaUtils.MIME_TYPE_IMAGE);
+        mNextEvent = TestEvents.UPLOADED_MEDIA;
+        MediaPayload payload = new MediaPayload(sSite, testMedia);
+        mCountDownLatch = new CountDownLatch(1);
+        mDispatcher.dispatch(MediaActionBuilder.newUploadMediaAction(payload));
+
+        // Wait for the event to be processed by the UploadStore, then check that a MediaUploadModel has been registered
+        TestUtils.waitFor(50);
+
+        MediaUploadModel mediaUploadModel = mUploadStore.getMediaUploadModelForMediaModel(testMedia);
+        assertNotNull(mediaUploadModel);
+        assertEquals(MediaUploadModel.UPLOADING, mediaUploadModel.getUploadState());
+
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        // Verify and set media ID
+        assertTrue(mLastUploadedId >= 0);
+        testMedia.setMediaId(mLastUploadedId);
+        assertNotNull(mMediaStore.getSiteMediaWithId(sSite, testMedia.getMediaId()));
+
+        // Confirm that the corresponding MediaUploadModel's state has been updated automatically
+        mediaUploadModel = mUploadStore.getMediaUploadModelForMediaModel(testMedia);
+        assertEquals(1F, mediaUploadModel.getProgress());
+        assertEquals(MediaUploadModel.COMPLETED, mediaUploadModel.getUploadState());
+
+        // Delete test image
+        mNextEvent = TestEvents.DELETED_MEDIA;
+        deleteMedia(testMedia);
+
+        // Since the MediaUploadModel keys are linked to MediaModels, the corresponding MediaUploadModel should
+        // have been deleted as well
+        assertNull(mUploadStore.getMediaUploadModelForMediaModel(testMedia));
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onMediaUploaded(OnMediaUploaded event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        if (event.canceled) {
+            if (mNextEvent == TestEvents.CANCELED_MEDIA) {
+                mCountDownLatch.countDown();
+            } else {
+                throw new AssertionError("Unexpected cancellation for media: " + event.media.getId());
+            }
+        } else if (event.completed) {
+             if (mNextEvent == TestEvents.UPLOADED_MEDIA) {
+                mLastUploadedId = event.media.getMediaId();
+            } else {
+                throw new AssertionError("Unexpected completion for media: " + event.media.getId());
+            }
+            mCountDownLatch.countDown();
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onMediaChanged(OnMediaChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        if (event.cause == MediaAction.PUSH_MEDIA) {
+            assertEquals(TestEvents.PUSHED_MEDIA, mNextEvent);
+        } else if (event.cause == MediaAction.DELETE_MEDIA) {
+            assertEquals(TestEvents.DELETED_MEDIA, mNextEvent);
+        } else if (event.cause == MediaAction.REMOVE_MEDIA) {
+            assertEquals(TestEvents.REMOVED_MEDIA, mNextEvent);
+        } else {
+            throw new AssertionError("Unexpected event: " + event.cause);
+        }
+        mCountDownLatch.countDown();
+    }
+
+    private MediaModel newMediaModel(String mediaPath, String mimeType) {
+        return newMediaModel("Test Title", mediaPath, mimeType);
+    }
+
+    private MediaModel newMediaModel(String testTitle, String mediaPath, String mimeType) {
+        final String testDescription = "Test Description";
+        final String testCaption = "Test Caption";
+        final String testAlt = "Test Alt";
+
+        MediaModel testMedia = mMediaStore.instantiateMediaModel();
+        testMedia.setFilePath(mediaPath);
+        testMedia.setFileExtension(mediaPath.substring(mediaPath.lastIndexOf(".") + 1, mediaPath.length()));
+        testMedia.setMimeType(mimeType + testMedia.getFileExtension());
+        testMedia.setFileName(mediaPath.substring(mediaPath.lastIndexOf("/"), mediaPath.length()));
+        testMedia.setTitle(testTitle);
+        testMedia.setDescription(testDescription);
+        testMedia.setCaption(testCaption);
+        testMedia.setAlt(testAlt);
+        testMedia.setLocalSiteId(sSite.getId());
+
+        return testMedia;
+    }
+
+    private void uploadMedia(MediaModel media) throws InterruptedException {
+        MediaPayload payload = new MediaPayload(sSite, media);
+        mCountDownLatch = new CountDownLatch(1);
+        mDispatcher.dispatch(MediaActionBuilder.newUploadMediaAction(payload));
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private void deleteMedia(MediaModel media) throws InterruptedException {
+        MediaPayload deletePayload = new MediaPayload(sSite, media);
+        mCountDownLatch = new CountDownLatch(1);
+        mDispatcher.dispatch(MediaActionBuilder.newDeleteMediaAction(deletePayload));
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private void removeMedia(MediaModel media) throws InterruptedException {
+        mCountDownLatch = new CountDownLatch(1);
+        mDispatcher.dispatch(MediaActionBuilder.newRemoveMediaAction(media));
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+}
