45625bd8d124b4fa1cf84edeaa4567207465ee7c
Tony Rankin <tonyr@tonyrankin.com>
2017-08-28 17:20:44 -0700

    Update example app with installed themes action

--- example/src/main/java/org/wordpress/android/fluxc/example/ThemeFragment.java
+++ example/src/main/java/org/wordpress/android/fluxc/example/ThemeFragment.java
@@ -10,6 +10,7 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.generated.ThemeActionBuilder;
+import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.store.ThemeStore;
 import org.wordpress.android.fluxc.store.SiteStore;
 
@@ -51,6 +52,18 @@ public class ThemeFragment extends Fragment {
             }
         });
 
+        view.findViewById(R.id.fetch_installed_themes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SiteModel jetpackSite = getJetpackConnectedSite();
+                if (jetpackSite == null) {
+                    prependToLog("No Jetpack connected site found, unable to test.");
+                } else {
+                    mDispatcher.dispatch(ThemeActionBuilder.newFetchInstalledThemesAction(jetpackSite));
+                }
+            }
+        });
+
 
         return view;
     }
@@ -62,12 +75,34 @@ public class ThemeFragment extends Fragment {
             prependToLog("error: " + event.error.message);
         } else {
             prependToLog("success: WP.com theme count = " + mThemeStore.getWpThemes().size());
+            SiteModel jpSite = getJetpackConnectedSite();
+            if (jpSite != null) {
+                prependToLog("Installed theme count = " + mThemeStore.getThemesForSite(jpSite).size());
+            }
         }
     }
 
     @SuppressWarnings("unused")
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onSiteChanged(SiteStore.OnSiteChanged event) {
+    }
+
+    private SiteModel getWPComSite() {
+        for (SiteModel site : mSiteStore.getSites()) {
+            if (site != null && site.isWPCom()) {
+                return site;
+            }
+        }
+        return null;
+    }
+
+    private SiteModel getJetpackConnectedSite() {
+        for (SiteModel site : mSiteStore.getSites()) {
+            if (site != null && site.isJetpackConnected()) {
+                return site;
+            }
+        }
+        return null;
     }
 
     private void prependToLog(final String s) {
