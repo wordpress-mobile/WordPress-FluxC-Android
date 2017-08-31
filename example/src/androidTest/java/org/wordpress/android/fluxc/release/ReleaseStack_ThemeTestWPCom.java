8d07e38afc869dbcf584915b39cb003818adb568
Tony Rankin <tonyr@tonyrankin.com>
2017-08-31 13:27:14 -0700

    Prepare test class for adding tests

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ThemeTestWPCom.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ThemeTestWPCom.java
@@ -1,16 +1,32 @@
 package org.wordpress.android.fluxc.release;
 
-import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.SiteStore;
+import org.wordpress.android.fluxc.store.ThemeStore;
 
 import javax.inject.Inject;
 
 public class ReleaseStack_ThemeTestWPCom extends ReleaseStack_Base {
+    enum TestEvents {
+        NONE,
+    }
+
     @Inject SiteStore mSiteStore;
-    @Inject AccountStore mAccountStore;
+    @Inject ThemeStore mThemeStore;
+    private TestEvents mNextEvent;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        mReleaseStackAppComponent.inject(this);
+        // Register
+        init();
+        // Reset expected test event
+        mNextEvent = TestEvents.NONE;
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onThemesChanged(ThemeStore.OnThemesChanged event) {
+        mCountDownLatch.countDown();
     }
 }
