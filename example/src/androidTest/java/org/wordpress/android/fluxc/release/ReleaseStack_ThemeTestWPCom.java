37dfe5e8d3db7cc90d816e1ef71971bd1c8e0ab0
Tony Rankin <tonyr@tonyrankin.com>
2017-09-01 16:37:06 -0700

    Add test for fetching current theme on WP.com

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ThemeTestWPCom.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ThemeTestWPCom.java
@@ -1,8 +1,14 @@
 package org.wordpress.android.fluxc.release;
 
 import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 import org.wordpress.android.fluxc.TestUtils;
+import org.wordpress.android.fluxc.example.test.BuildConfig;
+import org.wordpress.android.fluxc.generated.AccountActionBuilder;
+import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
+import org.wordpress.android.fluxc.generated.SiteActionBuilder;
 import org.wordpress.android.fluxc.generated.ThemeActionBuilder;
+import org.wordpress.android.fluxc.model.SiteModel;
 import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.SiteStore;
 import org.wordpress.android.fluxc.store.ThemeStore;
@@ -16,6 +22,9 @@ public class ReleaseStack_ThemeTestWPCom extends ReleaseStack_Base {
     enum TestEvents {
         NONE,
         FETCHED_THEMES,
+        FETCHED_CURRENT_THEME,
+        SITE_CHANGED,
+        SITE_REMOVED
     }
 
     @Inject AccountStore mAccountStore;
@@ -38,7 +47,28 @@ public class ReleaseStack_ThemeTestWPCom extends ReleaseStack_Base {
         mCountDownLatch = new CountDownLatch(1);
         mNextEvent = TestEvents.FETCHED_THEMES;
         mDispatcher.dispatch(ThemeActionBuilder.newFetchWpComThemesAction());
+
+        // verify response received and WP themes list is not empty
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+        assertTrue(mThemeStore.getWpThemes().size() > 0);
+    }
+
+    public void testFetchCurrentTheme() throws InterruptedException {
+        // sign into a WP.com account
+        authenticateWPComAndFetchSites(BuildConfig.TEST_WPCOM_USERNAME_TEST1, BuildConfig.TEST_WPCOM_PASSWORD_TEST1);
+
+        // verify WP.com site is available
+        SiteModel wpComSite = getWPComSite();
+        assertNotNull(wpComSite);
+
+        // fetch current theme
+        mNextEvent = TestEvents.FETCHED_CURRENT_THEME;
+        mCountDownLatch = new CountDownLatch(1);
+        mDispatcher.dispatch(ThemeActionBuilder.newFetchCurrentThemeAction(wpComSite));
+
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        signOutWPCom();
     }
 
     @SuppressWarnings("unused")
@@ -47,10 +77,98 @@ public class ReleaseStack_ThemeTestWPCom extends ReleaseStack_Base {
         if (event.isError()) {
             throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
         }
+        mCountDownLatch.countDown();
+    }
 
-        if (mNextEvent == TestEvents.FETCHED_THEMES) {
-            assertTrue(mThemeStore.getWpThemes().size() > 0);
+    @SuppressWarnings("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onCurrentThemeFetched(ThemeStore.OnCurrentThemeFetched event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+
+        if (mNextEvent == TestEvents.FETCHED_CURRENT_THEME) {
+            assertNotNull(event.theme);
             mCountDownLatch.countDown();
         }
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onAuthenticationChanged(AccountStore.OnAuthenticationChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        mCountDownLatch.countDown();
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onAccountChanged(AccountStore.OnAccountChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        mCountDownLatch.countDown();
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onSiteChanged(SiteStore.OnSiteChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        mCountDownLatch.countDown();
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onSiteRemoved(SiteStore.OnSiteRemoved event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        assertEquals(TestEvents.SITE_REMOVED, mNextEvent);
+        mCountDownLatch.countDown();
+    }
+
+    private void authenticateWPComAndFetchSites(String username, String password) throws InterruptedException {
+        // Authenticate a test user (actual credentials declared in gradle.properties)
+        AccountStore.AuthenticatePayload payload = new AccountStore.AuthenticatePayload(username, password);
+        mCountDownLatch = new CountDownLatch(1);
+
+        // Correct user we should get an OnAuthenticationChanged message
+        mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateAction(payload));
+        // Wait for a network response / onChanged event
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        // Fetch account from REST API, and wait for OnAccountChanged event
+        mCountDownLatch = new CountDownLatch(1);
+        mDispatcher.dispatch(AccountActionBuilder.newFetchAccountAction());
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        // Fetch sites from REST API, and wait for onSiteChanged event
+        mCountDownLatch = new CountDownLatch(1);
+        mNextEvent = TestEvents.SITE_CHANGED;
+        mDispatcher.dispatch(SiteActionBuilder.newFetchSitesAction());
+
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+        assertTrue(mSiteStore.getSitesCount() > 0);
+    }
+
+    private void signOutWPCom() throws InterruptedException {
+        // Clear WP.com sites, and wait for OnSiteRemoved event
+        mCountDownLatch = new CountDownLatch(1);
+        mNextEvent = TestEvents.SITE_REMOVED;
+        mDispatcher.dispatch(SiteActionBuilder.newRemoveWpcomAndJetpackSitesAction());
+
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private SiteModel getWPComSite() {
+        for (SiteModel site : mSiteStore.getSites()) {
+            if (site.isWPCom()) {
+                return site;
+            }
+        }
+        return null;
     }
 }
