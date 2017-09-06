4ffbb8412f50f6224bbb3fba986f4a9cb027507e
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-09-06 12:51:11 +0300

    General field in request to pass more context

--- fluxc/src/main/java/org/wordpress/android/fluxc/RequestPayload.java
+++ fluxc/src/main/java/org/wordpress/android/fluxc/RequestPayload.java
@@ -6,6 +6,8 @@ public abstract class RequestPayload extends BasePayload {
     private static final AtomicLong GLOBAL_REQUEST_COUNTER = new AtomicLong(1);
     private final long mRequestId;
 
+    public Object extra;
+
     public RequestPayload() {
         mRequestId = GLOBAL_REQUEST_COUNTER.getAndIncrement();
     }
