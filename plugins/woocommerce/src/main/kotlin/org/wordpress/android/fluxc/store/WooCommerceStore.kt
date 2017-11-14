759d110611fe14bc4b716a5ae61f8d951488bc16
Alex Forcier <alex.forcier@automattic.com>
2017-11-14 10:55:02 -0500

    Support retrieving WooCommerce sites from WooCommerceStore

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WooCommerceStore.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WooCommerceStore.kt
@@ -1,10 +1,13 @@
 package org.wordpress.android.fluxc.store
 
+import com.wellsql.generated.SiteModelTable
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.action.WooCommerceAction
 import org.wordpress.android.fluxc.annotations.action.Action
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.persistence.SiteSqlUtils
 import org.wordpress.android.util.AppLog
 import org.wordpress.android.util.AppLog.T
 import javax.inject.Inject
@@ -21,4 +24,7 @@ class WooCommerceStore @Inject constructor(dispatcher: Dispatcher) : Store(dispa
         val actionType = action.type as? WooCommerceAction ?: return
         // TODO
     }
+
+    fun getWooCommerceSites(): MutableList<SiteModel> =
+            SiteSqlUtils.getSitesWith(SiteModelTable.HAS_WOO_COMMERCE, true).asModel
 }
