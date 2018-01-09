21bc7e6d7a191eeff424f527c00455a26ee9aee8
Oguz Kocer <oguz.kocer@automattic.com>
2018-01-09 15:51:01 -0500

    DotOrgPluginModel renamed to WPOrgPluginModel due to checkstyle issue

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_PluginInfoTest.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_PluginInfoTest.java
@@ -3,7 +3,7 @@ package org.wordpress.android.fluxc.release;
 import org.greenrobot.eventbus.Subscribe;
 import org.wordpress.android.fluxc.TestUtils;
 import org.wordpress.android.fluxc.generated.PluginActionBuilder;
-import org.wordpress.android.fluxc.model.DotOrgPluginModel;
+import org.wordpress.android.fluxc.model.WPOrgPluginModel;
 import org.wordpress.android.fluxc.store.PluginStore;
 import org.wordpress.android.fluxc.store.PluginStore.OnPluginInfoChanged;
 
@@ -50,7 +50,7 @@ public class ReleaseStack_PluginInfoTest extends ReleaseStack_Base {
         }
 
         assertEquals(TestEvents.PLUGIN_INFO_FETCHED, mNextEvent);
-        DotOrgPluginModel pluginInfo = mPluginStore.getPluginInfoBySlug(mSlug);
+        WPOrgPluginModel pluginInfo = mPluginStore.getPluginInfoBySlug(mSlug);
         assertNotNull(pluginInfo);
         mCountDownLatch.countDown();
     }
