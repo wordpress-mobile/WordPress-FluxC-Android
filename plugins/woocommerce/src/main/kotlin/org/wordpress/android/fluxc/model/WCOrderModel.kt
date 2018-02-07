904a9d39486b80df7f1f10d8db31b303759a4ae0
Alex Forcier <alex.forcier@automattic.com>
2018-02-07 13:31:08 +1100

    Add basic fields to order model

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCOrderModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCOrderModel.kt
@@ -9,7 +9,13 @@ import org.wordpress.android.fluxc.persistence.WellSqlConfig
 @Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
 data class WCOrderModel(@PrimaryKey @Column private var id: Int = 0) : Identifiable {
     @Column var localSiteId: Int = 0
-    @Column var remoteOrderId: Long? = 0
+    @Column var remoteOrderId: Long = 0
+    @Column var status: String = ""
+    @Column var currency: String = ""
+    @Column var dateCreated: String = "" // ISO 8601-formatted date in UTC, e.g. 1955-11-05T14:15:00Z
+    @Column var total: Float = 0F
+    @Column var billingFirstName: String = ""
+    @Column var billingLastName: String = ""
 
     override fun getId(): Int = id
 
