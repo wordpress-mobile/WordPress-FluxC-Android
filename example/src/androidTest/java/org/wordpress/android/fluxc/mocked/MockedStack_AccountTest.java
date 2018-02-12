7704baebe272d9efe7a6c65fcbd299c3bd0a1778
Alex Forcier <alex.forcier@automattic.com>
2018-02-12 19:46:45 +1100

    Ensure AccountStore is clear before mocked auth tests

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_AccountTest.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_AccountTest.java
@@ -3,9 +3,11 @@ package org.wordpress.android.fluxc.mocked;
 import org.greenrobot.eventbus.Subscribe;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.TestUtils;
+import org.wordpress.android.fluxc.generated.AccountActionBuilder;
 import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
 import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.AccountStore.AuthenticatePayload;
+import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged;
 import org.wordpress.android.fluxc.store.AccountStore.OnAuthenticationChanged;
 
 import java.util.concurrent.CountDownLatch;
@@ -41,6 +43,10 @@ public class MockedStack_AccountTest extends MockedStack_Base {
     }
 
     public void testAuthenticationOK() throws InterruptedException {
+        if (mAccountStore.hasAccessToken()) {
+            signOut();
+        }
+
         AuthenticatePayload payload = new AuthenticatePayload("test", "test");
         mIsError = false;
         // Correct user we should get an OnAuthenticationChanged message
@@ -50,6 +56,10 @@ public class MockedStack_AccountTest extends MockedStack_Base {
     }
 
     public void testAuthenticationKO() throws InterruptedException {
+        if (mAccountStore.hasAccessToken()) {
+            signOut();
+        }
+
         AuthenticatePayload payload = new AuthenticatePayload("error", "error");
         mIsError = true;
         // Correct user we should get an OnAuthenticationChanged message
@@ -58,9 +68,26 @@ public class MockedStack_AccountTest extends MockedStack_Base {
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 
+    @SuppressWarnings("unused")
     @Subscribe
     public void onAuthenticationChanged(OnAuthenticationChanged event) {
         assertEquals(mIsError, event.isError());
         mCountDownLatch.countDown();
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onAccountChanged(OnAccountChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Got unexpected error in OnAccountChanged: " + event.error.type);
+        }
+        mCountDownLatch.countDown();
+    }
+
+    private void signOut() throws InterruptedException {
+        mIsError = false;
+        mCountDownLatch = new CountDownLatch(2); // Wait for OnAuthenticationChanged and OnAccountChanged
+        mDispatcher.dispatch(AccountActionBuilder.newSignOutAction());
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 }
