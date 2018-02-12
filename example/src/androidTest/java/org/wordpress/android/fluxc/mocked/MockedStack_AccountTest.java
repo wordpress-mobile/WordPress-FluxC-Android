8c4cb652d00a1494800659c9b4c4edf64a22bb82
Alex Forcier <alex.forcier@automattic.com>
2018-02-12 19:45:10 +1100

    Wait for AUTHENTICATE to complete in mocked account tests

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_AccountTest.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_AccountTest.java
@@ -2,10 +2,14 @@ package org.wordpress.android.fluxc.mocked;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.wordpress.android.fluxc.Dispatcher;
+import org.wordpress.android.fluxc.TestUtils;
 import org.wordpress.android.fluxc.generated.AuthenticationActionBuilder;
 import org.wordpress.android.fluxc.store.AccountStore;
 import org.wordpress.android.fluxc.store.AccountStore.AuthenticatePayload;
 import org.wordpress.android.fluxc.store.AccountStore.OnAuthenticationChanged;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -16,7 +20,8 @@ public class MockedStack_AccountTest extends MockedStack_Base {
     @Inject Dispatcher mDispatcher;
     @Inject AccountStore mAccountStore;
 
-    public boolean mIsError;
+    private boolean mIsError;
+    private CountDownLatch mCountDownLatch;
 
     @Override
     protected void setUp() throws Exception {
@@ -35,22 +40,27 @@ public class MockedStack_AccountTest extends MockedStack_Base {
         super.tearDown();
     }
 
-    public void testAuthenticationOK() {
+    public void testAuthenticationOK() throws InterruptedException {
         AuthenticatePayload payload = new AuthenticatePayload("test", "test");
         mIsError = false;
         // Correct user we should get an OnAuthenticationChanged message
+        mCountDownLatch = new CountDownLatch(1);
         mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateAction(payload));
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 
-    public void testAuthenticationKO() {
+    public void testAuthenticationKO() throws InterruptedException {
         AuthenticatePayload payload = new AuthenticatePayload("error", "error");
         mIsError = true;
         // Correct user we should get an OnAuthenticationChanged message
+        mCountDownLatch = new CountDownLatch(1);
         mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateAction(payload));
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
     }
 
     @Subscribe
     public void onAuthenticationChanged(OnAuthenticationChanged event) {
         assertEquals(mIsError, event.isError());
+        mCountDownLatch.countDown();
     }
 }
