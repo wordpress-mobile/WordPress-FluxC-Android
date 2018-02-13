0b0f0f222455e98b1dd684491780ad95630fbcc6
Alex Forcier <alex.forcier@automattic.com>
2018-02-14 10:29:23 +1100

    Add OrderStatus constants

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
@@ -0,0 +1,12 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
+
+object OrderStatus {
+    // Standard core WooCommerce order statuses
+    const val PENDING = "pending"
+    const val ON_HOLD = "on-hold"
+    const val PROCESSING = "processing"
+    const val COMPLETED = "completed"
+    const val CANCELLED = "cancelled"
+    const val REFUNDED = "refunded"
+    const val FAILED = "failed"
+}
