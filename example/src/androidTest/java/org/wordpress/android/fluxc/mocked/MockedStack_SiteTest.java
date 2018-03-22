45b78a83aa20ce796cd0bd1b024fdb060d4d1f1b
Oguz Kocer <oguz.kocer@automattic.com>
2018-03-22 16:56:38 -0400

    Add test account and site to MockedStack_SiteTest

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_SiteTest.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_SiteTest.java
@@ -1,13 +1,20 @@
 package org.wordpress.android.fluxc.mocked;
 
+import android.support.annotation.NonNull;
+
 import org.greenrobot.eventbus.Subscribe;
 import org.junit.Test;
 import org.wordpress.android.fluxc.Dispatcher;
 import org.wordpress.android.fluxc.TestUtils;
+import org.wordpress.android.fluxc.generated.AccountActionBuilder;
 import org.wordpress.android.fluxc.generated.SiteActionBuilder;
+import org.wordpress.android.fluxc.model.AccountModel;
 import org.wordpress.android.fluxc.model.SiteModel;
+import org.wordpress.android.fluxc.store.AccountStore;
+import org.wordpress.android.fluxc.store.AccountStore.OnAccountChanged;
 import org.wordpress.android.fluxc.store.SiteStore;
 import org.wordpress.android.fluxc.store.SiteStore.OnAutomatedTransferEligibilityChecked;
+import org.wordpress.android.fluxc.store.SiteStore.OnSiteChanged;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -21,10 +28,13 @@ import static org.junit.Assert.assertTrue;
 public class MockedStack_SiteTest extends MockedStack_Base {
     @Inject Dispatcher mDispatcher;
     @Inject SiteStore mSiteStore;
+    @Inject AccountStore mAccountStore;
 
     enum TestEvents {
         NONE,
-        ELIGIBLE_FOR_AUTOMATED_TRANSFER
+        ACCOUNT_CHANGED,
+        ELIGIBLE_FOR_AUTOMATED_TRANSFER,
+        UPDATE_SITE
     }
 
     private TestEvents mNextEvent;
@@ -43,12 +53,23 @@ public class MockedStack_SiteTest extends MockedStack_Base {
 
     @Test
     public void testEligibleForAutomatedTransfer() throws InterruptedException {
-        SiteModel site = new SiteModel();
-        site.setSiteId(322);
+        // Create dummy site
+        SiteModel site = createAccountAndLocalTestSite();
+
         mNextEvent = TestEvents.ELIGIBLE_FOR_AUTOMATED_TRANSFER;
         mDispatcher.dispatch(SiteActionBuilder.newCheckAutomatedTransferEligibilityAction(site));
         mCountDownLatch = new CountDownLatch(1);
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onAccountChanged(OnAccountChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        assertEquals(mNextEvent, TestEvents.ACCOUNT_CHANGED);
+        mCountDownLatch.countDown();
     }
 
     @SuppressWarnings("unused")
@@ -61,5 +82,37 @@ public class MockedStack_SiteTest extends MockedStack_Base {
         assertNotNull(event.site);
         assertTrue(event.site.isEligibleForAutomatedTransfer());
         mCountDownLatch.countDown();
+    }
+
+    @SuppressWarnings("unused")
+    @Subscribe
+    public void onSiteChanged(OnSiteChanged event) {
+        if (event.isError()) {
+            throw new AssertionError("Unexpected error occurred with type: " + event.error.type);
+        }
+        assertEquals(mNextEvent, TestEvents.UPDATE_SITE);
+        assertEquals(event.rowsAffected, 1);
+        mCountDownLatch.countDown();
+    }
+
+    private @NonNull SiteModel createAccountAndLocalTestSite() throws InterruptedException {
+        AccountModel account = new AccountModel();
+        account.setUserId(478);
+        mNextEvent = TestEvents.ACCOUNT_CHANGED;
+        mDispatcher.dispatch(AccountActionBuilder.newUpdateAccountAction(account));
+        mCountDownLatch = new CountDownLatch(1);
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        SiteModel site = new SiteModel();
+        site.setSiteId(322);
+        site.setIsWPCom(true);
+        mNextEvent = TestEvents.UPDATE_SITE;
+        mDispatcher.dispatch(SiteActionBuilder.newUpdateSiteAction(site));
+        mCountDownLatch = new CountDownLatch(1);
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+
+        SiteModel siteFromStore = mSiteStore.getSiteBySiteId(site.getSiteId());
+        assertNotNull(siteFromStore);
+        return siteFromStore;
     }
 }
