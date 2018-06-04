866b13b64a20932a58d3f61858e9ef36d6b27ea4
Nick Bradbury <nick.bradbury@gmail.com>
2018-06-04 14:22:51 -0400

    Added second test

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ReaderTest.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ReaderTest.java
@@ -6,17 +6,20 @@ import org.greenrobot.eventbus.Subscribe;
 import org.junit.Test;
 import org.wordpress.android.fluxc.TestUtils;
 import org.wordpress.android.fluxc.generated.ReaderActionBuilder;
+import org.wordpress.android.fluxc.model.ReaderFeedModel;
 import org.wordpress.android.fluxc.network.rest.wpcom.reader.ReaderRestClient;
 import org.wordpress.android.fluxc.store.ReaderStore;
 import org.wordpress.android.fluxc.store.ReaderStore.OnReaderSitesSearched;
 import org.wordpress.android.fluxc.store.ReaderStore.ReaderSearchSitesPayload;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ReleaseStack_ReaderTest extends ReleaseStack_Base {
@@ -24,8 +27,11 @@ public class ReleaseStack_ReaderTest extends ReleaseStack_Base {
 
     private enum TestEvents {
         NONE,
-        READER_SEARCH_SITES
+        READER_SEARCH_SITES,
+        READER_SEARCH_SITES_WITH_OFFSET
     }
+
+    private List<ReaderFeedModel> mFirstSearchResults;
 
     private static final String SEARCH_TERM = "dogs";
     private TestEvents mNextEvent;
@@ -44,7 +50,9 @@ public class ReleaseStack_ReaderTest extends ReleaseStack_Base {
     public void testReaderSearchSites() throws InterruptedException {
         mNextEvent = TestEvents.READER_SEARCH_SITES;
         searchReaderSites(SEARCH_TERM, 0);
-        // TODO: search with offset
+
+        mNextEvent = TestEvents.READER_SEARCH_SITES_WITH_OFFSET;
+        searchReaderSites(SEARCH_TERM, ReaderRestClient.NUM_SEARCH_RESULTS);
     }
 
     private void searchReaderSites(@NonNull String searchTerm, int offset) throws InterruptedException {
@@ -52,6 +60,15 @@ public class ReleaseStack_ReaderTest extends ReleaseStack_Base {
         mCountDownLatch = new CountDownLatch(1);
         mDispatcher.dispatch(ReaderActionBuilder.newReaderSearchSitesAction(payload));
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
+    }
+
+    private boolean isInFirstSetOfResults(@NonNull ReaderFeedModel feed) {
+        for (ReaderFeedModel otherFeed : mFirstSearchResults) {
+            if (otherFeed.getFeedId() == feed.getFeedId()) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @SuppressWarnings("unused")
@@ -62,9 +79,23 @@ public class ReleaseStack_ReaderTest extends ReleaseStack_Base {
                                      + event.error.type);
         }
 
-        assertEquals(mNextEvent, TestEvents.READER_SEARCH_SITES);
-        assertEquals(event.feeds.size(), ReaderRestClient.NUM_SEARCH_RESULTS);
-        assertTrue(event.canLoadMore);
+        if (mNextEvent == TestEvents.READER_SEARCH_SITES) {
+            mFirstSearchResults = event.feeds;
+            assertEquals(event.feeds.size(), ReaderRestClient.NUM_SEARCH_RESULTS);
+            assertTrue(event.canLoadMore);
+        } else if (mNextEvent == TestEvents.READER_SEARCH_SITES_WITH_OFFSET) {
+            List<ReaderFeedModel> feeds = event.feeds;
+            boolean areThereDups = false;
+            for (ReaderFeedModel feed : event.feeds) {
+                if (isInFirstSetOfResults(feed)) {
+                    areThereDups = true;
+                    break;
+                }
+            }
+            assertFalse(areThereDups);
+        } else {
+            throw new AssertionError("Wrong event received");
+        }
 
         mCountDownLatch.countDown();
     }
