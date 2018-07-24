f3a980ad242d4586851a87a10545c4445ecb22e8
Amanda Riu <amanda.riu@automattic.com>
2018-07-24 15:00:56 -0600

    Add instrumented test for fetching orders filtered by a single status

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_WCOrderTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_WCOrderTest.kt
@@ -64,6 +64,22 @@ class ReleaseStack_WCOrderTest : ReleaseStack_WCBase() {
 
     @Throws(InterruptedException::class)
     @Test
+    fun testFetchOrdersByStatus() {
+        nextEvent = TestEvent.FETCHED_ORDERS
+        mCountDownLatch = CountDownLatch(1)
+        val statusFilter = "completed"
+
+        mDispatcher.dispatch(WCOrderActionBuilder.newFetchOrdersAction(FetchOrdersPayload(sSite, false, statusFilter)))
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS))
+
+        val firstFetchOrders = orderStore.getOrdersForSite(sSite)
+        val isValid = firstFetchOrders.stream().allMatch { it -> it.status == statusFilter }
+        assertTrue(firstFetchOrders.isNotEmpty() &&
+                firstFetchOrders.size <= WCOrderStore.NUM_ORDERS_PER_FETCH && isValid)
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
     fun testFetchOrderNotes() {
         // Grab a list of orders
         nextEvent = TestEvent.FETCHED_ORDERS
