d9189cb7f018dadc1c87316ec0679cebdc2ce5b6
Oguz Kocer <oguz.kocer@automattic.com>
2018-07-27 18:38:46 -0400

    Mock implementation of PostListActivity

--- example/src/main/java/org/wordpress/android/fluxc/example/PostListActivity.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/PostListActivity.kt
@@ -4,7 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.post_list_activity.*
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.PostStore
@@ -23,10 +32,20 @@ class PostListActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.post_list_activity)
-
         site = siteStore.getSiteByLocalId(intent.getIntExtra(LOCAL_SITE_ID, 0))
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recycler.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recycler.adapter = PostListAdapter(this)
+
+        swipeToRefresh.setOnRefreshListener {
+            swipeToRefresh.isRefreshing = false
+        }
     }
 
     companion object {
@@ -34,6 +53,28 @@ class PostListActivity: AppCompatActivity() {
             val intent = Intent(context, PostListActivity::class.java)
             intent.putExtra(LOCAL_SITE_ID, localSiteId)
             return intent
+        }
+    }
+
+    private class PostListAdapter(context: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        private val layoutInflater = LayoutInflater.from(context)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = layoutInflater.inflate(R.layout.post_list_row, parent, false)
+            return PostViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 5
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val postHolder = holder as PostViewHolder
+            postHolder.postTitle.text = "Some title"
+        }
+
+        private class PostViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val postTitle: TextView = itemView.findViewById(R.id.post_list_row_post_title)
         }
     }
 }
