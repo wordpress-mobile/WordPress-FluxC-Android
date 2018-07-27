53b6e09e4173dfeaa0f98c5285049edc0c625878
Amanda Riu <amanda.riu@automattic.com>
2018-07-27 16:45:51 -0600

    Convert OrderStatus from static object to an Enum Class for more flexibility

--- example/src/main/java/org/wordpress/android/fluxc/example/WooCommerceFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/WooCommerceFragment.kt
@@ -101,7 +101,8 @@ class WooCommerceFragment : Fragment() {
             getFirstWCSite()?.let { site ->
                 prependToLog("Submitting request to fetch only completed orders from the api")
                 pendingFetchCompletedOrders = true
-                val payload = FetchOrdersPayload(site, loadMore = false, statusFilter = OrderStatus.COMPLETED)
+                val payload = FetchOrdersPayload(
+                        site, loadMore = false, statusFilter = OrderStatus.COMPLETED.toString())
                 dispatcher.dispatch(WCOrderActionBuilder.newFetchOrdersAction(payload))
             }
         }
--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
@@ -1,12 +1,40 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
 
-object OrderStatus {
+enum class OrderStatus {
     // Standard core WooCommerce order statuses
-    const val PENDING = "pending"
-    const val ON_HOLD = "on-hold"
-    const val PROCESSING = "processing"
-    const val COMPLETED = "completed"
-    const val CANCELLED = "cancelled"
-    const val REFUNDED = "refunded"
-    const val FAILED = "failed"
+    PENDING {
+        override fun toString(): String {
+            return "pending"
+        }
+    },
+    ON_HOLD {
+        override fun toString(): String {
+            return "on-hold"
+        }
+    },
+    PROCESSING {
+        override fun toString(): String {
+            return "processing"
+        }
+    },
+    COMPLETED {
+        override fun toString(): String {
+            return "completed"
+        }
+    },
+    CANCELLED {
+        override fun toString(): String {
+            return "cancelled"
+        }
+    },
+    REFUNDED {
+        override fun toString(): String {
+            return "refunded"
+        }
+    },
+    FAILED {
+        override fun toString(): String {
+            return "failed"
+        }
+    }
 }
