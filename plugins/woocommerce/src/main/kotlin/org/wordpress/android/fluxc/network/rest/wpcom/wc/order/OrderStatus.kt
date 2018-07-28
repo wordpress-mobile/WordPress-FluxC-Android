4bbd1102169c3580d6d2df3657e0a2b6a8f953ee
Amanda Riu <amanda.riu@automattic.com>
2018-07-27 19:42:35 -0600

    Add indexes for sorting and mapping

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
@@ -3,22 +3,28 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
 /**
  * Standard Core WooCommerce order statuses
  */
-enum class OrderStatus(val label: String, val value: String) {
-    ALL("All", "any"),
-    PENDING("Pending Payment", "pending"),
-    ON_HOLD("On-Hold", "on-hold"),
-    PROCESSING("Processing", "processing"),
-    COMPLETED("Completed", "completed"),
-    CANCELLED("Cancelled", "cancelled"),
-    REFUNDED("Refunded", "refunded"),
-    FAILED("Failed", "failed");
+enum class OrderStatus(val index: Int, val label: String, val value: String) {
+    ALL(0, "All", "any"),
+    PENDING(1, "Pending Payment", "pending"),
+    PROCESSING(2, "Processing", "processing"),
+    ON_HOLD(3, "On-Hold", "on-hold"),
+    COMPLETED(4, "Completed", "completed"),
+    CANCELLED(5, "Cancelled", "cancelled"),
+    REFUNDED(6, "Refunded", "refunded"),
+    FAILED(7, "Failed", "failed");
 
     companion object {
-        private val map = OrderStatus.values().associateBy(OrderStatus::label);
+        private val labelMap = OrderStatus.values().associateBy(OrderStatus::label);
+        private val indexMap = OrderStatus.values().associateBy(OrderStatus::index)
 
         /**
-         * Convert the label value back into an OrderStatus object
+         * Convert the label value back into the associated OrderStatus object
          */
-        fun fromLabel(label: String) = map[label]
+        fun fromLabel(label: String) = labelMap[label]
+
+        /**
+         * Convert the index back into the associated OrderStatus object
+         */
+        fun fromIndex(index: Int) = indexMap[index]
     }
 }
