4db867e24a75816c7c9fd679909493134d9c46c3
Amanda Riu <amanda.riu@automattic.com>
2018-07-27 19:23:54 -0600

    Add an "ANY" type for OrderStatus as well as a way to do a reverse lookup by the order status label

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
@@ -4,6 +4,7 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
  * Standard Core WooCommerce order statuses
  */
 enum class OrderStatus(val label: String, val value: String) {
+    ALL("All", "any"),
     PENDING("Pending Payment", "pending"),
     ON_HOLD("On-Hold", "on-hold"),
     PROCESSING("Processing", "processing"),
@@ -11,4 +12,13 @@ enum class OrderStatus(val label: String, val value: String) {
     CANCELLED("Cancelled", "cancelled"),
     REFUNDED("Refunded", "refunded"),
     FAILED("Failed", "failed");
+
+    companion object {
+        private val map = OrderStatus.values().associateBy(OrderStatus::label);
+
+        /**
+         * Convert the label value back into an OrderStatus object
+         */
+        fun fromLabel(label: String) = map[label]
+    }
 }
