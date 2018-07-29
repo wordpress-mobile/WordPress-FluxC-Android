4a7fad5ab115b1e5c4af9f09b2a8a3d04ea23abe
Amanda Riu <amanda.riu@automattic.com>
2018-07-29 14:56:07 -0600

    Remove unused reverse lookup by order status index

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderStatus.kt
@@ -3,30 +3,24 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
 /**
  * Standard Core WooCommerce order statuses
  */
-enum class OrderStatus(val index: Int, val label: String, val value: String) {
-    ALL(0, "All", "any"),
-    PENDING(1, "Pending Payment", "pending"),
-    PROCESSING(2, "Processing", "processing"),
-    ON_HOLD(3, "On-Hold", "on-hold"),
-    COMPLETED(4, "Completed", "completed"),
-    CANCELLED(5, "Cancelled", "cancelled"),
-    REFUNDED(6, "Refunded", "refunded"),
-    FAILED(7, "Failed", "failed");
+enum class OrderStatus(val label: String, val value: String) {
+    ALL("All", "any"),
+    PENDING("Pending Payment", "pending"),
+    PROCESSING("Processing", "processing"),
+    ON_HOLD("On-Hold", "on-hold"),
+    COMPLETED("Completed", "completed"),
+    CANCELLED("Cancelled", "cancelled"),
+    REFUNDED("Refunded", "refunded"),
+    FAILED("Failed", "failed");
 
     companion object {
         private val labelMap = OrderStatus.values().associateBy(OrderStatus::label)
-        private val indexMap = OrderStatus.values().associateBy(OrderStatus::index)
         private val valueMap = OrderStatus.values().associateBy(OrderStatus::value)
 
         /**
          * Convert the label value back into the associated OrderStatus object
          */
         fun fromLabel(label: String) = labelMap[label]
-
-        /**
-         * Convert the index back into the associated OrderStatus object
-         */
-        fun fromIndex(index: Int) = indexMap[index]
 
         /**
          * Convert the base value into the associated OrderStatus object
