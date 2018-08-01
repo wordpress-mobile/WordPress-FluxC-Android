e715967305593e38097756f3a1650a0669850a3b
Amanda Riu <amanda.riu@automattic.com>
2018-08-01 15:22:25 -0500

    Removed test suites

--- example/src/test/java/org/wordpress/android/fluxc/wc/order/OrdersTestSuite.kt
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.wc.order
-
-import org.junit.runner.RunWith
-import org.junit.runners.Suite
-import org.junit.runners.Suite.SuiteClasses
-
-@RunWith(Suite::class)
-@SuiteClasses(OrderSqlUtilsTest::class, WCOrderModelTest::class, WCOrderStoreTest::class)
-class OrdersTestSuite
--- example/src/test/java/org/wordpress/android/fluxc/wc/stats/StatsTestSuite.kt
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.wc.stats
-
-import org.junit.runner.RunWith
-import org.junit.runners.Suite
-import org.junit.runners.Suite.SuiteClasses
-
-@RunWith(Suite::class)
-@SuiteClasses(WCOrderStatsModelTest::class, WCStatsStoreTest::class)
-class StatsTestSuite
