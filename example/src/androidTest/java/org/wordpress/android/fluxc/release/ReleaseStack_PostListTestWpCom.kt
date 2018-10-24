e7c34bf51c4746317177fe989a0b5b02de856725
Oguz Kocer <oguz.kocer@automattic.com>
2018-10-24 16:06:17 -0400

    Adds connected tests for more complicated PostListDescriptors

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_PostListTestWpCom.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_PostListTestWpCom.kt
@@ -1,7 +1,11 @@
 package org.wordpress.android.fluxc.release
 
 import org.junit.Test
+import org.wordpress.android.fluxc.model.list.ListOrder
+import org.wordpress.android.fluxc.model.list.PostListDescriptor
 import org.wordpress.android.fluxc.model.list.PostListDescriptor.PostListDescriptorForRestSite
+import org.wordpress.android.fluxc.model.list.PostListOrderBy
+import org.wordpress.android.fluxc.model.post.PostStatus
 
 class ReleaseStack_PostListTestWpCom : ReleaseStack_WPComBase() {
     @Throws(Exception::class)
@@ -13,17 +17,54 @@ class ReleaseStack_PostListTestWpCom : ReleaseStack_WPComBase() {
 
     @Throws(InterruptedException::class)
     @Test
-    fun fetchFirstPage() {
-        val postListConnectedTestHelper = PostListConnectedTestHelper(mDispatcher, mReleaseStackAppComponent)
-        val postListDescriptor = PostListDescriptorForRestSite(sSite)
-        postListConnectedTestHelper.fetchFirstPageHelper(postListDescriptor)
+    fun testFetchFirstPageForDefaultDescriptor() {
+        fetchFirstPage(PostListDescriptorForRestSite(sSite))
     }
 
     @Throws(InterruptedException::class)
     @Test
-    fun loadMore() {
+    fun testLoadMoreForDefaultDescriptor() {
+        loadMore(PostListDescriptorForRestSite(sSite, pageSize = 10))
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchPublishedPosts() {
+        fetchFirstPage(PostListDescriptorForRestSite(sSite, statusList = listOf(PostStatus.PUBLISHED)))
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchDraftsOrderedByTitle() {
+        fetchFirstPage(
+                PostListDescriptorForRestSite(
+                        site = sSite,
+                        statusList = listOf(PostStatus.DRAFT),
+                        orderBy = PostListOrderBy.TITLE
+                )
+        )
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchTrashedPostsOrderedByIdInDescendingOrder() {
+        fetchFirstPage(
+                PostListDescriptorForRestSite(
+                        site = sSite,
+                        statusList = listOf(PostStatus.TRASHED),
+                        orderBy = PostListOrderBy.ID,
+                        order = ListOrder.DESC
+                )
+        )
+    }
+
+    private fun fetchFirstPage(listDescriptor: PostListDescriptor) {
         val postListConnectedTestHelper = PostListConnectedTestHelper(mDispatcher, mReleaseStackAppComponent)
-        val postListDescriptor = PostListDescriptorForRestSite(sSite, pageSize = 10)
-        postListConnectedTestHelper.loadMoreHelper(postListDescriptor)
+        postListConnectedTestHelper.fetchFirstPageHelper(listDescriptor)
+    }
+
+    private fun loadMore(listDescriptor: PostListDescriptor) {
+        val postListConnectedTestHelper = PostListConnectedTestHelper(mDispatcher, mReleaseStackAppComponent)
+        postListConnectedTestHelper.loadMoreHelper(listDescriptor)
     }
 }
--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_PostListTestXMLRPC.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_PostListTestXMLRPC.kt
@@ -1,7 +1,11 @@
 package org.wordpress.android.fluxc.release
 
 import org.junit.Test
+import org.wordpress.android.fluxc.model.list.ListOrder
+import org.wordpress.android.fluxc.model.list.PostListDescriptor
 import org.wordpress.android.fluxc.model.list.PostListDescriptor.PostListDescriptorForXmlRpcSite
+import org.wordpress.android.fluxc.model.list.PostListOrderBy
+import org.wordpress.android.fluxc.model.post.PostStatus
 
 class ReleaseStack_PostListTestXMLRPC : ReleaseStack_XMLRPCBase() {
     @Throws(Exception::class)
@@ -12,17 +16,54 @@ class ReleaseStack_PostListTestXMLRPC : ReleaseStack_XMLRPCBase() {
 
     @Throws(InterruptedException::class)
     @Test
-    fun fetchFirstPage() {
-        val postListConnectedTestHelper = PostListConnectedTestHelper(mDispatcher, mReleaseStackAppComponent)
-        val postListDescriptor = PostListDescriptorForXmlRpcSite(sSite)
-        postListConnectedTestHelper.fetchFirstPageHelper(postListDescriptor)
+    fun testFetchFirstPageForDefaultDescriptor() {
+        fetchFirstPage(PostListDescriptorForXmlRpcSite(sSite))
     }
 
     @Throws(InterruptedException::class)
     @Test
-    fun loadMore() {
+    fun testLoadMoreForDefaultDescriptor() {
+        loadMore(PostListDescriptorForXmlRpcSite(sSite, pageSize = 10))
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchPublishedPosts() {
+        fetchFirstPage(PostListDescriptorForXmlRpcSite(sSite, statusList = listOf(PostStatus.PUBLISHED)))
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchDraftsOrderedByTitle() {
+        fetchFirstPage(
+                PostListDescriptorForXmlRpcSite(
+                        site = sSite,
+                        statusList = listOf(PostStatus.DRAFT),
+                        orderBy = PostListOrderBy.TITLE
+                )
+        )
+    }
+
+    @Throws(InterruptedException::class)
+    @Test
+    fun testFetchTrashedPostsOrderedByIdInDescendingOrder() {
+        fetchFirstPage(
+                PostListDescriptorForXmlRpcSite(
+                        site = sSite,
+                        statusList = listOf(PostStatus.TRASHED),
+                        orderBy = PostListOrderBy.ID,
+                        order = ListOrder.DESC
+                )
+        )
+    }
+
+    private fun fetchFirstPage(listDescriptor: PostListDescriptor) {
         val postListConnectedTestHelper = PostListConnectedTestHelper(mDispatcher, mReleaseStackAppComponent)
-        val postListDescriptor = PostListDescriptorForXmlRpcSite(sSite, pageSize = 10)
-        postListConnectedTestHelper.loadMoreHelper(postListDescriptor)
+        postListConnectedTestHelper.fetchFirstPageHelper(listDescriptor)
+    }
+
+    private fun loadMore(listDescriptor: PostListDescriptor) {
+        val postListConnectedTestHelper = PostListConnectedTestHelper(mDispatcher, mReleaseStackAppComponent)
+        postListConnectedTestHelper.loadMoreHelper(listDescriptor)
     }
 }
