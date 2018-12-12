c49c06a9199bcbba2c3b9c8dea99b350721f48f3
Oguz Kocer <oguz.kocer@automattic.com>
2018-12-12 18:28:17 -0500

    Adds fetch verticals connected test

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_VerticalTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_VerticalTest.kt
@@ -9,10 +9,13 @@ import org.wordpress.android.fluxc.TestUtils
 import org.wordpress.android.fluxc.generated.VerticalActionBuilder
 import org.wordpress.android.fluxc.release.ReleaseStack_VerticalTest.TestEvents.SEGMENTS_FETCHED
 import org.wordpress.android.fluxc.release.ReleaseStack_VerticalTest.TestEvents.SEGMENT_PROMPT_FETCHED
+import org.wordpress.android.fluxc.release.ReleaseStack_VerticalTest.TestEvents.VERTICALS_FETCHED
 import org.wordpress.android.fluxc.store.VerticalStore
 import org.wordpress.android.fluxc.store.VerticalStore.FetchSegmentPromptPayload
+import org.wordpress.android.fluxc.store.VerticalStore.FetchVerticalsPayload
 import org.wordpress.android.fluxc.store.VerticalStore.OnSegmentPromptFetched
 import org.wordpress.android.fluxc.store.VerticalStore.OnSegmentsFetched
+import org.wordpress.android.fluxc.store.VerticalStore.OnVerticalsFetched
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -23,6 +26,7 @@ import javax.inject.Inject
  * If these tests ever start to fail because of this ID, we can simply change it to a different value.
  */
 private const val SEGMENT_ID_TO_TEST = 101L
+private const val FETCH_VERTICALS_SEARCH_QUERY = "restaurant"
 
 /**
  * Tests with real credentials on real servers using the full release stack (no mock)
@@ -36,7 +40,8 @@ class ReleaseStack_VerticalTest : ReleaseStack_Base() {
     internal enum class TestEvents {
         NONE,
         SEGMENTS_FETCHED,
-        SEGMENT_PROMPT_FETCHED
+        SEGMENT_PROMPT_FETCHED,
+        VERTICALS_FETCHED
     }
 
     @Throws(Exception::class)
@@ -68,6 +73,16 @@ class ReleaseStack_VerticalTest : ReleaseStack_Base() {
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS))
     }
 
+    @Test
+    fun testFetchVerticals() {
+        nextEvent = VERTICALS_FETCHED
+        mCountDownLatch = CountDownLatch(1)
+        val payload = FetchVerticalsPayload(searchQuery = FETCH_VERTICALS_SEARCH_QUERY)
+        mDispatcher.dispatch(VerticalActionBuilder.newFetchVerticalsAction(payload))
+
+        assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS))
+    }
+
     @Subscribe
     @Suppress("unused")
     fun onSegmentsFetched(event: OnSegmentsFetched) {
@@ -87,6 +102,17 @@ class ReleaseStack_VerticalTest : ReleaseStack_Base() {
         }
         assertEquals(TestEvents.SEGMENT_PROMPT_FETCHED, nextEvent)
         assertNotNull(event.prompt)
+        mCountDownLatch.countDown()
+    }
+
+    @Subscribe
+    @Suppress("unused")
+    fun onVerticalsFetched(event: OnVerticalsFetched) {
+         if (event.isError) {
+            throw AssertionError("Unexpected error occurred with type: " + event.error.type)
+        }
+        assertEquals(TestEvents.VERTICALS_FETCHED, nextEvent)
+        assertTrue(event.verticalList.isNotEmpty())
         mCountDownLatch.countDown()
     }
 }
