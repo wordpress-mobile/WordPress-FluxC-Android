cd038702ef738f181ee2a4616f96741199e49ec1
Alex Forcier <alex.forcier@automattic.com>
2019-01-29 09:45:30 -0500

    Add overall test for Woo currency formatting

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCBaseStoreTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCBaseStoreTest.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.fluxc.mocked
 
 import android.os.Build
+import com.yarolegovich.wellsql.WellSql
 import org.greenrobot.eventbus.Subscribe
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNull
@@ -12,9 +13,12 @@ import org.wordpress.android.fluxc.TestUtils
 import org.wordpress.android.fluxc.action.WCCoreAction
 import org.wordpress.android.fluxc.annotations.action.Action
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.WCSettingsModel
 import org.wordpress.android.fluxc.model.WCSettingsModel.CurrencyPosition
 import org.wordpress.android.fluxc.module.ResponseMockingInterceptor
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooCommerceRestClient
+import org.wordpress.android.fluxc.persistence.WCSettingsSqlUtils
+import org.wordpress.android.fluxc.persistence.WCSettingsSqlUtils.WCSettingsBuilder
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import org.wordpress.android.fluxc.store.WooCommerceStore.FetchApiVersionResponsePayload
 import org.wordpress.android.fluxc.store.WooCommerceStore.FetchWCSiteSettingsResponsePayload
@@ -31,6 +35,7 @@ import kotlin.properties.Delegates.notNull
  */
 class MockedStack_WCBaseStoreTest : MockedStack_Base() {
     @Inject internal lateinit var wcRestClient: WooCommerceRestClient
+    @Inject internal lateinit var wooCommerceStore: WooCommerceStore
     @Inject internal lateinit var dispatcher: Dispatcher
 
     @Inject internal lateinit var interceptor: ResponseMockingInterceptor
@@ -114,6 +119,130 @@ class MockedStack_WCBaseStoreTest : MockedStack_Base() {
             assertEquals("\$CA", WCCurrencyUtils.getLocalizedCurrencySymbolForCode("CAD", localeFrFR))
             assertEquals("€", WCCurrencyUtils.getLocalizedCurrencySymbolForCode("EUR", localeFrFR))
             assertEquals("JPY", WCCurrencyUtils.getLocalizedCurrencySymbolForCode("JPY", localeFrFR))
+        }
+    }
+
+    @Test
+    fun testFormatCurrencyForDisplay() {
+        assumeTrue(
+                "Requires API 23 or higher due to localized currency values differing on older versions",
+                Build.VERSION.SDK_INT >= 23
+        )
+
+        // Override device locale and use en_US so currency symbols can be predicted
+        TestUtils.updateLocale(mAppContext, Locale("en", "US"))
+
+        // -- Site using CAD
+        val cadSettings = WCSettingsModel(
+                localSiteId = siteModel.id,
+                currencyCode = "CAD",
+                currencyPosition = CurrencyPosition.LEFT,
+                currencyThousandSeparator = ",",
+                currencyDecimalSeparator = ".",
+                currencyDecimalNumber = 2)
+        WCSettingsSqlUtils.insertOrUpdateSettings(cadSettings)
+
+        with(wooCommerceStore) {
+            val formattedCurrencyDouble = formatCurrencyForDisplay(1234.12, siteModel, "CAD", true)
+            assertEquals("CA$1,234.12", formattedCurrencyDouble)
+
+            val formattedCurrencyString = formatCurrencyForDisplay("1234.12", siteModel, "CAD", true)
+            assertEquals("CA$1,234.12", formattedCurrencyString)
+
+            val formattedCurrencyPretty = formatCurrencyForDisplay("1.2k", siteModel, "CAD", false)
+            assertEquals("CA$1.2k", formattedCurrencyPretty)
+
+            val formattedCurrencyNegative = formatCurrencyForDisplay(-1234.12, siteModel, "CAD", true)
+            assertEquals("-CA$1,234.12", formattedCurrencyNegative)
+
+            val formattedCurrencyUseSiteCurrency = formatCurrencyForDisplay(1234.12, siteModel, null, true)
+            assertEquals("CA$1,234.12", formattedCurrencyUseSiteCurrency)
+
+            val formattedCurrencyDifferentCurrency = formatCurrencyForDisplay(1234.12, siteModel, "EUR", true)
+            assertEquals("€1,234.12", formattedCurrencyDifferentCurrency)
+        }
+
+        // -- Site using EUR
+        val eurSettings = WCSettingsModel(
+                localSiteId = siteModel.id,
+                currencyCode = "EUR",
+                currencyPosition = CurrencyPosition.RIGHT_SPACE,
+                currencyThousandSeparator = ".",
+                currencyDecimalSeparator = ",",
+                currencyDecimalNumber = 2)
+        WCSettingsSqlUtils.insertOrUpdateSettings(eurSettings)
+
+        with(wooCommerceStore) {
+            val formattedCurrencyDouble = formatCurrencyForDisplay(1234.12, siteModel, "EUR", true)
+            assertEquals("1.234,12 €", formattedCurrencyDouble)
+
+            val formattedCurrencyString = formatCurrencyForDisplay("1234.12", siteModel, "EUR", true)
+            assertEquals("1.234,12 €", formattedCurrencyString)
+
+            val formattedCurrencyPretty = formatCurrencyForDisplay("1.2k", siteModel, "EUR", false)
+            assertEquals("1.2k €", formattedCurrencyPretty)
+
+            val formattedCurrencyNegative = formatCurrencyForDisplay(-1234.12, siteModel, "EUR", true)
+            assertEquals("-1.234,12 €", formattedCurrencyNegative)
+
+            val formattedCurrencyUseSiteCurrency = formatCurrencyForDisplay(1234.12, siteModel, null, true)
+            assertEquals("1.234,12 €", formattedCurrencyUseSiteCurrency)
+
+            val formattedCurrencyDifferentCurrency = formatCurrencyForDisplay(1234.12, siteModel, "USD", true)
+            assertEquals("1.234,12 \$", formattedCurrencyDifferentCurrency)
+        }
+
+        // -- Site using JPY
+        val jpySettings = WCSettingsModel(
+                localSiteId = siteModel.id,
+                currencyCode = "JPY",
+                currencyPosition = CurrencyPosition.LEFT,
+                currencyThousandSeparator = "",
+                currencyDecimalSeparator = "",
+                currencyDecimalNumber = 0)
+        WCSettingsSqlUtils.insertOrUpdateSettings(jpySettings)
+
+        with(wooCommerceStore) {
+            val formattedCurrencyDouble = formatCurrencyForDisplay(1234.0, siteModel, "JPY", true)
+            assertEquals("¥1234", formattedCurrencyDouble)
+
+            val formattedCurrencyString = formatCurrencyForDisplay("1234", siteModel, "JPY", true)
+            assertEquals("¥1234", formattedCurrencyString)
+
+            val formattedCurrencyPretty = formatCurrencyForDisplay("1.2k", siteModel, "JPY", false)
+            assertEquals("¥1.2k", formattedCurrencyPretty)
+
+            val formattedCurrencyNegative = formatCurrencyForDisplay(-1234.12, siteModel, "JPY", true)
+            assertEquals("-¥1234", formattedCurrencyNegative)
+
+            val formattedCurrencyUseSiteCurrency = formatCurrencyForDisplay(1234.0, siteModel, null, true)
+            assertEquals("¥1234", formattedCurrencyUseSiteCurrency)
+
+            val formattedCurrencyDifferentCurrency = formatCurrencyForDisplay(1234.0, siteModel, "USD", true)
+            assertEquals("$1234", formattedCurrencyDifferentCurrency)
+        }
+
+        // -- No site settings stored
+        WellSql.delete(WCSettingsBuilder::class.java).execute()
+
+        with(wooCommerceStore) {
+            val formattedCurrencyDouble = formatCurrencyForDisplay(1234.12, siteModel, "CAD", true)
+            assertEquals("CA$1234.12", formattedCurrencyDouble)
+
+            val formattedCurrencyString = formatCurrencyForDisplay("1234.12", siteModel, "CAD", true)
+            assertEquals("CA$1234.12", formattedCurrencyString)
+
+            val formattedCurrencyPretty = formatCurrencyForDisplay("1.2k", siteModel, "CAD", false)
+            assertEquals("CA$1.2k", formattedCurrencyPretty)
+
+            val formattedCurrencyNegative = formatCurrencyForDisplay(-1234.12, siteModel, "CAD", true)
+            assertEquals("-CA$1234.12", formattedCurrencyNegative)
+
+            val formattedCurrencyUseSiteCurrency = formatCurrencyForDisplay(1234.12, siteModel, null, true)
+            assertEquals("1234.12", formattedCurrencyUseSiteCurrency)
+
+            val formattedCurrencyDifferentCurrency = formatCurrencyForDisplay(1234.12, siteModel, "EUR", true)
+            assertEquals("€1234.12", formattedCurrencyDifferentCurrency)
         }
     }
 
