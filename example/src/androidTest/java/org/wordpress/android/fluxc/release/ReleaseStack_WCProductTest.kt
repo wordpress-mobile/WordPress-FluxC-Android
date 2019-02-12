511112562fec68362978de852de6c023c8e7b58a
Nick Bradbury <nick.bradbury@gmail.com>
2019-02-12 13:54:53 -0500

    Fixed release test

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_WCProductTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_WCProductTest.kt
@@ -26,7 +26,7 @@ class ReleaseStack_WCProductTest : ReleaseStack_WCBase() {
 
     private var nextEvent: TestEvent = TestEvent.NONE
     private val productModel = WCProductModel(8).apply {
-        remoteProductId = 1125
+        remoteProductId = 1537
         date_created = "2018-04-20T15:45:14Z"
     }
     private var lastEvent: OnProductChanged? = null
