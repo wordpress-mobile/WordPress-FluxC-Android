b035125b6793eff694367e3a155a713ed8bd15ac
Oguz Kocer <oguz.kocer@automattic.com>
2019-02-21 17:18:30 -0500

    Significant documentation improvements and minor refactors for ListStoreConnectedTestHelper

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ListStoreConnectedTestHelper.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ListStoreConnectedTestHelper.kt
@@ -22,13 +22,27 @@ import org.wordpress.android.fluxc.store.ListStore
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
-internal class ListStoreConnectedTestHelper(
-    private val listStore: ListStore,
-    private val lifecycle: Lifecycle = SimpleTestLifecycle().lifecycle
-) {
+/**
+ * A helper class that makes writing connected tests for [ListStore] dead easy. It provides helpers for testing fetching
+ * first page and loading more data. It also makes it very easy to update/improve every connected test for [ListStore]
+ * from a single place.
+ */
+internal class ListStoreConnectedTestHelper(private val listStore: ListStore) {
+    /**
+     * A helper function that returns the list from [ListStore] for the given [ListDescriptor] and
+     * [ListDataStoreInterface].
+     *
+     * It provides a default [Lifecycle] instance and skips the `transform` function to make it easier to write tests.
+     * The default [Lifecycle] instance will NOT be destroyed throughout the test.
+     *
+     * @param listDescriptor [ListDescriptor] instance to be used to retrieve the list
+     * @param dataStore [ListDataStoreInterface] instance to be used to retrieve the list
+     * @param lifecycle [Lifecycle] instance to be used to retrieve the list.
+     */
     fun <T> getList(
         listDescriptor: ListDescriptor,
-        dataStore: ListDataStoreInterface<T>
+        dataStore: ListDataStoreInterface<T>,
+        lifecycle: Lifecycle = SimpleTestLifecycle().lifecycle
     ): PagedListWrapper<T> {
         return listStore.getList(
                 listDescriptor = listDescriptor,
@@ -38,34 +52,54 @@ internal class ListStoreConnectedTestHelper(
         )
     }
 
+    /**
+     * A helper function that fetches the first page of a list and asserts that correct values are observed for the
+     * given [PagedListWrapper].
+     *
+     * This function takes a [PagedListWrapper] generator instead of a [PagedListWrapper] instance to discourage any
+     * attempt of setting up the given [PagedListWrapper] and convey the intend that the setup will be performed
+     * by this helper instead.
+     *
+     * @param createPagedListWrapper A factory function that will create a [PagedListWrapper] instance.
+     */
     fun <T> testFetchFirstPage(createPagedListWrapper: () -> PagedListWrapper<T>) {
         fetchFirstPageAndAssert(createPagedListWrapper())
     }
 
-    fun <T> testLoadMore(createPagedListWrapper: () -> PagedListWrapper<T>) = runWithSimpleLifecycle { testLifecycle ->
+    /**
+     * A helper function that initially fetches the first page of a list and asserts that correct values are observed
+     * for the given [PagedListWrapper]. This is done so that it can then trigger loading more data to observe that
+     * all values are changed correctly.
+     *
+     * This function takes a [PagedListWrapper] generator instead of a [PagedListWrapper] instance to discourage any
+     * attempt of setting up the given [PagedListWrapper] and convey the intend that the setup will be performed
+     * by this helper instead.
+     *
+     * @param createPagedListWrapper A factory function that will create a [PagedListWrapper] instance.
+     */
+    fun <T> testLoadMore(createPagedListWrapper: () -> PagedListWrapper<T>) = runWithSimpleLifecycle { lifecycle ->
         val pagedListWrapper = createPagedListWrapper()
         fetchFirstPageAndAssert(pagedListWrapper)
         pagedListWrapper.testObservedDistinctValues(
-                lifecycle = testLifecycle,
+                lifecycle = lifecycle,
                 expectedIsEmptyValues = listOf(NOT_EMPTY),
                 expectedIsFetchingFirstPageValues = listOf(NOT_FETCHING_FIRST_PAGE),
                 expectedIsLoadingMoreValues = listOf(NOT_LOADING_MORE, LOADING_MORE, NOT_LOADING_MORE)
         ) {
-            // TODO: Cleanup how we trigger load more data
-            var alreadyObserved = false
-            pagedListWrapper.data.observeForever {
-                if (!alreadyObserved && it != null && it.size > 0) {
-                    it.loadAround(it.size - 1)
-                    alreadyObserved = true
-                }
-            }
+            pagedListWrapper.triggerLoadMore()
         }
     }
 
+    /**
+     * An internal helper function that fetches the first page of a list and asserts that correct values are observed
+     * for the given [PagedListWrapper].
+     *
+     * @param pagedListWrapper [PagedListWrapper] instance that will be tested
+     */
     private fun <T> fetchFirstPageAndAssert(pagedListWrapper: PagedListWrapper<T>) =
-            runWithSimpleLifecycle { testLifecycle ->
+            runWithSimpleLifecycle { lifecycle ->
                 pagedListWrapper.testObservedDistinctValues(
-                        lifecycle = testLifecycle,
+                        lifecycle = lifecycle,
                         expectedIsEmptyValues = listOf(EMPTY, NOT_EMPTY),
                         // TODO: Initial value should be NOT_FETCHING_FIRST_PAGE, but this is not posted by
                         // TODO: `PagedListWrapper`
@@ -77,6 +111,10 @@ internal class ListStoreConnectedTestHelper(
             }
 }
 
+/**
+ * A helper function to run a block using a [SimpleTestLifecycle] which will be destroyed automatically once the
+ * block is completed.
+ */
 private fun runWithSimpleLifecycle(
     lifecycle: SimpleTestLifecycle = SimpleTestLifecycle(),
     runTest: (LifecycleOwner) -> Unit
@@ -85,20 +123,41 @@ private fun runWithSimpleLifecycle(
     lifecycle.destroy()
 }
 
+/**
+ * A simple interface to make testing observed values easier.
+ */
 private interface ObservedValue<T> {
     val value: T
 }
 
+/**
+ * A simple [Boolean] wrapper to represent [PagedListWrapper.isEmpty] values.
+ *
+ * We should't directly use listOf(false, true, false) values, because it doesn't carry intend. This wrapper makes our
+ * tests more meaningful and less error prone.
+ */
 private enum class IsEmptyValue(override val value: Boolean) : ObservedValue<Boolean> {
     EMPTY(true),
     NOT_EMPTY(false);
 }
 
+/**
+ * A simple [Boolean] wrapper to represent [PagedListWrapper.isFetchingFirstPage] values.
+ *
+ * We should't directly use listOf(false, true, false) values, because it doesn't carry intend. This wrapper makes our
+ * tests more meaningful and less error prone.
+ */
 private enum class IsFetchingFirstPageValue(override val value: Boolean) : ObservedValue<Boolean> {
     FETCHING_FIRST_PAGE(true),
     NOT_FETCHING_FIRST_PAGE(false);
 }
 
+/**
+ * A simple [Boolean] wrapper to represent [PagedListWrapper.isLoadingMore] values.
+ *
+ * We should't directly use listOf(false, true, false) values, because it doesn't carry intend. This wrapper makes our
+ * tests more meaningful and less error prone.
+ */
 private enum class IsLoadingMoreValue(override val value: Boolean) : ObservedValue<Boolean> {
     LOADING_MORE(true),
     NOT_LOADING_MORE(false);
@@ -111,7 +170,7 @@ private enum class IsLoadingMoreValue(override val value: Boolean) : ObservedVal
  * @param expectedIsEmptyValues List of expected [PagedListWrapper.isEmpty] values
  * @param expectedIsFetchingFirstPageValues List of expected [PagedListWrapper.isFetchingFirstPage] values
  * @param expectedIsLoadingMoreValues List of expected [PagedListWrapper.isLoadingMore] values
- * @param setup
+ * @param testSetup The test setup to be run before waiting for all values to be observed
  *
  */
 private fun <T> PagedListWrapper<T>.testObservedDistinctValues(
@@ -119,7 +178,7 @@ private fun <T> PagedListWrapper<T>.testObservedDistinctValues(
     expectedIsEmptyValues: List<IsEmptyValue>,
     expectedIsFetchingFirstPageValues: List<IsFetchingFirstPageValue>,
     expectedIsLoadingMoreValues: List<IsLoadingMoreValue>,
-    setup: () -> Unit
+    testSetup: () -> Unit
 ) {
     val countDownLatch = CountDownLatch(3)
     val done = { countDownLatch.countDown() }
@@ -141,8 +200,19 @@ private fun <T> PagedListWrapper<T>.testObservedDistinctValues(
             assertionMessage = createAssertionMessage("IsLoadingMore"),
             done = done
     )
-    setup()
+    testSetup()
     assertTrue(countDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), TimeUnit.MILLISECONDS))
+}
+
+private fun <T> PagedListWrapper<T>.triggerLoadMore() {
+    val lifecycle = SimpleTestLifecycle()
+    this.data.observe(lifecycle, Observer {
+        if (it != null && it.size > 0) {
+            // `PagedList.loadAround` is used with the final item index to trigger loading more data from remote
+            it.loadAround(it.size - 1)
+            lifecycle.destroy()
+        }
+    })
 }
 
 /**
