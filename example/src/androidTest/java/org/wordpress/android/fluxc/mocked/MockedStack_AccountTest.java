98b6390a9103b29561c1a09a5541ded90b4da466
Oguz Kocer <oguz.kocer@automattic.com>
2019-02-21 21:20:57 -0500

    Fix MockedStack_AccountTest.testAuthenticationOK

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_AccountTest.java
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_AccountTest.java
@@ -79,6 +79,7 @@ public class MockedStack_AccountTest extends MockedStack_Base {
         // Correct user we should get an OnAuthenticationChanged message
         mCountDownLatch = new CountDownLatch(1);
         mDispatcher.dispatch(AuthenticationActionBuilder.newAuthenticateAction(payload));
+        mInterceptor.respondWith("authentication-success-response.json");
         assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS, TimeUnit.MILLISECONDS));
 
         // Log out and clear stored dummy account
