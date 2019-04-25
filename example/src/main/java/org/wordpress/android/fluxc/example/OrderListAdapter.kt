be1cc572b034f3017c33a971f91bfebb94e3a11f
Oguz Kocer <oguz.kocer@automattic.com>
2019-04-24 20:10:01 -0400

    Implements initial sectioning for woo orders in example app

--- example/src/main/java/org/wordpress/android/fluxc/example/OrderListAdapter.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/OrderListAdapter.kt
@@ -10,16 +10,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import org.wordpress.android.fluxc.example.WCOrderListItemUIType.LoadingItem
+import org.wordpress.android.fluxc.example.WCOrderListItemUIType.SectionHeader
 import org.wordpress.android.fluxc.example.WCOrderListItemUIType.WCOrderListUIItem
 
 private const val VIEW_TYPE_ORDER_ITEM = 0
 private const val VIEW_TYPE_LOADING = 1
+private const val VIEW_TYPE_SECTION_HEADER = 2
 
 class OrderListAdapter : PagedListAdapter<WCOrderListItemUIType, ViewHolder>(OrderListDiffItemCallback) {
     override fun getItemViewType(position: Int): Int {
         return when (getItem(position)) {
             is WCOrderListUIItem -> VIEW_TYPE_ORDER_ITEM
             is LoadingItem -> VIEW_TYPE_LOADING
+            is SectionHeader -> VIEW_TYPE_SECTION_HEADER
             null -> VIEW_TYPE_LOADING // Placeholder by paged list
         }
     }
@@ -31,6 +34,7 @@ class OrderListAdapter : PagedListAdapter<WCOrderListItemUIType, ViewHolder>(Ord
                 val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_skeleton, parent, false)
                 LoadingViewHolder(view)
             }
+            VIEW_TYPE_SECTION_HEADER -> SectionHeaderViewHolder(R.layout.list_item_section_header, parent)
             else -> {
                 // Fail fast if a new view type is added so the we can handle it
                 throw IllegalStateException("The view type '$viewType' needs to be handled")
@@ -39,13 +43,19 @@ class OrderListAdapter : PagedListAdapter<WCOrderListItemUIType, ViewHolder>(Ord
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
         if (holder is WCOrderItemUIViewHolder) {
-            val item = getItem(position)
             assert(item is WCOrderListUIItem) {
                 "If we are presenting WCOrderItemUIViewHolder, the item has to be of type WCOrderListUIItem " +
                         "for position: $position"
             }
             holder.onBind((item as WCOrderListUIItem))
+        } else if (holder is SectionHeaderViewHolder) {
+            assert(item is SectionHeader) {
+                "If we are presenting SectionHeaderViewHolder, the item has to be of type SectionHeader " +
+                        "for position: $position"
+            }
+            holder.onBind((item as SectionHeader))
         }
     }
 }
@@ -98,3 +108,12 @@ private class WCOrderItemUIViewHolder(
 }
 
 private class LoadingViewHolder(view: View) : RecyclerView.ViewHolder(view)
+private class SectionHeaderViewHolder(
+    @LayoutRes layout: Int,
+    parentView: ViewGroup
+) : RecyclerView.ViewHolder(LayoutInflater.from(parentView.context).inflate(layout, parentView, false)) {
+    private val titleTv: TextView = itemView.findViewById(R.id.section_header_title)
+    fun onBind(header: SectionHeader) {
+        titleTv.text = header.title.name
+    }
+}
--- example/src/main/java/org/wordpress/android/fluxc/example/WooOrderListFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/WooOrderListFragment.kt
@@ -16,19 +16,31 @@ import android.view.ViewGroup
 import android.widget.ProgressBar
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_woo_order_list.*
+import org.apache.commons.lang3.time.DateUtils
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.example.TimeGroup.GROUP_OLDER_MONTH
+import org.wordpress.android.fluxc.example.TimeGroup.GROUP_OLDER_TWO_DAYS
+import org.wordpress.android.fluxc.example.TimeGroup.GROUP_OLDER_WEEK
+import org.wordpress.android.fluxc.example.TimeGroup.GROUP_TODAY
+import org.wordpress.android.fluxc.example.TimeGroup.GROUP_YESTERDAY
+import org.wordpress.android.fluxc.example.WCOrderListItemIdentifier.OrderIdentifier
+import org.wordpress.android.fluxc.example.WCOrderListItemIdentifier.SectionHeaderIdentifier
 import org.wordpress.android.fluxc.example.WCOrderListItemUIType.LoadingItem
+import org.wordpress.android.fluxc.example.WCOrderListItemUIType.SectionHeader
 import org.wordpress.android.fluxc.example.WCOrderListItemUIType.WCOrderListUIItem
 import org.wordpress.android.fluxc.generated.WCOrderActionBuilder
 import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
 import org.wordpress.android.fluxc.model.WCOrderListDescriptor
+import org.wordpress.android.fluxc.model.WCOrderSummaryModel
 import org.wordpress.android.fluxc.model.list.PagedListWrapper
 import org.wordpress.android.fluxc.model.list.datasource.ListItemDataSourceInterface
 import org.wordpress.android.fluxc.store.ListStore
 import org.wordpress.android.fluxc.store.WCOrderStore
 import org.wordpress.android.fluxc.store.WCOrderStore.FetchOrderListPayload
 import org.wordpress.android.fluxc.store.WooCommerceStore
+import org.wordpress.android.util.DateTimeUtils
 import org.wordpress.android.util.widgets.CustomSwipeRefreshLayout
+import java.util.Date
 import javax.inject.Inject
 
 class WooOrderListFragment : Fragment() {
@@ -69,7 +81,8 @@ class WooOrderListFragment : Fragment() {
         val orderListDescriptor = WCOrderListDescriptor(
                 site = wooCommerceStore.getWooCommerceSites()[0], // crash if site is not there
                 statusFilter = null,
-                searchQuery = "")
+                searchQuery = ""
+        )
 
         loadList(orderListDescriptor)
 
@@ -78,7 +91,8 @@ class WooOrderListFragment : Fragment() {
                 setColorSchemeColors(
                         ContextCompat.getColor(act, android.R.color.holo_blue_bright),
                         ContextCompat.getColor(act, android.R.color.holo_green_light),
-                        ContextCompat.getColor(act, android.R.color.holo_orange_dark))
+                        ContextCompat.getColor(act, android.R.color.holo_orange_dark)
+                )
             }
             setOnRefreshListener {
                 pagedListWrapper?.fetchFirstPage()
@@ -89,7 +103,8 @@ class WooOrderListFragment : Fragment() {
             val descriptor = WCOrderListDescriptor(
                     site = wooCommerceStore.getWooCommerceSites()[0], // crash if site is not there
                     statusFilter = null,
-                    searchQuery = order_search_query.text.toString())
+                    searchQuery = order_search_query.text.toString()
+            )
             loadList(descriptor)
         }
 
@@ -98,7 +113,8 @@ class WooOrderListFragment : Fragment() {
             val descriptor = WCOrderListDescriptor(
                     site = wooCommerceStore.getWooCommerceSites()[0], // crash if site is not there
                     statusFilter = null,
-                    searchQuery = null)
+                    searchQuery = null
+            )
             loadList(descriptor)
         }
     }
@@ -136,7 +152,35 @@ class WooOrderListFragment : Fragment() {
     }
 }
 
+enum class TimeGroup {
+    GROUP_TODAY,
+    GROUP_YESTERDAY,
+    GROUP_OLDER_TWO_DAYS,
+    GROUP_OLDER_WEEK,
+    GROUP_OLDER_MONTH;
+
+    companion object {
+        fun getTimeGroupForDate(date: Date): TimeGroup {
+            val dateToday = Date()
+            return when {
+                date < DateUtils.addMonths(dateToday, -1) -> GROUP_OLDER_MONTH
+                date < DateUtils.addWeeks(dateToday, -1) -> GROUP_OLDER_WEEK
+                date < DateUtils.addDays(dateToday, -2) -> GROUP_OLDER_TWO_DAYS
+                DateUtils.isSameDay(DateUtils.addDays(dateToday, -2), date) -> GROUP_OLDER_TWO_DAYS
+                DateUtils.isSameDay(DateUtils.addDays(dateToday, -1), date) -> GROUP_YESTERDAY
+                else -> GROUP_TODAY
+            }
+        }
+    }
+}
+
+sealed class WCOrderListItemIdentifier {
+    class SectionHeaderIdentifier(val title: TimeGroup) : WCOrderListItemIdentifier()
+    class OrderIdentifier(val remoteId: RemoteId) : WCOrderListItemIdentifier()
+}
+
 sealed class WCOrderListItemUIType {
+    class SectionHeader(val title: TimeGroup) : WCOrderListItemUIType()
     class LoadingItem(val remoteId: RemoteId) : WCOrderListItemUIType()
     data class WCOrderListUIItem(
         val remoteOrderId: RemoteId,
@@ -151,24 +195,26 @@ private class WCOrderListItemDataSource(
     val dispatcher: Dispatcher,
     val wcOrderStore: WCOrderStore,
     val lifecycle: Lifecycle
-) : ListItemDataSourceInterface<WCOrderListDescriptor, RemoteId, WCOrderListItemUIType> {
+) : ListItemDataSourceInterface<WCOrderListDescriptor, WCOrderListItemIdentifier, WCOrderListItemUIType> {
     private val fetcher = WCOrderFetcher(lifecycle, dispatcher)
 
     override fun getItemsAndFetchIfNecessary(
         listDescriptor: WCOrderListDescriptor,
-        itemIdentifiers: List<RemoteId>
+        itemIdentifiers: List<WCOrderListItemIdentifier>
     ): List<WCOrderListItemUIType> {
-        val ordersMap = wcOrderStore.getOrdersForDescriptor(listDescriptor, itemIdentifiers)
+        // TODO: Move fetching to its own method
+        val remoteItemIds = itemIdentifiers.mapNotNull { (it as? OrderIdentifier)?.remoteId }
+        val ordersMap = wcOrderStore.getOrdersForDescriptor(listDescriptor, remoteItemIds)
         // Fetch missing items
         fetcher.fetchOrders(
                 site = listDescriptor.site,
-                remoteItemIds = itemIdentifiers.filter { !ordersMap.containsKey(it) }
+                remoteItemIds = remoteItemIds.filter { !ordersMap.containsKey(it) }
         )
 
-        return itemIdentifiers.map { remoteId ->
-            ordersMap[remoteId].let { order ->
+        val mapSummary = { remoteOrderId: RemoteId ->
+            ordersMap[remoteOrderId].let { order ->
                 if (order == null) {
-                    LoadingItem(remoteId)
+                    LoadingItem(remoteOrderId)
                 } else {
                     WCOrderListUIItem(
                             remoteOrderId = RemoteId(order.remoteOrderId),
@@ -180,13 +226,51 @@ private class WCOrderListItemDataSource(
                 }
             }
         }
+        return itemIdentifiers.map { identifier ->
+            when (identifier) {
+                is OrderIdentifier -> mapSummary(identifier.remoteId)
+                is SectionHeaderIdentifier -> SectionHeader(title = identifier.title)
+            }
+        }
     }
 
+    // TODO: Optimize this calculation
     override fun getItemIdentifiers(
         listDescriptor: WCOrderListDescriptor,
         remoteItemIds: List<RemoteId>,
         isListFullyFetched: Boolean
-    ): List<RemoteId> = remoteItemIds
+    ): List<WCOrderListItemIdentifier> {
+        val orderSummaries = wcOrderStore.getOrderSummariesByRemoteOrderIds(listDescriptor.site, remoteItemIds)
+                .let { orderSummaryMap ->
+                    // TODO: order summaries should never be null, how can we make that clear?
+                    remoteItemIds.mapNotNull { orderSummaryMap[it] }
+                }
+        val listToday = ArrayList<OrderIdentifier>()
+        val listYesterday = ArrayList<OrderIdentifier>()
+        val listTwoDays = ArrayList<OrderIdentifier>()
+        val listWeek = ArrayList<OrderIdentifier>()
+        val listMonth = ArrayList<OrderIdentifier>()
+        val mapToRemoteOrderIdentifier = { summary: WCOrderSummaryModel ->
+            OrderIdentifier(RemoteId(summary.remoteOrderId))
+        }
+        orderSummaries.forEach {
+            // Default to today if the date cannot be parsed
+            val date: Date = DateTimeUtils.dateUTCFromIso8601(it.dateCreated) ?: Date()
+            when (TimeGroup.getTimeGroupForDate(date)) {
+                GROUP_TODAY -> listToday.add(mapToRemoteOrderIdentifier(it))
+                GROUP_YESTERDAY -> listYesterday.add(mapToRemoteOrderIdentifier(it))
+                GROUP_OLDER_TWO_DAYS -> listTwoDays.add(mapToRemoteOrderIdentifier(it))
+                GROUP_OLDER_WEEK -> listWeek.add(mapToRemoteOrderIdentifier(it))
+                GROUP_OLDER_MONTH -> listMonth.add(mapToRemoteOrderIdentifier(it))
+            }
+        }
+        // TODO: Take empty lists into account
+        return listOf(SectionHeaderIdentifier(GROUP_TODAY)) + listToday +
+                listOf(SectionHeaderIdentifier(GROUP_YESTERDAY)) + listYesterday +
+                listOf(SectionHeaderIdentifier(GROUP_OLDER_TWO_DAYS)) + listTwoDays +
+                listOf(SectionHeaderIdentifier(GROUP_OLDER_WEEK)) + listWeek +
+                listOf(SectionHeaderIdentifier(GROUP_OLDER_MONTH)) + listMonth
+    }
 
     override fun fetchList(listDescriptor: WCOrderListDescriptor, offset: Long) {
         val fetchOrderListPayload = FetchOrderListPayload(listDescriptor, offset)
