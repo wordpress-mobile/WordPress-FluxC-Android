b27195d459628b9c90085bc7e861abf641a00d3a
Klymentiy haykov <forsver@gmail.com>
2019-04-29 22:01:45 -0700

    Testing insufficient funds and wrong domain contact model.

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TransactionsTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_TransactionsTest.kt
@@ -1,16 +1,28 @@
 package org.wordpress.android.fluxc.release
 
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertNotNull
+import junit.framework.Assert.assertTrue
+import junit.framework.Assert.fail
 import org.greenrobot.eventbus.Subscribe
 import org.junit.Assert
 import org.junit.Test
 import org.wordpress.android.fluxc.TestUtils
 import org.wordpress.android.fluxc.action.TransactionAction
 import org.wordpress.android.fluxc.generated.TransactionActionBuilder
+import org.wordpress.android.fluxc.model.DomainContactModel
+import org.wordpress.android.fluxc.release.ReleaseStack_TransactionsTest.TestEvents.ERROR_REDEEMING_SHOPPING_CART_INSUFFICIENT_FUNDS
+import org.wordpress.android.fluxc.release.ReleaseStack_TransactionsTest.TestEvents.ERROR_REDEEMING_SHOPPING_CART_WRONG_COUNTRY_CODE
 import org.wordpress.android.fluxc.store.SiteStore
 import org.wordpress.android.fluxc.store.TransactionsStore
 import org.wordpress.android.fluxc.store.TransactionsStore.CreateShoppingCartPayload
 import org.wordpress.android.fluxc.store.TransactionsStore.OnShoppingCartCreated
+import org.wordpress.android.fluxc.store.TransactionsStore.OnShoppingCartRedeemed
 import org.wordpress.android.fluxc.store.TransactionsStore.OnSupportedCountriesFetched
+import org.wordpress.android.fluxc.store.TransactionsStore.RedeemShoppingCartPayload
+import org.wordpress.android.fluxc.store.TransactionsStore.TransactionErrorType.COUNTRY_CODE
+import org.wordpress.android.fluxc.store.TransactionsStore.TransactionErrorType.INSUFFICIENT_FUNDS
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import javax.inject.Inject
@@ -24,12 +36,28 @@ class ReleaseStack_TransactionsTest : ReleaseStack_WPComBase() {
     companion object {
         private const val TEST_DOMAIN_NAME = "superraredomainname156726.blog"
         private const val TEST_DOMAIN_PRODUCT_ID = 76
+        private val TEST_DOMAIN_CONTACT_MODEL = DomainContactModel(
+                "Wapu",
+                "Wordpress",
+                "WordPress",
+                "7337 Publishing Row",
+                "Apt 404",
+                "90210",
+                "Best City",
+                "CA",
+                "US",
+                "wapu@wordpress.org",
+                "+1.3120000000",
+                null
+        )
     }
 
     internal enum class TestEvents {
         NONE,
         COUNTRIES_FETCHED,
-        SHOPPING_CART_CREATED
+        SHOPPING_CART_CREATED,
+        ERROR_REDEEMING_SHOPPING_CART_INSUFFICIENT_FUNDS,
+        ERROR_REDEEMING_SHOPPING_CART_WRONG_COUNTRY_CODE
     }
 
     @Throws(Exception::class)
@@ -66,6 +94,30 @@ class ReleaseStack_TransactionsTest : ReleaseStack_WPComBase() {
         Assert.assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), MILLISECONDS))
     }
 
+    @Test
+    fun testRedeemingCardWithoutDomainCredit() {
+        nextEvent = TestEvents.ERROR_REDEEMING_SHOPPING_CART_INSUFFICIENT_FUNDS
+        mCountDownLatch = CountDownLatch(1)
+        mDispatcher.dispatch(
+                TransactionActionBuilder.newCreateShoppingCartAction(
+                        CreateShoppingCartPayload(sSite, TEST_DOMAIN_PRODUCT_ID.toString(), TEST_DOMAIN_NAME, true)
+                )
+        )
+        Assert.assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), MILLISECONDS))
+    }
+
+    @Test
+    fun testRedeemingCardWithWrongCountryCode() {
+        nextEvent = TestEvents.ERROR_REDEEMING_SHOPPING_CART_WRONG_COUNTRY_CODE
+        mCountDownLatch = CountDownLatch(1)
+        mDispatcher.dispatch(
+                TransactionActionBuilder.newCreateShoppingCartAction(
+                        CreateShoppingCartPayload(sSite, TEST_DOMAIN_PRODUCT_ID.toString(), TEST_DOMAIN_NAME, true)
+                )
+        )
+        Assert.assertTrue(mCountDownLatch.await(TestUtils.DEFAULT_TIMEOUT_MS.toLong(), MILLISECONDS))
+    }
+
     @Subscribe
     @Suppress("unused")
     fun onSupportedCountriesFetched(event: OnSupportedCountriesFetched) {
@@ -85,16 +137,56 @@ class ReleaseStack_TransactionsTest : ReleaseStack_WPComBase() {
             throw AssertionError("Unexpected error occurred with type: " + event.error.type)
         }
 
-        Assert.assertNotNull(event.cartDetails)
-        Assert.assertEquals(event.cartDetails!!.blog_id.toLong(), sSite.siteId)
-        Assert.assertEquals(event.cartDetails!!.cart_key, sSite.siteId.toString())
+        assertNotNull(event.cartDetails)
+        assertEquals(event.cartDetails!!.blog_id.toLong(), sSite.siteId)
+        assertEquals(event.cartDetails!!.cart_key, sSite.siteId.toString())
 
-        Assert.assertNotNull(event.cartDetails!!.products)
-        Assert.assertTrue(event.cartDetails!!.products!!.isNotEmpty())
-        Assert.assertEquals(event.cartDetails!!.products!!.size, 2)
+        assertNotNull(event.cartDetails!!.products)
+        assertTrue(event.cartDetails!!.products!!.isNotEmpty())
+        assertEquals(event.cartDetails!!.products!!.size, 2)
 
-        Assert.assertEquals(event.cartDetails!!.products!![0].product_id, TEST_DOMAIN_PRODUCT_ID)
-        Assert.assertEquals(event.cartDetails!!.products!![0].meta, TEST_DOMAIN_NAME)
+        assertEquals(event.cartDetails!!.products!![0].product_id, TEST_DOMAIN_PRODUCT_ID)
+        assertEquals(event.cartDetails!!.products!![0].meta, TEST_DOMAIN_NAME)
+
+        if (nextEvent == ERROR_REDEEMING_SHOPPING_CART_INSUFFICIENT_FUNDS) {
+            mDispatcher.dispatch(
+                    TransactionActionBuilder.newRedeemCartWithCreditsAction(
+                            RedeemShoppingCartPayload(
+                                    event.cartDetails!!, TEST_DOMAIN_CONTACT_MODEL
+                            )
+                    )
+            )
+
+            return
+        } else if (nextEvent == ERROR_REDEEMING_SHOPPING_CART_WRONG_COUNTRY_CODE) {
+            val contactModelWithWrongCountryCode = TEST_DOMAIN_CONTACT_MODEL.copy(countryCode = "USB")
+
+            mDispatcher.dispatch(
+                    TransactionActionBuilder.newRedeemCartWithCreditsAction(
+                            RedeemShoppingCartPayload(
+                                    event.cartDetails!!, contactModelWithWrongCountryCode
+                            )
+                    )
+            )
+            return
+        }
+
+        mCountDownLatch.countDown()
+    }
+
+    @Subscribe
+    @Suppress("unused")
+    fun onShoppingCartRedeemed(event: OnShoppingCartRedeemed) {
+        assertNotNull(event)
+        assertFalse(event.success)
+        assertNotNull(event.error)
+
+        when (nextEvent) {
+            ERROR_REDEEMING_SHOPPING_CART_WRONG_COUNTRY_CODE -> assertEquals(COUNTRY_CODE, event.error.type)
+            ERROR_REDEEMING_SHOPPING_CART_INSUFFICIENT_FUNDS -> assertEquals(INSUFFICIENT_FUNDS, event.error.type)
+            else -> fail("Unexpected test event: $nextEvent")
+        }
+
         mCountDownLatch.countDown()
     }
 }
