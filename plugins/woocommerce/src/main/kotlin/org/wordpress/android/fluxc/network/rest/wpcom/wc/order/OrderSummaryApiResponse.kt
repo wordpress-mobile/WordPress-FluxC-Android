9aa2fc26f8ec805d6b9644f465311bd22aa4f179
AmandaRiu <amanda.riu@automattic.com>
2019-06-04 21:23:01 -0600

    Add serialized naming

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderSummaryApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderSummaryApiResponse.kt
@@ -1,9 +1,11 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
 
+import com.google.gson.annotations.SerializedName
 import org.wordpress.android.fluxc.network.Response
 
-@Suppress("PropertyName")
 class OrderSummaryApiResponse : Response {
     val id: Long? = null
-    val date_created_gmt: String? = null
+
+    @SerializedName("dateCreatedGmt")
+    val dateCreatedGmt: String? = null
 }
