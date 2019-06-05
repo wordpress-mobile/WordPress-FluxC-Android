56a7823e28b6ce6a55e64c1f7843c6a530103235
AmandaRiu <amanda.riu@automattic.com>
2019-06-04 21:14:08 -0600

    Remove unused class

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/TimeGroup.kt
+++
@@ -1,9 +0,0 @@
-package org.wordpress.android.fluxc.model
-
-enum class TimeGroup {
-    GROUP_TODAY,
-    GROUP_YESTERDAY,
-    GROUP_OLDER_TWO_DAYS,
-    GROUP_OLDER_WEEK,
-    GROUP_OLDER_MONTH;
-}
