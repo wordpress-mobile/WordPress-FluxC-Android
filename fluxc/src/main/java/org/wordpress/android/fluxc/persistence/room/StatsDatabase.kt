89f3770d6b026f548c5a562b699f090ebe96dbe9
vojtasmrcek <planarvoid@gmail.com>
2019-07-03 15:09:02 +0200

    Remove migration and rename database

--- fluxc/src/main/java/org/wordpress/android/fluxc/persistence/room/StatsDatabase.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/persistence/room/StatsDatabase.kt
@@ -1,23 +1,22 @@
 package org.wordpress.android.fluxc.persistence.room
 
+import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import android.content.Context
 import org.wordpress.android.fluxc.persistence.room.StatsDao.StatsBlock
 
-const val STATS_DB_NAME = "stats.db"
+const val STATS_DB_NAME = "stats_database.db"
 
-@Database(entities = arrayOf(StatsBlock::class), version = 2, exportSchema = true)
+@Database(entities = arrayOf(StatsBlock::class), version = 1, exportSchema = true)
 @TypeConverters(BlockTypeConverter::class, StatsTypeConverter::class)
 abstract class StatsDatabase : RoomDatabase() {
     abstract fun statsDao(): StatsDao
 
     companion object {
         fun build(context: Context): StatsDatabase {
-            return Room.databaseBuilder(context.applicationContext, StatsDatabase::class.java, STATS_DB_NAME)
-                    .addMigrations(Migration1to2).build()
+            return Room.databaseBuilder(context.applicationContext, StatsDatabase::class.java, STATS_DB_NAME).build()
         }
     }
 }
