3c684a04db5bcdaacd8d7d526fbe28adf4d894f4
Ondrej Ruttkay <ondrej@automattic.com>
2019-07-17 13:56:19 +0200

    Add RefundsStore

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/RefundsStore.kt
@@ -0,0 +1,90 @@
+package org.wordpress.android.fluxc.store
+
+import kotlinx.coroutines.withContext
+import org.wordpress.android.fluxc.Payload
+import org.wordpress.android.fluxc.model.WCOrderModel
+import org.wordpress.android.fluxc.model.refunds.RefundsMapper
+import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType
+import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequest.WPComGsonNetworkError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.refunds.RefundsRestClient
+import org.wordpress.android.fluxc.store.RefundsStore.RefundsError
+import org.wordpress.android.fluxc.store.Store.OnChangedError
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+import org.wordpress.android.fluxc.store.RefundsStore.RefundsErrorType.GENERIC_ERROR
+
+@Singleton
+class RefundsStore @Inject constructor(
+    private val restClient: RefundsRestClient,
+    private val coroutineContext: CoroutineContext,
+    private val refundsMapper: RefundsMapper
+) {
+    suspend fun createFullRefund(order: WCOrderModel) =
+            withContext(coroutineContext) {
+//                if (!forced && sqlUtils.hasFreshRequest(siteModel, limitMode.limit)) {
+//                    return@withContext OnStatsFetched(getTags(siteModel, limitMode), cached = true)
+//                }
+                val response = restClient.createRefund(order)
+                return@withContext when {
+                    response.isError -> {
+                        OnRefundCreated(response.error)
+                    }
+                    response.response != null -> {
+//                        sqlUtils.insert(siteModel, response.response, requestedItems = limitMode.limit)
+                        OnRefundCreated(
+                                refundsMapper.map(response.response)
+                        )
+                    }
+                    else -> OnRefundCreated(RefundsError(GENERIC_ERROR))
+                }
+            }
+
+//    fun getTags(site: SiteModel, cacheMode: LimitMode): TagsModel? {
+//        return sqlUtils.select(site)?.let { insightsMapper.map(it, cacheMode) }
+//    }
+
+    data class RefundsPayload<T>(
+        val response: T? = null
+    ) : Payload<RefundsError>() {
+        constructor(error: RefundsError) : this() {
+            this.error = error
+        }
+    }
+
+    data class OnRefundCreated<T>(val model: T? = null, val cached: Boolean = false) : Store.OnChanged<RefundsError>() {
+        constructor(error: RefundsError) : this() {
+            this.error = error
+        }
+    }
+
+    class RefundsError(var type: RefundsErrorType, var message: String? = null) : OnChangedError
+
+    enum class RefundsErrorType {
+        INVALID_PARAM,
+        INVALID_ID,
+        ORDER_STATUS_NOT_FOUND,
+        PLUGIN_NOT_ACTIVE,
+        GENERIC_ERROR
+    }
+}
+
+fun WPComGsonNetworkError.toRefundsError(): RefundsError {
+    val type = when (type) {
+        GenericErrorType.TIMEOUT,
+        GenericErrorType.NO_CONNECTION,
+        GenericErrorType.INVALID_SSL_CERTIFICATE,
+        GenericErrorType.NETWORK_ERROR,
+        GenericErrorType.PARSE_ERROR,
+        GenericErrorType.NOT_FOUND,
+        GenericErrorType.CENSORED,
+        GenericErrorType.INVALID_RESPONSE,
+        GenericErrorType.HTTP_AUTH_ERROR,
+        GenericErrorType.AUTHORIZATION_REQUIRED,
+        GenericErrorType.NOT_AUTHENTICATED,
+        GenericErrorType.UNKNOWN,
+        GenericErrorType.SERVER_ERROR,
+        null -> GENERIC_ERROR
+    }
+    return RefundsError(type, message)
+}
