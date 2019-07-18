7f46f5d25d586f3ae7aa62457cff0ad4177b27dc
anitaa <murthyanitaa@gmail.com>
2019-07-18 11:31:14 +0530

    Added utils class/methods to insert/update/get visitor stats from local db

--- example/src/test/java/org/wordpress/android/fluxc/wc/stats/WCStatsTestUtils.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/stats/WCStatsTestUtils.kt
@@ -2,6 +2,7 @@ package org.wordpress.android.fluxc.wc.stats
 
 import org.wordpress.android.fluxc.UnitTestUtils
 import org.wordpress.android.fluxc.model.WCOrderStatsModel
+import org.wordpress.android.fluxc.model.WCVisitorStatsModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.orderstats.OrderStatsRestClient.OrderStatsApiUnit
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -17,6 +18,30 @@ object WCStatsTestUtils {
         data: String = UnitTestUtils.getStringFromResourceFile(this.javaClass, "wc/order-stats-data.json")
     ): WCOrderStatsModel {
         return WCOrderStatsModel().apply {
+            this.localSiteId = localSiteId
+            this.unit = unit
+            this.quantity = quantity
+            this.endDate = endDate
+            this.fields = fields
+            this.data = data
+            this.date = endDate
+            startDate?.let {
+                this.startDate = it
+                this.isCustomField = true
+            }
+        }
+    }
+
+    fun generateSampleVisitorStatsModel(
+        localSiteId: Int = 6,
+        unit: String = OrderStatsApiUnit.DAY.toString(),
+        quantity: String = "30",
+        startDate: String? = null,
+        endDate: String = DateTimeFormatter.ofPattern("YYYY-MM-dd").format(LocalDateTime.now()),
+        fields: String = UnitTestUtils.getStringFromResourceFile(this.javaClass, "wc/visitor-stats-fields.json"),
+        data: String = UnitTestUtils.getStringFromResourceFile(this.javaClass, "wc/visitor-stats-data.json")
+    ): WCVisitorStatsModel {
+        return WCVisitorStatsModel().apply {
             this.localSiteId = localSiteId
             this.unit = unit
             this.quantity = quantity
