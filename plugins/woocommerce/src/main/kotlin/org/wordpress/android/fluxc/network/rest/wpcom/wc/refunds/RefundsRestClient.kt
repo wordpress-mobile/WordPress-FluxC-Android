ede2448c92b22360ef2cc499501b8085efb10a4f
Ondrej Ruttkay <ondrej@automattic.com>
2019-07-19 20:28:18 +0200

    Update the RefundsRestClient to use the Jetpack tunnel requests

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/refunds/RefundsRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/refunds/RefundsRestClient.kt
@@ -5,14 +5,14 @@ import com.android.volley.RequestQueue
 import com.google.gson.annotations.SerializedName
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
-import org.wordpress.android.fluxc.model.WCOrderModel
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.WCOrderModel.LineItem
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
-import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder
-import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Error
-import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Success
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
+import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder
+import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder.JetpackResponse.JetpackError
+import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder.JetpackResponse.JetpackSuccess
 import org.wordpress.android.fluxc.store.RefundsStore.RefundsPayload
 import org.wordpress.android.fluxc.store.toRefundsError
 import javax.inject.Singleton
@@ -21,40 +21,39 @@ import javax.inject.Singleton
 class RefundsRestClient
 constructor(
     dispatcher: Dispatcher,
-    private val wpComGsonRequestBuilder: WPComGsonRequestBuilder,
+    private val jetpackTunnelGsonRequestBuilder: JetpackTunnelGsonRequestBuilder,
     appContext: Context?,
     requestQueue: RequestQueue,
     accessToken: AccessToken,
     userAgent: UserAgent
 ) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent) {
     suspend fun createRefund(
-        order: WCOrderModel,
+        site: SiteModel,
+        orderId: Long,
+        amount: String,
         reason: String = "",
         automaticRefund: Boolean = true,
-        partialRefundLineItems: List<LineItem> = emptyList(),
-        forced: Boolean = true
+        partialRefundLineItems: List<LineItem> = emptyList()
     ): RefundsPayload<RefundResponse> {
-        val url = WOOCOMMERCE.orders.id(order.remoteOrderId).refund.pathV3
+        val url = WOOCOMMERCE.orders.id(orderId).refund.pathV3
 
         val params = mapOf(
-            "amount" to order.total,
+            "amount" to amount,
             "reason" to reason,
             "api_refund" to automaticRefund.toString(),
             "line_items" to partialRefundLineItems.toString()
         )
-        val response = wpComGsonRequestBuilder.syncGetRequest(
+        val response = jetpackTunnelGsonRequestBuilder.syncPostRequest(
                 this,
+                site,
                 url,
                 params,
-                RefundResponse::class.java,
-                enableCaching = false,
-                forced = forced
+                RefundResponse::class.java
         )
         return when (response) {
-            is Success -> {
+            is JetpackSuccess -> {
                 RefundsPayload(response.data)
             }
-            is Error -> {
                 RefundsPayload(response.error.toRefundsError())
             }
         }
