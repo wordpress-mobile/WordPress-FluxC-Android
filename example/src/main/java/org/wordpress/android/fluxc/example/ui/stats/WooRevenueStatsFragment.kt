6ec53d7ff8ab5c31d276eb714eec043af30bec54
anitaa <murthyanitaa@gmail.com>
2019-08-06 12:16:49 +0530

    Added new buttons to example app to test the visitor stats data for current day, week, month and year

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/stats/WooRevenueStatsFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/stats/WooRevenueStatsFragment.kt
@@ -13,14 +13,16 @@ import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.action.WCStatsAction
 import org.wordpress.android.fluxc.example.R
-import org.wordpress.android.fluxc.example.SiteSelectorDialog
 import org.wordpress.android.fluxc.example.prependToLog
+import org.wordpress.android.fluxc.example.ui.StoreSelectorDialog
 import org.wordpress.android.fluxc.generated.WCStatsActionBuilder
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.store.WCStatsStore
+import org.wordpress.android.fluxc.store.WCStatsStore.FetchNewVisitorStatsPayload
 import org.wordpress.android.fluxc.store.WCStatsStore.FetchRevenueStatsAvailabilityPayload
 import org.wordpress.android.fluxc.store.WCStatsStore.FetchRevenueStatsPayload
 import org.wordpress.android.fluxc.store.WCStatsStore.OnWCRevenueStatsChanged
+import org.wordpress.android.fluxc.store.WCStatsStore.OnWCStatsChanged
 import org.wordpress.android.fluxc.store.WCStatsStore.StatsGranularity
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
@@ -45,7 +47,7 @@ class WooRevenueStatsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         stats_select_site.setOnClickListener {
-            showSiteSelectorDialog(selectedPos, object : SiteSelectorDialog.Listener {
+            showSiteSelectorDialog(selectedPos, object : StoreSelectorDialog.Listener {
                 override fun onSiteSelected(site: SiteModel, pos: Int) {
                     selectedSite = site
                     selectedPos = pos
@@ -117,6 +119,34 @@ class WooRevenueStatsFragment : Fragment() {
                 dispatcher.dispatch(WCStatsActionBuilder.newFetchRevenueStatsAction(payload))
             } ?: prependToLog("No site selected!")
         }
+
+        fetch_current_day_visitor_stats.setOnClickListener {
+            selectedSite?.let {
+                val payload = FetchNewVisitorStatsPayload(site = it, granularity = StatsGranularity.DAYS)
+                dispatcher.dispatch(WCStatsActionBuilder.newFetchNewVisitorStatsAction(payload))
+            } ?: prependToLog("No site selected!")
+        }
+
+        fetch_current_week_visitor_stats_forced.setOnClickListener {
+            selectedSite?.let {
+                val payload = FetchNewVisitorStatsPayload(it, StatsGranularity.WEEKS, forced = true)
+                dispatcher.dispatch(WCStatsActionBuilder.newFetchNewVisitorStatsAction(payload))
+            } ?: prependToLog("No site selected!")
+        }
+
+        fetch_current_month_visitor_stats.setOnClickListener {
+            selectedSite?.let {
+                val payload = FetchNewVisitorStatsPayload(site = it, granularity = StatsGranularity.MONTHS)
+                dispatcher.dispatch(WCStatsActionBuilder.newFetchNewVisitorStatsAction(payload))
+            } ?: prependToLog("No site selected!")
+        }
+
+        fetch_current_year_visitor_stats_forced.setOnClickListener {
+            selectedSite?.let {
+                val payload = FetchNewVisitorStatsPayload(it, StatsGranularity.YEARS, forced = true)
+                dispatcher.dispatch(WCStatsActionBuilder.newFetchNewVisitorStatsAction(payload))
+            } ?: prependToLog("No site selected!")
+        }
     }
 
     override fun onStart() {
@@ -159,10 +189,48 @@ class WooRevenueStatsFragment : Fragment() {
         }
     }
 
-    private fun showSiteSelectorDialog(selectedPos: Int, listener: SiteSelectorDialog.Listener) {
+    @Suppress("unused")
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onWCStatsChanged(event: OnWCStatsChanged) {
+        if (event.isError) {
+            prependToLog("Error from " + event.causeOfChange + " - error: " + event.error.type)
+            return
+        }
+
+        val site = selectedSite
+        when (event.causeOfChange) {
+            WCStatsAction.FETCH_NEW_VISITOR_STATS -> {
+                val visitorStatsMap = wcStatsStore.getNewVisitorStats(
+                        site!!,
+                        event.granularity,
+                        event.quantity,
+                        event.date,
+                        event.isCustomField
+                )
+                if (visitorStatsMap.isEmpty()) {
+                    prependToLog("No visitor stats were stored for site " + site.name + " =(")
+                } else {
+                    if (event.isCustomField) {
+                        prependToLog(
+                                "Fetched visitor stats for " + visitorStatsMap.size + " " +
+                                        event.granularity.toString().toLowerCase() + " from " + site.name +
+                                        " with quantity " + event.quantity + " and date " + event.date
+                        )
+                    } else {
+                        prependToLog(
+                                "Fetched visitor stats for " + visitorStatsMap.size + " " +
+                                        event.granularity.toString().toLowerCase() + " from " + site.name
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showSiteSelectorDialog(selectedPos: Int, listener: StoreSelectorDialog.Listener) {
         fragmentManager?.let { fm ->
-            val dialog = SiteSelectorDialog.newInstance(listener, selectedPos)
-            dialog.show(fm, "SiteSelectorDialog")
+            val dialog = StoreSelectorDialog.newInstance(listener, selectedPos)
+            dialog.show(fm, "StoreSelectorDialog")
         }
     }
 
@@ -176,5 +244,9 @@ class WooRevenueStatsFragment : Fragment() {
         fetch_current_month_revenue_stats_forced.isEnabled = enabled
         fetch_current_year_revenue_stats.isEnabled = enabled
         fetch_current_year_revenue_stats_forced.isEnabled = enabled
+        fetch_current_day_visitor_stats.isEnabled = enabled
+        fetch_current_week_visitor_stats_forced.isEnabled = enabled
+        fetch_current_month_visitor_stats.isEnabled = enabled
+        fetch_current_year_visitor_stats_forced.isEnabled = enabled
     }
 }
