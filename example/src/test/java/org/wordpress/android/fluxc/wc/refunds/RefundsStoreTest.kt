1969c10aab75edd29be5ae73dccb076ffc15e6c4
Ondrej Ruttkay <ondrej@automattic.com>
2019-08-13 12:05:41 +0200

    WIP: RefundStore unit tests

--- example/src/test/java/org/wordpress/android/fluxc/wc/refunds/RefundsStoreTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/refunds/RefundsStoreTest.kt
@@ -6,6 +6,7 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
+import com.yarolegovich.wellsql.WellSql
 import kotlinx.coroutines.Dispatchers.Unconfined
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
@@ -13,7 +14,13 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.WCOrderModel
+import org.wordpress.android.fluxc.model.WCOrderNoteModel
 import org.wordpress.android.fluxc.model.refunds.RefundModel
 import org.wordpress.android.fluxc.model.refunds.RefundsMapper
 import org.wordpress.android.fluxc.model.stats.time.ClicksModel
@@ -21,6 +28,7 @@ import org.wordpress.android.fluxc.network.rest.wpcom.stats.time.ClicksRestClien
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.refunds.RefundsRestClient
 import org.wordpress.android.fluxc.network.utils.StatsGranularity.DAYS
 import org.wordpress.android.fluxc.persistence.RefundsSqlUtils
+import org.wordpress.android.fluxc.persistence.WellSqlConfig
 import org.wordpress.android.fluxc.store.RefundsStore
 import org.wordpress.android.fluxc.store.RefundsStore.RefundsPayload
 import org.wordpress.android.fluxc.store.StatsStore.FetchStatsPayload
@@ -30,15 +38,25 @@ import org.wordpress.android.fluxc.test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
-@RunWith(MockitoJUnitRunner::class)
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner::class)
 class RefundsStoreTest {
-    @Mock lateinit var restClient: RefundsRestClient
-    @Mock lateinit var site: SiteModel
-    @Mock lateinit var mapper: RefundsMapper
+    val restClient = mock<RefundsRestClient>()
+    val site = mock<SiteModel>()
+    val mapper = mock<RefundsMapper>()
     private lateinit var store: RefundsStore
 
     @Before
     fun setUp() {
+        val appContext = RuntimeEnvironment.application.applicationContext
+        val config = SingleStoreWellSqlConfigForTests(
+                appContext,
+                listOf(RefundsSqlUtils.RefundsBuilder::class.java),
+                WellSqlConfig.ADDON_WOOCOMMERCE
+        )
+        WellSql.init(config)
+        config.reset()
+
         store = RefundsStore(
                 restClient,
                 Unconfined,
@@ -61,7 +79,49 @@ class RefundsStoreTest {
 
         val responseModel = store.fetchAllRefunds(site, orderId)
 
-        assertThat(responseModel.model).isEqualTo(model)
+        assertThat(responseModel.model).isEqualTo(listOf(model))
         verify(RefundsSqlUtils).insert(site, orderId, data.toList())
     }
+
+//    @Test
+//    fun `returns cached data per site`() = test {
+//        whenever(sqlUtils.hasFreshRequest(site, DAYS, DATE, ITEMS_TO_LOAD)).thenReturn(true)
+//        whenever(sqlUtils.select(site, DAYS, DATE)).thenReturn(CLICKS_RESPONSE)
+//        val model = mock<ClicksModel>()
+//        whenever(mapper.map(CLICKS_RESPONSE, limitMode)).thenReturn(model)
+//
+//        val forced = false
+//        val responseModel = store.fetchClicks(site, DAYS, limitMode, DATE, forced)
+//
+//        assertThat(responseModel.model).isEqualTo(model)
+//        assertThat(responseModel.cached).isTrue()
+//        verify(sqlUtils, never()).insert(any(), any(), any(), any(), isNull())
+//    }
+//
+//    @Test
+//    fun `returns error when clicks call fail`() = test {
+//        val type = API_ERROR
+//        val message = "message"
+//        val errorPayload = FetchStatsPayload<ClicksResponse>(StatsError(type, message))
+//        val forced = true
+//        whenever(restClient.fetchClicks(site, DAYS, DATE, ITEMS_TO_LOAD + 1, forced)).thenReturn(errorPayload)
+//
+//        val responseModel = store.fetchClicks(site, DAYS, limitMode, DATE, forced)
+//
+//        assertNotNull(responseModel.error)
+//        val error = responseModel.error!!
+//        assertEquals(type, error.type)
+//        assertEquals(message, error.message)
+//    }
+//
+//    @Test
+//    fun `returns clicks from db`() {
+//        whenever(sqlUtils.select(site, DAYS, DATE)).thenReturn(CLICKS_RESPONSE)
+//        val model = mock<ClicksModel>()
+//        whenever(mapper.map(CLICKS_RESPONSE, limitMode)).thenReturn(model)
+//
+//        val result = store.getClicks(site, DAYS, limitMode, DATE)
+//
+//        assertThat(result).isEqualTo(model)
+//    }
 }
