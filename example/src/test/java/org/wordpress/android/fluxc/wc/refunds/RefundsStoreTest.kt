01549f0e23309a506eab631f345ccf8b99759376
Ondrej Ruttkay <ondrej@automattic.com>
2019-08-26 23:56:17 +0200

    Add additional unit tests

--- example/src/test/java/org/wordpress/android/fluxc/wc/refunds/RefundsStoreTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/refunds/RefundsStoreTest.kt
@@ -13,21 +13,30 @@ import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.refunds.RefundModel
 import org.wordpress.android.fluxc.model.refunds.RefundsMapper
+import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.NOT_FOUND
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.refunds.RefundsRestClient
 import org.wordpress.android.fluxc.persistence.RefundsSqlUtils
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 import org.wordpress.android.fluxc.store.RefundsStore
+import org.wordpress.android.fluxc.store.RefundsStore.RefundsResult
+import org.wordpress.android.fluxc.store.RefundsStore.RefundsError
+import org.wordpress.android.fluxc.store.RefundsStore.RefundsErrorType.INVALID_ID
 import org.wordpress.android.fluxc.store.RefundsStore.RefundsPayload
 import org.wordpress.android.fluxc.test
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 class RefundsStoreTest {
-    val restClient = mock<RefundsRestClient>()
-    val site = mock<SiteModel>()
-    val mapper = RefundsMapper()
+    private val restClient = mock<RefundsRestClient>()
+    private val site = mock<SiteModel>()
+    private val mapper = RefundsMapper()
     private lateinit var store: RefundsStore
+
+    private val orderId = 1L
+    private val refundId = REFUND_RESPONSE.refundId
+    private val error = RefundsError(INVALID_ID, NOT_FOUND, "Invalid order ID")
 
     @Before
     fun setUp() {
@@ -48,62 +57,71 @@ class RefundsStoreTest {
     }
 
     @Test
-    fun `returns refunds of an order`() = test {
-        val data = arrayOf(REFUND_RESPONSE)
+    fun `fetch all refunds of an order`() = test {
+        val data = arrayOf(REFUND_RESPONSE, REFUND_RESPONSE)
+        val result = fetchAllTestRefunds()
+
+        assertThat(result.model?.size).isEqualTo(data.size)
+        assertThat(result.model?.first()).isEqualTo(mapper.map(data.first()))
+
+        val invalidRequestResult = store.fetchAllRefunds(site, 2)
+        assertThat(invalidRequestResult.model).isNull()
+        assertThat(invalidRequestResult.error).isEqualTo(error)
+    }
+
+    @Test
+    fun `get all refunds of an order`() = test {
+        fetchAllTestRefunds()
+
+        val refunds = store.getAllRefunds(site, orderId)
+
+        assertThat(refunds.size).isEqualTo(1)
+        assertThat(refunds.first()).isEqualTo(mapper.map(REFUND_RESPONSE))
+
+        val invalidRequestResult = store.getAllRefunds(site, 2)
+        assertThat(invalidRequestResult.size).isEqualTo(0)
+    }
+
+    @Test
+    fun `fetch specific refund`() = test {
+        val refund = fetchSpecificTestRefund()
+
+        assertThat(refund.model).isEqualTo(mapper.map(REFUND_RESPONSE))
+    }
+
+    @Test
+    fun `get specific refund`() = test {
+        fetchSpecificTestRefund()
+        val refund = store.getRefund(site, orderId, refundId)
+
+        assertThat(refund).isEqualTo(mapper.map(REFUND_RESPONSE))
+    }
+
+    private suspend fun fetchSpecificTestRefund(): RefundsResult<RefundModel> {
         val fetchRefundsPayload = RefundsPayload(
-                data
+                REFUND_RESPONSE
         )
-        val orderId = 1L
-        whenever(restClient.fetchAllRefunds(site, orderId)).thenReturn(
+        whenever(restClient.fetchRefund(site, orderId, refundId)).thenReturn(
                 fetchRefundsPayload
         )
 
-        val result = store.fetchAllRefunds(site, orderId)
-
-        val refunds = RefundsSqlUtils.selectAllRefunds(site, orderId)
-        assertThat(refunds.size).isEqualTo(data.size)
-        assertThat(mapper.map(refunds.first())).isEqualTo(result.model?.first())
+        whenever(restClient.fetchRefund(site, 2, refundId)).thenReturn(
+                RefundsPayload(error)
+        )
+        return store.fetchRefund(site, orderId, refundId)
     }
 
-//    @Test
-//    fun `returns cached data per site`() = test {
-//        whenever(sqlUtils.hasFreshRequest(site, DAYS, DATE, ITEMS_TO_LOAD)).thenReturn(true)
-//        whenever(sqlUtils.select(site, DAYS, DATE)).thenReturn(CLICKS_RESPONSE)
-//        val model = mock<ClicksModel>()
-//        whenever(mapper.map(CLICKS_RESPONSE, limitMode)).thenReturn(model)
-//
-//        val forced = false
-//        val responseModel = store.fetchClicks(site, DAYS, limitMode, DATE, forced)
-//
-//        assertThat(responseModel.model).isEqualTo(model)
-//        assertThat(responseModel.cached).isTrue()
-//        verify(sqlUtils, never()).insert(any(), any(), any(), any(), isNull())
-//    }
-//
-//    @Test
-//    fun `returns error when clicks call fail`() = test {
-//        val type = API_ERROR
-//        val message = "message"
-//        val errorPayload = FetchStatsPayload<ClicksResponse>(StatsError(type, message))
-//        val forced = true
-//        whenever(restClient.fetchClicks(site, DAYS, DATE, ITEMS_TO_LOAD + 1, forced)).thenReturn(errorPayload)
-//
-//        val responseModel = store.fetchClicks(site, DAYS, limitMode, DATE, forced)
-//
-//        assertNotNull(responseModel.error)
-//        val error = responseModel.error!!
-//        assertEquals(type, error.type)
-//        assertEquals(message, error.message)
-//    }
-//
-//    @Test
-//    fun `returns clicks from db`() {
-//        whenever(sqlUtils.select(site, DAYS, DATE)).thenReturn(CLICKS_RESPONSE)
-//        val model = mock<ClicksModel>()
-//        whenever(mapper.map(CLICKS_RESPONSE, limitMode)).thenReturn(model)
-//
-//        val result = store.getClicks(site, DAYS, limitMode, DATE)
-//
-//        assertThat(result).isEqualTo(model)
-//    }
+    private suspend fun fetchAllTestRefunds(): RefundsResult<List<RefundModel>> {
+        val data = arrayOf(REFUND_RESPONSE, REFUND_RESPONSE)
+        val fetchRefundsPayload = RefundsPayload(
+                data
+        )
+        whenever(restClient.fetchAllRefunds(site, orderId)).thenReturn(
+                fetchRefundsPayload
+        )
+        whenever(restClient.fetchAllRefunds(site, 2)).thenReturn(
+                RefundsPayload(error)
+        )
+        return store.fetchAllRefunds(site, orderId)
+    }
 }
