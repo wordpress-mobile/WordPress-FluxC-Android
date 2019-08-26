180c1e7bcf49500d2ca2ecb4d88b4e71629be98e
Ondrej Ruttkay <ondrej@automattic.com>
2019-08-26 13:34:43 +0200

    Fix the order refund unit test

--- example/src/test/java/org/wordpress/android/fluxc/wc/refunds/RefundsStoreTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/refunds/RefundsStoreTest.kt
@@ -1,10 +1,6 @@
 package org.wordpress.android.fluxc.wc.refunds
 
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.isNull
 import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.never
-import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import com.yarolegovich.wellsql.WellSql
 import kotlinx.coroutines.Dispatchers.Unconfined
@@ -12,38 +8,25 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.model.WCOrderModel
-import org.wordpress.android.fluxc.model.WCOrderNoteModel
-import org.wordpress.android.fluxc.model.refunds.RefundModel
 import org.wordpress.android.fluxc.model.refunds.RefundsMapper
-import org.wordpress.android.fluxc.model.stats.time.ClicksModel
-import org.wordpress.android.fluxc.network.rest.wpcom.stats.time.ClicksRestClient.ClicksResponse
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.refunds.RefundsRestClient
-import org.wordpress.android.fluxc.network.utils.StatsGranularity.DAYS
 import org.wordpress.android.fluxc.persistence.RefundsSqlUtils
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 import org.wordpress.android.fluxc.store.RefundsStore
 import org.wordpress.android.fluxc.store.RefundsStore.RefundsPayload
-import org.wordpress.android.fluxc.store.StatsStore.FetchStatsPayload
-import org.wordpress.android.fluxc.store.StatsStore.StatsError
-import org.wordpress.android.fluxc.store.StatsStore.StatsErrorType.API_ERROR
 import org.wordpress.android.fluxc.test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner::class)
 class RefundsStoreTest {
     val restClient = mock<RefundsRestClient>()
     val site = mock<SiteModel>()
-    val mapper = mock<RefundsMapper>()
+    val mapper = RefundsMapper()
     private lateinit var store: RefundsStore
 
     @Before
@@ -74,13 +57,12 @@ class RefundsStoreTest {
         whenever(restClient.fetchAllRefunds(site, orderId)).thenReturn(
                 fetchRefundsPayload
         )
-        val model = mock<RefundModel>()
-        whenever(mapper.map(REFUND_RESPONSE)).thenReturn(model)
 
-        val responseModel = store.fetchAllRefunds(site, orderId)
+        val result = store.fetchAllRefunds(site, orderId)
 
-        assertThat(responseModel.model).isEqualTo(listOf(model))
-        verify(RefundsSqlUtils).insert(site, orderId, data.toList())
+        val refunds = RefundsSqlUtils.selectAllRefunds(site, orderId)
+        assertThat(refunds.size).isEqualTo(data.size)
+        assertThat(mapper.map(refunds.first())).isEqualTo(result.model?.first())
     }
 
 //    @Test
