430791413343df6e44367f0f231b04ee670a64d1
Oguz Kocer <oguz.kocer@automattic.com>
2019-10-09 14:45:40 -0400

    Implement equals and hashCode for LocalOrRemoteId

--- fluxc/src/main/java/org/wordpress/android/fluxc/model/LocalOrRemoteId.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/model/LocalOrRemoteId.kt
@@ -3,4 +3,22 @@ package org.wordpress.android.fluxc.model
 sealed class LocalOrRemoteId {
     data class LocalId(val value: Int) : LocalOrRemoteId()
     data class RemoteId(val value: Long) : LocalOrRemoteId()
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (this is LocalId && other is LocalId) {
+            return this.value == other.value
+        }
+        if (this is RemoteId && other is RemoteId) {
+            return this.value == other.value
+        }
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return when(this) {
+            is LocalId -> this.hashCode()
+            is RemoteId -> this.hashCode()
+        }
+    }
 }
