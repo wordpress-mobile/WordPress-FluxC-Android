bd5a185fdb319fdb8f6515dea65580347645a213
anitaa <murthyanitaa@gmail.com>
2019-12-17 23:21:04 +0530

    Added new store, rest client class and mapper for fetching/storing tax classes

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WCTaxStore.kt
@@ -0,0 +1,48 @@
+package org.wordpress.android.fluxc.store
+
+import kotlinx.coroutines.withContext
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.taxes.WCTaxClassMapper
+import org.wordpress.android.fluxc.model.taxes.WCTaxClassModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.taxes.WCTaxRestClient
+import org.wordpress.android.fluxc.persistence.WCTaxSqlUtils
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.CoroutineContext
+import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.UNKNOWN
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType.GENERIC_ERROR
+
+@Singleton
+class WCTaxStore @Inject constructor(
+    private val restClient: WCTaxRestClient,
+    private val coroutineContext: CoroutineContext,
+    private val mapper: WCTaxClassMapper
+) {
+    /**
+     * returns a list of tax classes for a specific site in the database
+     */
+    fun getShippingClassListForSite(site: SiteModel): List<WCTaxClassModel> =
+            WCTaxSqlUtils.getTaxClassesForSite(site.id)
+
+    suspend fun fetchTaxClassList(site: SiteModel): WooResult<List<WCTaxClassModel>> {
+        return withContext(coroutineContext) {
+            val response = restClient.fetchTaxClassList(site)
+            return@withContext when {
+                response.isError -> {
+                    WooResult(response.error)
+                }
+                response.result != null -> {
+                    val taxClassModels = response.result.map {
+                        mapper.map(it).apply { localSiteId = site.id }
+                    }
+
+                    WCTaxSqlUtils.insertOrUpdateTaxClasses(taxClassModels)
+                    WooResult(taxClassModels)
+                }
+                else -> WooResult(WooError(GENERIC_ERROR, UNKNOWN))
+            }
+        }
+    }
+}
