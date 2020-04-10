d3b1dacacf3897e617187c3da0060097b9a3412d
anitaa <murthyanitaa@gmail.com>
2020-04-10 18:43:48 +0530

    Added enum classes for product status and product type

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/product/CoreProductStatus.kt
@@ -0,0 +1,17 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.wc.product
+
+enum class CoreProductStatus(val value: String) {
+    DRAFT("draft"),
+    PENDING("pending"),
+    PRIVATE("private"),
+    PUBLISH("publish");
+
+    companion object {
+        private val valueMap = values().associateBy(CoreProductStatus::value)
+
+        /**
+         * Convert the base value into the associated CoreProductStatus object
+         */
+        fun fromValue(value: String) = valueMap[value]
+    }
+}
