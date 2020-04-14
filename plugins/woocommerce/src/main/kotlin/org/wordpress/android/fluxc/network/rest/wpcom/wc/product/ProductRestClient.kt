76a8a04af8127629f69a22c50da33bed8a31a9f6
Nick Bradbury <nick.bradbury@gmail.com>
2020-04-14 13:18:30 -0400

    Added product visibility and featured when updating a product

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/product/ProductRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/product/ProductRestClient.kt
@@ -589,6 +589,12 @@ class ProductRestClient(
         if (storedWCProductModel.status != updatedProductModel.status) {
             body["status"] = updatedProductModel.status
         }
+        if (storedWCProductModel.catalogVisibility != updatedProductModel.catalogVisibility) {
+            body["catalog_visibility"] = updatedProductModel.catalogVisibility
+        }
+        if (storedWCProductModel.featured != updatedProductModel.featured) {
+            body["featured"] = updatedProductModel.featured
+        }
         if (storedWCProductModel.manageStock != updatedProductModel.manageStock) {
             body["manage_stock"] = updatedProductModel.manageStock
         }
