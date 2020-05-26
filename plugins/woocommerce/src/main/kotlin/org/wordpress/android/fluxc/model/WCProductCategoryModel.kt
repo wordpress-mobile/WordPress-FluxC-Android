87ba55f71163a2b3577ffdb5143551b4df29cb4f
anitaa <murthyanitaa@gmail.com>
2020-05-26 12:16:41 +0530

    Updated create table script when creating WCProductCategoryModel

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCProductCategoryModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCProductCategoryModel.kt
@@ -3,10 +3,15 @@ package org.wordpress.android.fluxc.model
 import com.yarolegovich.wellsql.core.Identifiable
 import com.yarolegovich.wellsql.core.annotation.Column
 import com.yarolegovich.wellsql.core.annotation.PrimaryKey
+import com.yarolegovich.wellsql.core.annotation.RawConstraints
 import com.yarolegovich.wellsql.core.annotation.Table
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 
 @Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
+@RawConstraints(
+        "FOREIGN KEY(LOCAL_SITE_ID) REFERENCES SiteModel(_id) ON DELETE CASCADE",
+        "UNIQUE (REMOTE_CATEGORY_ID, LOCAL_SITE_ID) ON CONFLICT REPLACE"
+)
 data class WCProductCategoryModel(@PrimaryKey @Column private var id: Int = 0) : Identifiable {
     @Column var localSiteId = 0
     @Column var remoteCategoryId = 0L // The unique identifier for this category on the server
