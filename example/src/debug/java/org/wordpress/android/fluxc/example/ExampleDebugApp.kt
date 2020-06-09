3aa5a160224e7597abc972bdbddd414a739f2e2e
Maxime Biais <maxime.biais@gmail.com>
2020-06-09 09:36:18 +0200

    Replace Stetho by Flipper, add the Layout and Network plugins

--- example/src/debug/java/org/wordpress/android/fluxc/example/ExampleDebugApp.kt
+++ example/src/debug/java/org/wordpress/android/fluxc/example/ExampleDebugApp.kt
@@ -1,6 +1,11 @@
 package org.wordpress.android.fluxc.example
 
-import com.facebook.stetho.Stetho
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.soloader.SoLoader
 import org.wordpress.android.fluxc.example.di.AppComponent
 import org.wordpress.android.fluxc.example.di.DaggerAppComponentDebug
 
@@ -13,6 +18,15 @@ class ExampleDebugApp : ExampleApp() {
 
     override fun onCreate() {
         super.onCreate()
-        Stetho.initializeWithDefaults(this)
+        component.inject(this)
+        SoLoader.init(this, false)
+
+        if (FlipperUtils.shouldEnableFlipper(this)) {
+            val client = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(applicationContext, DescriptorMapping.withDefaults()))
+            client.addPlugin(NetworkFlipperPlugin())
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.start()
+        }
     }
 }
