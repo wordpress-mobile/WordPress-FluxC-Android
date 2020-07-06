fa110117be5466f2c225e91426571c8575e9841d
Oguz Kocer <oguz.kocer@automattic.com>
2020-07-06 09:58:54 +0300

    A very basic connected test for encrypted logging

---
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_EncryptedLogTest.kt
@@ -0,0 +1,36 @@
+package org.wordpress.android.fluxc.release
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.hamcrest.core.IsNull.nullValue
+import org.junit.Test
+import org.wordpress.android.fluxc.model.encryptedlogging.EncryptedLogUploadState.UPLOADING
+import org.wordpress.android.fluxc.persistence.EncryptedLogSqlUtils
+import org.wordpress.android.fluxc.store.EncryptedLogStore
+import javax.inject.Inject
+
+private const val TEST_UUID = "TEST-UUID"
+
+class ReleaseStack_EncryptedLogTest: ReleaseStack_Base() {
+    @Inject lateinit var encryptedLogStore: EncryptedLogStore
+    @Inject lateinit var encryptedLogSqlUtils: EncryptedLogSqlUtils
+
+    @Throws(Exception::class)
+    override fun setUp() {
+        super.setUp()
+        mReleaseStackAppComponent.inject(this)
+    }
+
+    @Test
+    fun testQueueForUpload() {
+        runBlocking {
+            encryptedLogStore.queueLogForUpload(TEST_UUID, createTempFile(suffix = TEST_UUID))
+            assertThat(encryptedLogSqlUtils.getEncryptedLog(TEST_UUID)?.uploadState, `is`(UPLOADING))
+
+            delay(5000)
+            assertThat(encryptedLogSqlUtils.getEncryptedLog(TEST_UUID), nullValue())
+        }
+    }
+}
