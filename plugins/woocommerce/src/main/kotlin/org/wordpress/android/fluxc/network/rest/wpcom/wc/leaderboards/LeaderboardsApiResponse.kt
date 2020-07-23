81d00d7d0d5d4e3528694650553f0a12b3ff22dd
Thomaz Cortez <thomazfcortez@gmail.com>
2020-07-23 03:59:50 -0300

    Create HTML parsing and regex matching to achieve the parameter ID

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/leaderboards/LeaderboardsApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/leaderboards/LeaderboardsApiResponse.kt
@@ -1,37 +1,49 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards
 
+import android.text.Html
+import android.text.SpannableStringBuilder
+import android.text.style.URLSpan
 import org.wordpress.android.fluxc.network.Response
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.CATEGORIES
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.COUPONS
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.CUSTOMERS
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.PRODUCTS
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.UNKNOWN
 
 class LeaderboardsApiResponse : Response {
     private val rows: Array<Array<LeaderboardItem>>? = null
     val id: String? = null
     val label: String? = null
-    val type: Type?
-        get() = when (id) {
-            "customers" -> CUSTOMERS
-            "coupons" -> COUPONS
-            "categories" -> CATEGORIES
-            "products" -> PRODUCTS
-            else -> null
-        }
+    val type: Type
+        get() = Type.values()
+                .firstOrNull { it.value == id }
+                ?: UNKNOWN
     val items
         get() = rows
                 ?.firstOrNull()
                 ?.toList()
 
-    class LeaderboardItem {
+    inner class LeaderboardItem {
         val display: String? = null
         val value: String? = null
+        val link
+            get() = Html.fromHtml(display)
+                    .run { this as? SpannableStringBuilder }
+                    ?.spansAsList()
+                    ?.firstOrNull()
+                    ?.url
+
+        val itemId
+            get() = Regex("([?&])([^=]+)=([^&]+)")
+                    .takeIf { it.containsMatchIn(link.orEmpty()) }
+                    ?.find(link.orEmpty())
+
+        private fun SpannableStringBuilder.spansAsList() =
+                getSpans(0, length, URLSpan::class.java)
+                        .toList()
     }
 
-    enum class Type {
-        CUSTOMERS,
-        COUPONS,
-        CATEGORIES,
-        PRODUCTS
+    enum class Type(val value: String) {
+        CUSTOMERS("customers"),
+        COUPONS("coupons"),
+        CATEGORIES("categories"),
+        PRODUCTS("products"),
+        UNKNOWN("unknown")
     }
 }
