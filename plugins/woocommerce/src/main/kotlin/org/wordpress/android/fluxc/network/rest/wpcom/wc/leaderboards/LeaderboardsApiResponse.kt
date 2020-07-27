64a3156bffa98f54da4e8444005952a50661d4c9
Thomaz Cortez <thomazfcortez@gmail.com>
2020-07-27 02:43:55 -0300

    Redesign LeaderboardsApiResponse entities

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/leaderboards/LeaderboardsApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/leaderboards/LeaderboardsApiResponse.kt
@@ -7,38 +7,58 @@ import org.wordpress.android.fluxc.network.Response
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.UNKNOWN
 
 class LeaderboardsApiResponse : Response {
-    private val rows: Array<Array<LeaderboardItem>>? = null
+    private val rows: Array<Array<LeaderboardItemRow>>? = null
     val id: String? = null
     val label: String? = null
     val type: Type
         get() = Type.values()
                 .firstOrNull { it.value == id }
                 ?: UNKNOWN
-    val items
-        get() = rows
-                ?.firstOrNull()
+    val items by lazy {
+        rows
+                ?.map { LeaderboardItem(it) }
                 ?.toList()
+    }
 
-    class LeaderboardItem {
-        val display: String? = null
-        val value: String? = null
-        val link
-            get() = Html.fromHtml(display)
+    class LeaderboardItem(
+        val itemRows: Array<LeaderboardItemRow>? = null
+    ) {
+        val itemHtmlTag by lazy {
+            itemRows
+                    ?.first()
+                    ?.display
+        }
+
+        val itemName by lazy {
+            itemRows
+                    ?.first()
+                    ?.value
+        }
+
+        //TODO: fix deprecation issue
+        val link by lazy {
+            Html.fromHtml(itemHtmlTag)
                     .run { this as? SpannableStringBuilder }
                     ?.spansAsList()
                     ?.firstOrNull()
                     ?.url
+        }
 
         fun resolveItemIdByType(type: Type) = link
                 ?.split("&")
                 ?.firstOrNull { it.contains("${type.value}=", true) }
                 ?.split("=")
                 ?.last()
-                ?.toIntOrNull()
+                ?.toLongOrNull()
 
         private fun SpannableStringBuilder.spansAsList() =
                 getSpans(0, length, URLSpan::class.java)
                         .toList()
+    }
+
+    class LeaderboardItemRow {
+        val display: String? = null
+        val value: String? = null
     }
 
     enum class Type(val value: String) {
