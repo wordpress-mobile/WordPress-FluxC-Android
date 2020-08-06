e243fde21569b239782c9f5e920dd03d087ec7b3
Oguz Kocer <oguz.kocer@automattic.com>
2020-08-06 17:10:05 +0300

    Include newline while encrypting logs and add a unit test for it

--- example/src/androidTest/java/org/wordpress/android/fluxc/LogEncrypterTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/LogEncrypterTest.kt
@@ -15,6 +15,7 @@ import org.wordpress.android.fluxc.model.encryptedlogging.EncryptionUtils
 import org.wordpress.android.fluxc.model.encryptedlogging.LogEncrypter
 import org.wordpress.android.fluxc.model.encryptedlogging.SecretStreamKey
 import java.util.UUID
+import kotlin.random.Random.Default.nextInt
 
 class LogEncrypterTest {
     private lateinit var keypair: KeyPair
@@ -65,6 +66,12 @@ class LogEncrypterTest {
     @Test
     fun testThatLogsCanBeDecrypted() {
         val testLogString = UUID.randomUUID().toString()
+        assertEquals(testLogString, decryptContent(encryptContent(testLogString)))
+    }
+
+    @Test
+    fun testThatMultilineLogsCanBeDecrypted() {
+        val testLogString = (0..(nextInt(100) + 2)).joinToString(separator = "\n") { UUID.randomUUID().toString() }
         assertEquals(testLogString, decryptContent(encryptContent(testLogString)))
     }
 
