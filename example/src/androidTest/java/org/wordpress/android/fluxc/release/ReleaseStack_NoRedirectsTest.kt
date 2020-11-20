1368854d414bc2fae05b1f621cc4895da377c9de
Antonis Lilis <antonis.lilis@automattic.com>
2020-11-20 16:20:38 +0200

    Moved Temporary Redirect URL to mobile secrets

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_NoRedirectsTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_NoRedirectsTest.kt
@@ -7,13 +7,13 @@ import com.android.volley.toolbox.StringRequest
 import okhttp3.OkHttpClient
 import org.junit.Assert
 import org.junit.Test
+import org.wordpress.android.fluxc.example.BuildConfig.TEST_TEMPORARY_REDIRECT_307
 import org.wordpress.android.fluxc.network.OkHttpStack
 import org.wordpress.android.fluxc.network.RetryOnRedirectBasicNetwork
 import org.wordpress.android.fluxc.network.RetryOnRedirectBasicNetwork.HTTP_TEMPORARY_REDIRECT
 import javax.inject.Inject
 import javax.inject.Named
 
-private const val URL = "http://do.wpmt.co/307-redirect-test/timer.php"
 private const val RETRIES = 2
 private const val TIMEOUT = 100
 private const val BACKOFF_MULTIPLIER = 1f
@@ -29,7 +29,7 @@ class ReleaseStack_NoRedirectsTest : ReleaseStack_Base() {
     @Test
     fun testRetriesOccurOnRedirect() {
         val network = RetryOnRedirectBasicNetwork(OkHttpStack(okHttpClientBuilder))
-        val request = StringRequest(Request.Method.GET, URL, null, null)
+        val request = StringRequest(Request.Method.GET, TEST_TEMPORARY_REDIRECT_307, null, null)
         request.retryPolicy = DefaultRetryPolicy(TIMEOUT, RETRIES, BACKOFF_MULTIPLIER)
         val response = try {
             network.performRequest(request)
