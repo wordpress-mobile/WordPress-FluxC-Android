18dffe3c22587dda7f846c69cac0205743710e0b
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2020-11-26 13:25:21 +0200

    Change order by logic for getting product variation from the database
    This is done to align with the api request, which is also ordered by
    'date' and 'asc', see previous commit. Without this alignment, the
    pagination feature on the UI cannot work as expected.
    
    This fix commit is part of the solution for this linked issue below:
    https://github.com/woocommerce/woocommerce-android/issues/2848

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/ProductSqlUtils.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/ProductSqlUtils.kt
@@ -247,7 +247,7 @@ object ProductSqlUtils {
                 .equals(WCProductVariationModelTable.REMOTE_PRODUCT_ID, remoteProductId)
                 .equals(WCProductVariationModelTable.LOCAL_SITE_ID, site.id)
                 .endGroup().endWhere()
-                .orderBy(WCProductVariationModelTable.MENU_ORDER, SelectQuery.ORDER_ASCENDING)
+                .orderBy(WCProductVariationModelTable.DATE_CREATED, SelectQuery.ORDER_ASCENDING)
                 .asModel
     }
 
