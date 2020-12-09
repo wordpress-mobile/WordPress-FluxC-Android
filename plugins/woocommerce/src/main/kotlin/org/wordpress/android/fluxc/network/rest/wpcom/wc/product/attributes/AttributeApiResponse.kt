e8020cbf8ac9d4ef0e3ef77f6a176858a950257b
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-09 03:02:16 -0300

    Create AttributeApiResponse to represent a single JSON Attribute

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/product/attributes/AttributeApiResponse.kt
@@ -0,0 +1,10 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.wc.product.attributes
+
+import org.wordpress.android.fluxc.network.Response
+
+class AttributeApiResponse : Response {
+    val id: String? = null
+    val name: String? = null
+    val slug: String? = null
+    val type: String? = null
+}
