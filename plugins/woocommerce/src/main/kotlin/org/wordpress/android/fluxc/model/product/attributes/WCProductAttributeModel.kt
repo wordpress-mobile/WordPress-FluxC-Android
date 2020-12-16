29f1b9aef615b1e4ab64601897b43a1288cccabe
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-16 01:03:27 -0300

    Define WCProductAttributeModel fields with SQL support

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
@@ -1,4 +1,23 @@
 package org.wordpress.android.fluxc.model.product.attributes
 
-class WCProductAttributeModel {
+import com.yarolegovich.wellsql.core.Identifiable
+import com.yarolegovich.wellsql.core.annotation.Column
+import com.yarolegovich.wellsql.core.annotation.PrimaryKey
+import com.yarolegovich.wellsql.core.annotation.Table
+import org.wordpress.android.fluxc.persistence.WellSqlConfig
+
+@Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
+data class WCProductAttributeModel(
+    @PrimaryKey @Column private var id: Int = 0,
+    @Column var name: String = "",
+    @Column var slug: String = "",
+    @Column var type: String = "",
+    @Column var orderBy: String = "",
+    @Column var hasArchives: Boolean = false
+): Identifiable {
+    override fun setId(id: Int) {
+        this.id = id
+    }
+
+    override fun getId() = id
 }
