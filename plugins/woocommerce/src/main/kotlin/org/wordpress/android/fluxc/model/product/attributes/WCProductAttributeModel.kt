63c685bdc55b9c19f14b2197de7f6d5509d3c93e
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-16 01:33:50 -0300

    Revert SQL tags to avoid incompatibility crashes

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
@@ -6,14 +6,13 @@ import com.yarolegovich.wellsql.core.annotation.PrimaryKey
 import com.yarolegovich.wellsql.core.annotation.Table
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 
-@Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
 data class WCProductAttributeModel(
-    @PrimaryKey @Column private var id: Int = 0,
-    @Column var name: String = "",
-    @Column var slug: String = "",
-    @Column var type: String = "",
-    @Column var orderBy: String = "",
-    @Column var hasArchives: Boolean = false
+    private var id: Int = 0,
+    var name: String = "",
+    var slug: String = "",
+    var type: String = "",
+    var orderBy: String = "",
+    var hasArchives: Boolean = false
 ): Identifiable {
     override fun setId(id: Int) {
         this.id = id
