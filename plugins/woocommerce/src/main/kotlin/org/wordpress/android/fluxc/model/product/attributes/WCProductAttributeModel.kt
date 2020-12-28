30334b4b5f8efcf997757a075b506c98c936ad27
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-28 16:20:10 -0300

    Make WCProductAttributeModel compatible with WellSQL

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
@@ -1,14 +1,20 @@
 package org.wordpress.android.fluxc.model.product.attributes
 
 import com.yarolegovich.wellsql.core.Identifiable
+import com.yarolegovich.wellsql.core.annotation.Column
+import com.yarolegovich.wellsql.core.annotation.PrimaryKey
+import com.yarolegovich.wellsql.core.annotation.Table
+import org.wordpress.android.fluxc.persistence.WellSqlConfig
 
+@Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
 data class WCProductAttributeModel(
-    private var id: Int = 0,
-    var name: String = "",
-    var slug: String = "",
-    var type: String = "",
-    var orderBy: String = "",
-    var hasArchives: Boolean = false
+    @PrimaryKey @Column private var id: Int = 0,
+    @Column var localSiteId: Int = 0,
+    @Column var name: String = "",
+    @Column var slug: String = "",
+    @Column var type: String = "",
+    @Column var orderBy: String = "",
+    @Column var hasArchives: Boolean = false
 ) : Identifiable {
     override fun setId(id: Int) {
         this.id = id
