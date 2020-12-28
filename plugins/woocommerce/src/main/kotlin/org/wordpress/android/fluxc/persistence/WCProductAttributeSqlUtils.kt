d748f63379f67ca5b83a06fd2666714e5a9fe0b5
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-28 16:23:15 -0300

    Create basic transactions for Attributes SQL table

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCProductAttributeSqlUtils.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCProductAttributeSqlUtils.kt
@@ -1,7 +1,29 @@
 package org.wordpress.android.fluxc.persistence
 
+import com.wellsql.generated.WCProductAttributeModelTable
+import com.yarolegovich.wellsql.WellSql
+import org.wordpress.android.fluxc.model.product.attributes.WCProductAttributeModel
+
 object WCProductAttributeSqlUtils {
-    fun getCurrentAttributes() {}
-    fun deleteCurrentAttributes() {}
-    fun insertNewAttributes() {}
+    fun getCompleteAttributesList(siteID: Int) =
+            WellSql.select(WCProductAttributeModel::class.java)
+                    .where()
+                    .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
+                    .endWhere()
+                    .asModel
+                    ?.toList()
+                    .orEmpty()
+
+    fun deleteCompleteAttributesList(siteID: Int) =
+            WellSql.delete(WCProductAttributeModel::class.java)
+                    .where()
+                    .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
+                    .endWhere()
+                    .execute()
+
+    fun insertFromScratchCompleteAttributesList(attributes: List<WCProductAttributeModel>, siteID: Int) {
+        deleteCompleteAttributesList(siteID)
+        WellSql.insert(attributes)
+                .asSingleTransaction(true).execute()
+    }
 }
