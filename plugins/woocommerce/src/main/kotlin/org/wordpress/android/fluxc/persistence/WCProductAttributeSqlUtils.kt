98c39a33333fa0e63578e153b7b9f00fa3d6e9cb
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-29 08:03:47 -0300

    Create SQL CRUD for single Attribute operations

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCProductAttributeSqlUtils.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCProductAttributeSqlUtils.kt
@@ -5,7 +5,7 @@ import com.yarolegovich.wellsql.WellSql
 import org.wordpress.android.fluxc.model.product.attributes.WCProductAttributeModel
 
 object WCProductAttributeSqlUtils {
-    fun getCompleteAttributesList(siteID: Int) =
+    fun getCurrentAttributes(siteID: Int) =
             WellSql.select(WCProductAttributeModel::class.java)
                     .where()
                     .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
@@ -25,5 +25,29 @@ object WCProductAttributeSqlUtils {
         deleteCompleteAttributesList(siteID)
         WellSql.insert(attributes)
                 .asSingleTransaction(true).execute()
+    }
+
+    fun insertSingleAttribute(attribute: WCProductAttributeModel) = attribute.apply {
+        WellSql.insert(attribute)
+                .asSingleTransaction(true)
+                .execute()
+    }
+
+    fun deleteSingleStoredAttribute(attribute: WCProductAttributeModel, siteID: Int) = attribute.apply {
+        WellSql.delete(WCProductAttributeModel::class.java)
+                .where()
+                .equals(WCProductAttributeModelTable.ID, attribute.id)
+                .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
+                .endWhere()
+                .execute()
+    }
+
+    fun updateSingleStoredAttribute(attribute: WCProductAttributeModel, siteID: Int) = attribute.apply {
+        WellSql.update(WCProductAttributeModel::class.java)
+                .where()
+                .equals(WCProductAttributeModelTable.ID, attribute.id)
+                .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
+                .endWhere()
+                .execute()
     }
 }
