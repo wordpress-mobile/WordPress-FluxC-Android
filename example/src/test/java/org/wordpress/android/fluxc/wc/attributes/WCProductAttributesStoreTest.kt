44bf04c5f9e34f3b4fbd283cd79ae0b2446bca17
Thomaz Cortez <thomazfcortez@gmail.com>
2020-12-31 19:17:33 -0300

    Create AttributesStore base unit tests structure

---
+++ example/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesStoreTest.kt
@@ -0,0 +1,17 @@
+package org.wordpress.android.fluxc.wc.attributes
+
+import org.junit.Test
+
+class WCProductAttributesStoreTest {
+    @Test
+    fun `fetch attributes with empty result should return WooError`() {}
+
+    @Test
+    fun `fetch attributes should call mapper once`() {}
+
+    @Test
+    fun `fetch attributes should return WooResult correctly`() {}
+
+    @Test
+    fun `fetch attributes from a invalid site ID should return WooResult with error`() {}
+}
