a199b62b614bcfe36503a0754f0967e69d8abb8a
Thomaz Cortez <thomazfcortez@gmail.com>
2021-01-04 23:10:05 -0300

    Create WCProductAttributesStoreTest test setup configuration

--- example/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesStoreTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesStoreTest.kt
@@ -1,17 +1,68 @@
 package org.wordpress.android.fluxc.wc.attributes
 
+import com.nhaarman.mockitokotlin2.mock
+import com.yarolegovich.wellsql.WellSql
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.wordpress.android.fluxc.SingleStoreWellSqlConfigForTests
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.product.attributes.WCProductAttributeMapper
+import org.wordpress.android.fluxc.model.product.attributes.WCProductAttributeModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.attributes.ProductAttributeRestClient
+import org.wordpress.android.fluxc.persistence.WellSqlConfig
+import org.wordpress.android.fluxc.store.WCProductAttributesStore
+import org.wordpress.android.fluxc.tools.initCoroutineEngine
 
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner::class)
 class WCProductAttributesStoreTest {
-    @Test
-    fun `fetch attributes with empty result should return WooError`() {}
+    private lateinit var storeUnderTest: WCProductAttributesStore
+    private lateinit var restClient: ProductAttributeRestClient
+    private lateinit var mapper: WCProductAttributeMapper
+
+    @Before
+    fun setUp() {
+        val appContext = RuntimeEnvironment.application.applicationContext
+        val config = SingleStoreWellSqlConfigForTests(
+                appContext,
+                listOf(SiteModel::class.java, WCProductAttributeModel::class.java),
+                WellSqlConfig.ADDON_WOOCOMMERCE
+        )
+        WellSql.init(config)
+        config.reset()
+        initMocks()
+        createStoreUnderTest()
+    }
 
     @Test
-    fun `fetch attributes should call mapper once`() {}
+    fun `fetch attributes with empty result should return WooError`() {
+    }
 
     @Test
-    fun `fetch attributes should return WooResult correctly`() {}
+    fun `fetch attributes should call mapper once`() {
+    }
 
     @Test
-    fun `fetch attributes from a invalid site ID should return WooResult with error`() {}
+    fun `fetch attributes should return WooResult correctly`() {
+    }
+
+    @Test
+    fun `fetch attributes from a invalid site ID should return WooResult with error`() {
+    }
+
+    private fun initMocks() {
+        restClient = mock()
+        mapper = mock()
+    }
+
+    private fun createStoreUnderTest() =
+            WCProductAttributesStore(
+                    restClient,
+                    mapper,
+                    initCoroutineEngine()
+            ).apply { storeUnderTest = this }
 }
