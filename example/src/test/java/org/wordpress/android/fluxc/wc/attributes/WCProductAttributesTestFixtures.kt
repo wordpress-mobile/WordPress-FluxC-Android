7bef16d28563e6fb39a3cada2a644f30a9e985f3
Thomaz Cortez <thomazfcortez@gmail.com>
2021-01-07 23:42:11 -0300

    Create attribute sample response initialization

--- example/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesTestFixtures.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/attributes/WCProductAttributesTestFixtures.kt
@@ -1,7 +1,25 @@
 package org.wordpress.android.fluxc.wc.attributes
 
+import com.google.gson.Gson
+import org.wordpress.android.fluxc.UnitTestUtils
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.attributes.AttributeApiResponse
 
 object WCProductAttributesTestFixtures {
     val stubSite = SiteModel().apply { id = 321 }
+
+    val attributeDeleteResponse
+        get() = "wc/product-attribute-delete.json".fileNameToJson()
+
+    val attributeCreateResponse
+        get() = "wc/product-attribute-create.json".fileNameToJson()
+
+    val attributeUpdateResponse
+        get() = "wc/product-attribute-update.json".fileNameToJson()
+
+    private fun String.fileNameToJson() =
+            UnitTestUtils.getStringFromResourceFile(
+                    this@WCProductAttributesTestFixtures.javaClass,
+                    this
+            )?.let { Gson().fromJson(it, AttributeApiResponse::class.java) }
 }
