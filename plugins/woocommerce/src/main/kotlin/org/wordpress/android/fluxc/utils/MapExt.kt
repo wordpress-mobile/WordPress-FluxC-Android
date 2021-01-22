f3dea3c4dd74de4b0a0f4496ffcd768f7f0a65a3
Thomaz Cortez <thomazfcortez@gmail.com>
2021-01-22 12:31:28 -0300

    Create varargs support for putIfNotEmpty method

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/utils/MapExt.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/utils/MapExt.kt
@@ -1,6 +1,8 @@
 package org.wordpress.android.fluxc.utils
 
-fun <T> MutableMap<T, String>.putIfNotEmpty(pair: Pair<T, String?>) = apply {
-    pair.second?.takeIf { it.isNotEmpty() }
-            ?.let { put(pair.first, it) }
+fun <T> MutableMap<T, String>.putIfNotEmpty(vararg pairs: Pair<T, String?>) = apply {
+    pairs.forEach { pair ->
+        pair.second?.takeIf { it.isNotEmpty() }
+                ?.let { put(pair.first, it) }
+    }
 }
