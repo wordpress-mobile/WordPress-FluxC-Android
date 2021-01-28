f771ba64aee2004f00e1d443960d8e2656c3fc57
Thomaz Cortez <thomazfcortez@gmail.com>
2021-01-27 21:51:27 -0300

    Assign site ID when mapping Attributes

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeMapper.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeMapper.kt
@@ -1,14 +1,17 @@
 package org.wordpress.android.fluxc.model.product.attributes
 
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.attributes.AttributeApiResponse
 import javax.inject.Inject
 
 class WCProductAttributeMapper @Inject constructor() {
     fun mapToAttributeModel(
-        response: AttributeApiResponse
+        response: AttributeApiResponse,
+        site: SiteModel
     ) = with(response) {
         WCProductAttributeModel(
                 id = id?.toIntOrNull() ?: 0,
+                localSiteId = site.id,
                 name = name.orEmpty(),
                 slug = slug.orEmpty(),
                 type = type.orEmpty(),
@@ -18,7 +21,8 @@ class WCProductAttributeMapper @Inject constructor() {
     }
 
     fun mapToAttributeModelList(
-        response: Array<AttributeApiResponse>
-    ) = response.map { mapToAttributeModel(it) }
+        response: Array<AttributeApiResponse>,
+        site: SiteModel
+    ) = response.map { mapToAttributeModel(it, site) }
             .toList()
 }
