49311de1cd3375e82e8c31437659f9fca74baeac
Thomaz Cortez <thomazfcortez@gmail.com>
2021-01-29 00:56:01 -0300

    Extend WCProductAttributeModel to fetch all respective terms

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/product/attributes/WCProductAttributeModel.kt
@@ -4,6 +4,7 @@ import com.yarolegovich.wellsql.core.Identifiable
 import com.yarolegovich.wellsql.core.annotation.Column
 import com.yarolegovich.wellsql.core.annotation.PrimaryKey
 import com.yarolegovich.wellsql.core.annotation.Table
+import org.wordpress.android.fluxc.persistence.WCProductAttributeSqlUtils.getTerm
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 
 @Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE)
@@ -14,11 +15,19 @@ data class WCProductAttributeModel(
     @Column var slug: String = "",
     @Column var type: String = "",
     @Column var orderBy: String = "",
-    @Column var hasArchives: Boolean = false
+    @Column var hasArchives: Boolean = false,
+    @Column var termsId: String = ""
 ) : Identifiable {
     override fun setId(id: Int) {
         this.id = id
     }
 
     override fun getId() = id
+
+    val terms
+        get() = termsId
+                .split(";")
+                .mapNotNull { it.toIntOrNull() }
+                .takeIf { it.isNotEmpty() }
+                ?.map { getTerm(localSiteId, it) }
 }
