b80f5d31658e4881fc52047af33b7063223ba15d
Thomaz Cortez <thomazfcortez@gmail.com>
2021-01-29 00:59:37 -0300

    Rework WCProductAttributeSqlUtils to support Terms operations

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCProductAttributeSqlUtils.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCProductAttributeSqlUtils.kt
@@ -1,8 +1,10 @@
 package org.wordpress.android.fluxc.persistence
 
+import com.wellsql.generated.WCAttributeTermModelTable
 import com.wellsql.generated.WCProductAttributeModelTable
 import com.yarolegovich.wellsql.WellSql
 import org.wordpress.android.fluxc.model.product.attributes.WCProductAttributeModel
+import org.wordpress.android.fluxc.model.product.attributes.terms.WCAttributeTermModel
 
 object WCProductAttributeSqlUtils {
     fun getCurrentAttributes(siteID: Int) =
@@ -13,13 +15,6 @@ object WCProductAttributeSqlUtils {
                     .asModel
                     ?.toList()
                     .orEmpty()
-
-    fun deleteCompleteAttributesList(siteID: Int) =
-            WellSql.delete(WCProductAttributeModel::class.java)
-                    .where()
-                    .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
-                    .endWhere()
-                    .execute()
 
     fun insertFromScratchCompleteAttributesList(attributes: List<WCProductAttributeModel>, siteID: Int) {
         deleteCompleteAttributesList(siteID)
@@ -51,4 +46,40 @@ object WCProductAttributeSqlUtils {
                 .put(attribute)
                 .execute()
     }
+
+    fun getTerm(siteID: Int, termID: Int) =
+            WellSql.select(WCAttributeTermModel::class.java)
+                    .where()
+                    .equals(WCAttributeTermModelTable.ID, termID)
+                    .equals(WCAttributeTermModelTable.LOCAL_SITE_ID, siteID)
+                    .endWhere()
+                    .asModel
+                    ?.toList()
+                    ?.firstOrNull()
+
+    fun insertAttributeTermsFromScratch(
+        attributeID: Int,
+        siteID: Int,
+        terms: List<WCAttributeTermModel>
+    ) {
+        deleteAllTermsFromSingleAttribute(attributeID, siteID)
+        WellSql.insert(terms)
+                .asSingleTransaction(true)
+                .execute()
+    }
+
+    private fun deleteCompleteAttributesList(siteID: Int) =
+            WellSql.delete(WCProductAttributeModel::class.java)
+                    .where()
+                    .equals(WCProductAttributeModelTable.LOCAL_SITE_ID, siteID)
+                    .endWhere()
+                    .execute()
+
+    private fun deleteAllTermsFromSingleAttribute(attributeID: Int, siteID: Int) =
+            WellSql.delete(WCAttributeTermModel::class.java)
+                    .where()
+                    .equals(WCAttributeTermModelTable.LOCAL_SITE_ID, siteID)
+                    .equals(WCAttributeTermModelTable.ATTRIBUTE_ID, attributeID)
+                    .endWhere()
+                    .execute()
 }
