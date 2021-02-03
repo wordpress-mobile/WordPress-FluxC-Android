17183f304d3c421f2d515675e64ed2cdbc465f6d
Thomaz Cortez <thomazfcortez@gmail.com>
2021-02-03 15:08:48 -0300

    update database ID before updating Attribute

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCGlobalAttributeSqlUtils.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/WCGlobalAttributeSqlUtils.kt
@@ -9,9 +9,9 @@ import org.wordpress.android.fluxc.model.attribute.terms.WCAttributeTermModel
 object WCGlobalAttributeSqlUtils {
     fun getCurrentAttributes(siteID: Int) =
             WellSql.select(WCGlobalAttributeModel::class.java)
-                    .where()
+                    .where().beginGroup()
                     .equals(WCGlobalAttributeModelTable.LOCAL_SITE_ID, siteID)
-                    .endWhere()
+                    .endGroup().endWhere()
                     .asModel
                     ?.toList()
                     .orEmpty()
@@ -30,44 +30,47 @@ object WCGlobalAttributeSqlUtils {
 
     fun fetchSingleStoredAttribute(attributeId: Int, siteID: Int) =
         WellSql.select(WCGlobalAttributeModel::class.java)
-                .where()
+                .where().beginGroup()
                 .equals(WCGlobalAttributeModelTable.REMOTE_ID, attributeId)
                 .equals(WCGlobalAttributeModelTable.LOCAL_SITE_ID, siteID)
-                .endWhere()
+                .endGroup().endWhere()
                 .asModel
                 .takeIf { it.isNotEmpty() }
                 ?.first()
 
     fun deleteSingleStoredAttribute(attribute: WCGlobalAttributeModel, siteID: Int) = attribute.apply {
         WellSql.delete(WCGlobalAttributeModel::class.java)
-                .where()
+                .where().beginGroup()
                 .equals(WCGlobalAttributeModelTable.REMOTE_ID, attribute.remoteId)
                 .equals(WCGlobalAttributeModelTable.LOCAL_SITE_ID, siteID)
-                .endWhere()
+                .endGroup().endWhere()
                 .execute()
     }
 
     fun updateSingleStoredAttribute(attribute: WCGlobalAttributeModel, siteID: Int) = attribute.apply {
         WellSql.update(WCGlobalAttributeModel::class.java)
-                .where()
+                .where().beginGroup()
                 .equals(WCGlobalAttributeModelTable.REMOTE_ID, attribute.remoteId)
                 .equals(WCGlobalAttributeModelTable.LOCAL_SITE_ID, siteID)
-                .endWhere()
+                .endGroup().endWhere()
                 .put(attribute)
                 .execute()
     }
 
     fun insertOrUpdateSingleAttribute(attribute: WCGlobalAttributeModel, siteID: Int) =
             fetchSingleStoredAttribute(attribute.remoteId, siteID)
-                    ?.let { updateSingleStoredAttribute(attribute, siteID) }
+                    ?.let {
+                        attribute.id = it.id
+                        updateSingleStoredAttribute(attribute, siteID)
+                    }
                     ?: insertSingleAttribute(attribute)
 
     fun getTerm(siteID: Int, termID: Int) =
             WellSql.select(WCAttributeTermModel::class.java)
-                    .where()
+                    .where().beginGroup()
                     .equals(WCAttributeTermModelTable.REMOTE_ID, termID)
                     .equals(WCAttributeTermModelTable.LOCAL_SITE_ID, siteID)
-                    .endWhere()
+                    .endGroup().endWhere()
                     .asModel
                     ?.toList()
                     ?.firstOrNull()
@@ -85,16 +88,16 @@ object WCGlobalAttributeSqlUtils {
 
     private fun deleteCompleteAttributesList(siteID: Int) =
             WellSql.delete(WCGlobalAttributeModel::class.java)
-                    .where()
+                    .where().beginGroup()
                     .equals(WCGlobalAttributeModelTable.LOCAL_SITE_ID, siteID)
-                    .endWhere()
+                    .endGroup().endWhere()
                     .execute()
 
     private fun deleteAllTermsFromSingleAttribute(attributeID: Int, siteID: Int) =
             WellSql.delete(WCAttributeTermModel::class.java)
-                    .where()
+                    .where().beginGroup()
                     .equals(WCAttributeTermModelTable.LOCAL_SITE_ID, siteID)
                     .equals(WCAttributeTermModelTable.ATTRIBUTE_ID, attributeID)
-                    .endWhere()
+                    .endGroup().endWhere()
                     .execute()
 }
