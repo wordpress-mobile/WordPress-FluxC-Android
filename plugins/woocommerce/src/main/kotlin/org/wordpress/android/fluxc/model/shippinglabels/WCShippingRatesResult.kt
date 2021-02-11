3afe2c80d4b3732737e142823da542b05d8e2ced
Ondrej Ruttkay <ondrej@automattic.com>
2021-02-11 22:22:37 +0100

    Add the request and response models

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/shippinglabels/WCShippingRatesResult.kt
@@ -0,0 +1,18 @@
+package org.wordpress.android.fluxc.model.shippinglabels
+
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.shippinglabels.ShippingLabelRestClient.ShippingRatesApiResponse.Box.ShippingOption.Rate
+
+data class WCShippingRatesResult(
+    val packageRates: List<ShippingPackage>
+) {
+    data class ShippingOption(
+        val optionId: String,
+        val shipmentId: String,
+        val rates: List<Rate>
+    )
+
+    data class ShippingPackage(
+        val boxId: String,
+        val shippingOptions: List<ShippingOption>
+    )
+}
