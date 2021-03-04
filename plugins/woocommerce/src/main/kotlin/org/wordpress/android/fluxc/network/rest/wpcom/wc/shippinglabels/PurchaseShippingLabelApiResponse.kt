0d4d6970acc431f38363287488027d90c8d1b8f7
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-03-04 16:06:31 +0100

    Update the label model to use the same data between fetching and purchasing

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/shippinglabels/PurchaseShippingLabelApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/shippinglabels/PurchaseShippingLabelApiResponse.kt
@@ -8,19 +8,4 @@ data class PurchaseShippingLabelApiResponse(
     val isSuccess: Boolean = false,
     @SerializedName("labels")
     val labels: List<LabelItem>? = null
-) {
-    class LabelItem {
-        @SerializedName("label_id") val labelId: Long? = null
-        @SerializedName("tracking") val trackingNumber: String? = null
-        @SerializedName("carrier_id") val carrierId: String? = null
-        @SerializedName("service_name") val serviceName: String? = null
-        @SerializedName("created") val dateCreated: Long? = null
-        @SerializedName("package_name") val packageName: String? = null
-        @SerializedName("refundable_amount") val refundableAmount: BigDecimal? = null
-        @SerializedName("status") val status: String? = null
-        @SerializedName("commercial_invoice_url") val invoiceUrl: String? = null
-        @SerializedName("is_letter") val isLetter: Boolean? = null
-        @SerializedName("product_ids") val productIds: List<Long>? = emptyList()
-        @SerializedName("product_names") val productNames: List<String>? = emptyList()
-    }
-}
+)
--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/shippinglabels/ShippingLabelApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/shippinglabels/ShippingLabelApiResponse.kt
@@ -14,20 +14,4 @@ class ShippingLabelApiResponse : Response {
     val labelsData: List<LabelItem>? = null
 
     val success: Boolean? = null
-
-    class LabelItem {
-        @SerializedName("label_id") val labelId: Long? = null
-        @SerializedName("tracking") val trackingNumber: String? = null
-        @SerializedName("carrier_id") val carrierId: String? = null
-        @SerializedName("service_name") val serviceName: String? = null
-        @SerializedName("created_date") val dateCreated: Long? = null
-        @SerializedName("package_name") val packageName: String? = null
-        @SerializedName("product_names") val productNames: List<String>? = emptyList()
-        @SerializedName("product_ids") val productIds: List<Long>? = emptyList()
-        @SerializedName("refundable_amount") val refundableAmount: BigDecimal? = null
-        val status: String? = null
-        val rate: BigDecimal? = null
-        val currency: String? = null
-        val refund: JsonElement? = null
-    }
 }
