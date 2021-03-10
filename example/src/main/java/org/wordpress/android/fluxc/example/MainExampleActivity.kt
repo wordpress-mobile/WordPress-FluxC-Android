0aa983163239df60d1fe26f5ca70a475c3c43712
Nick Bradbury <nick.bradbury@gmail.com>
2021-03-10 13:40:30 -0500

    Added toolbar to example app

--- example/src/main/java/org/wordpress/android/fluxc/example/MainExampleActivity.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/MainExampleActivity.kt
@@ -2,7 +2,9 @@ package org.wordpress.android.fluxc.example
 
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
-import androidx.fragment.app.FragmentActivity
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager.OnBackStackChangedListener
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -10,7 +12,7 @@ import dagger.android.HasAndroidInjector
 import kotlinx.android.synthetic.main.activity_example.*
 import javax.inject.Inject
 
-class MainExampleActivity : FragmentActivity(), HasAndroidInjector {
+class MainExampleActivity : AppCompatActivity(), OnBackStackChangedListener,  HasAndroidInjector {
     @Inject lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +20,13 @@ class MainExampleActivity : FragmentActivity(), HasAndroidInjector {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_example)
+        supportFragmentManager.addOnBackStackChangedListener(this)
 
         if (savedInstanceState == null) {
             val mf = MainFragment()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container, mf).commit()
         }
+
         log.movementMethod = ScrollingMovementMethod()
         prependToLog("I'll log stuff here.")
     }
@@ -33,4 +37,19 @@ class MainExampleActivity : FragmentActivity(), HasAndroidInjector {
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onBackStackChanged() {
+        val showBackArrow = supportFragmentManager.backStackEntryCount > 0
+        supportActionBar?.setDisplayHomeAsUpEnabled(showBackArrow)
+    }
 }
