e46abdb8d4acada56cc31cfcb3cf2090c2f040e9
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-03-24 15:51:34 +0100

    Add a comment explaining the logic of the Poller class

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/utils/Poller.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/utils/Poller.kt
@@ -7,6 +7,18 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
 
+/**
+ * A utility class that handles polling, it allows polling a request, with a defined delay between the requests
+ * and automatic retrying on errors, the logic is as follows:
+ * 1. Invoke the request.
+ * 2. If the request fails, retry for the number of times specified by maxRetries after the specified delay
+ * 3. If the request succeeds, check the predicate agains the result
+ * 4. If it's true, then return the result.
+ * 5. If it's false, repeat from 1.
+ *
+ * @param delayInMs: the delay between requests in milliseconds
+ * @param maxRetries: the maximum number of retries when an error occurs
+ */
 class Poller(private val delayInMs: Long, private val maxRetries: Int) {
     suspend fun <T : WooPayload<*>> poll(
         request: suspend () -> T,
