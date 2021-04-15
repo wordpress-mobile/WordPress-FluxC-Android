c1a3a904b6dfc2f3e5dd2a300f57ab590b9a477d
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-04-15 15:29:40 +0200

    Extract OkHttp constant values

--- example/src/androidTest/java/org/wordpress/android/fluxc/ditests/OkHttpReleaseInjectionTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/ditests/OkHttpReleaseInjectionTest.kt
@@ -2,12 +2,11 @@ package org.wordpress.android.fluxc.ditests
 
 import okhttp3.CookieJar
 import okhttp3.OkHttpClient
-import okhttp3.internal.tls.OkHostnameVerifier
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import org.wordpress.android.fluxc.module.OkHttpConstants
 import org.wordpress.android.fluxc.network.BaseRequest
-import java.time.Duration
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -24,7 +23,7 @@ class OkHttpReleaseInjectionTest {
     @Inject lateinit var customCookieJar: CookieJar
 
     @Before
-    open fun setUp() {
+    fun setUp() {
         DaggerOkHttpReleaseTestComponent.builder()
                 .build()
                 .inject(this)
@@ -35,7 +34,7 @@ class OkHttpReleaseInjectionTest {
         regularClient.apply {
             assertThat(cookieJar).isEqualTo(customCookieJar)
             assertThat(followRedirects).isTrue
-            assertThat(hostnameVerifier).isEqualTo(OkHostnameVerifier)
+            assertThat(hostnameVerifier).isEqualTo(OkHttpConstants.DEFAULT_HOSTNAME_VERIFIER)
 
             assertThat(connectTimeoutMillis).isEqualTo(BaseRequest.DEFAULT_REQUEST_TIMEOUT)
             assertThat(readTimeoutMillis).isEqualTo(BaseRequest.UPLOAD_REQUEST_READ_TIMEOUT)
@@ -48,11 +47,11 @@ class OkHttpReleaseInjectionTest {
         noRedirectsOkHttpClient.apply {
             assertThat(cookieJar).isEqualTo(customCookieJar)
             assertThat(followRedirects).isFalse
-            assertThat(hostnameVerifier).isEqualTo(OkHostnameVerifier)
+            assertThat(hostnameVerifier).isEqualTo(OkHttpConstants.DEFAULT_HOSTNAME_VERIFIER)
 
-            assertThat(connectTimeoutMillis).isEqualTo(DEFAULT_CONNECT_TIMEOUT)
-            assertThat(readTimeoutMillis).isEqualTo(DEFAULT_READ_TIMEOUT)
-            assertThat(writeTimeoutMillis).isEqualTo(DEFAULT_WRITE_TIMEOUT)
+            assertThat(connectTimeoutMillis).isEqualTo(OkHttpConstants.DEFAULT_CONNECT_TIMEOUT_MILLIS)
+            assertThat(readTimeoutMillis).isEqualTo(OkHttpConstants.DEFAULT_READ_TIMEOUT_MILLIS)
+            assertThat(writeTimeoutMillis).isEqualTo(OkHttpConstants.DEFAULT_WRITE_TIMEOUT_MILLIS)
         }
     }
 
@@ -61,17 +60,11 @@ class OkHttpReleaseInjectionTest {
         customSslOkHttpClient.apply {
             assertThat(cookieJar).isEqualTo(customCookieJar)
             assertThat(followRedirects).isTrue
-            assertThat(hostnameVerifier).isNotEqualTo(OkHostnameVerifier)
+            assertThat(hostnameVerifier).isNotEqualTo(OkHttpConstants.DEFAULT_HOSTNAME_VERIFIER)
 
             assertThat(connectTimeoutMillis).isEqualTo(BaseRequest.DEFAULT_REQUEST_TIMEOUT)
             assertThat(readTimeoutMillis).isEqualTo(BaseRequest.UPLOAD_REQUEST_READ_TIMEOUT)
             assertThat(writeTimeoutMillis).isEqualTo(BaseRequest.DEFAULT_REQUEST_TIMEOUT)
         }
-    }
-
-    companion object {
-        private val DEFAULT_CONNECT_TIMEOUT = Duration.ofSeconds(10).toMillis()
-        private val DEFAULT_READ_TIMEOUT = Duration.ofSeconds(10).toMillis()
-        private val DEFAULT_WRITE_TIMEOUT = Duration.ofSeconds(10).toMillis()
     }
 }
