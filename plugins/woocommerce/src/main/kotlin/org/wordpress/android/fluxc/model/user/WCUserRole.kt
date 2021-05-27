b02102e85141a9189fbe2d7953ca9765cd8254fa
anitaa1990 <murthyanitaa@gmail.com>
2021-05-27 11:14:16 +0530

    Added option to return custom user role

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/user/WCUserRole.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/user/WCUserRole.kt
@@ -19,7 +19,7 @@ enum class WCUserRole(val value: String = "") {
          * So if we are unable to find a matching [WCUserRole] object, [OTHER] is returned.
          * This is not currently supported by our app.
          */
-        fun fromValue(value: String) = valueMap[value] ?: OTHER
+        fun fromValue(value: String) = valueMap[value] ?: value
     }
 
     fun isSupported() = this == ADMINISTRATOR || this == SHOP_MANAGER
