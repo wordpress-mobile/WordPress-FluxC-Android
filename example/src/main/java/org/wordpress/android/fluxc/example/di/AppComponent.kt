c50fc219d1909af50fb1e2311194e869f8cbd0bd
vojtasmrcek <planarvoid@gmail.com>
2021-05-28 16:21:30 +0200

    Create database module

--- example/src/main/java/org/wordpress/android/fluxc/example/di/AppComponent.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/di/AppComponent.kt
@@ -6,8 +6,9 @@ import dagger.Component
 import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
 import org.wordpress.android.fluxc.example.ExampleApp
-import org.wordpress.android.fluxc.module.ReleaseNetworkModule
+import org.wordpress.android.fluxc.module.DatabaseModule
 import org.wordpress.android.fluxc.module.OkHttpClientModule
+import org.wordpress.android.fluxc.module.ReleaseNetworkModule
 import javax.inject.Singleton
 
 @Singleton
@@ -18,7 +19,8 @@ import javax.inject.Singleton
         OkHttpClientModule::class,
         ReleaseNetworkModule::class,
         MainActivityModule::class,
-        WCOrderListActivityModule::class))
+        WCOrderListActivityModule::class,
+        DatabaseModule::class))
 interface AppComponent : AndroidInjector<ExampleApp> {
     override fun inject(app: ExampleApp)
 
