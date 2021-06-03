42fba414221a01eb21cb470b5e96e5fd3cb59845
Hicham Boushaba <hicham.boushaba@gmail.com>
2021-06-03 14:49:25 +0100

    Add commercial_invoice_url to LabelItem

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/shippinglabels/WCShippingLabelModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/shippinglabels/WCShippingLabelModel.kt
@@ -27,10 +27,9 @@ class WCShippingLabelModel(@PrimaryKey @Column private var id: Int = 0) : Identi
     @Column var currency = ""
     @Column var productNames = "" // list of product names the shipping label was purchased for
     @Column var productIds = "" // list of product ids the shipping label was purchased for
-
     @Column var formData = "" // map containing package and product details related to that shipping label
-
     @Column var refund = "" // map containing refund information for a shipping label
+    @Column var commercialInvoiceUrl: String? =  null // URL pointing to the international commercial URL
 
     override fun getId() = id
 
