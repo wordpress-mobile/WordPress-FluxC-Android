ef0351a561c1f873cf89ad2b5508cb8968e65f08
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2021-07-01 13:36:37 +0300

    Refactor: Optimize imports on activity log test jetpack connected test

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ActivityLogTestJetpack.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_ActivityLogTestJetpack.kt
@@ -20,9 +20,7 @@ import org.wordpress.android.fluxc.model.activity.ActivityLogModel
 import org.wordpress.android.fluxc.model.activity.RewindStatusModel
 import org.wordpress.android.fluxc.model.activity.RewindStatusModel.Reason
 import org.wordpress.android.fluxc.model.activity.RewindStatusModel.Rewind
-import org.wordpress.android.fluxc.model.activity.RewindStatusModel.Rewind.Status.RUNNING
-import org.wordpress.android.fluxc.model.activity.RewindStatusModel.State.ACTIVE
-import org.wordpress.android.fluxc.model.activity.RewindStatusModel.State.UNAVAILABLE
+import org.wordpress.android.fluxc.model.activity.RewindStatusModel.State
 import org.wordpress.android.fluxc.persistence.ActivityLogSqlUtils
 import org.wordpress.android.fluxc.release.ReleaseStack_ActivityLogTestJetpack.Sites.CompleteJetpackSite
 import org.wordpress.android.fluxc.release.ReleaseStack_ActivityLogTestJetpack.Sites.FreeJetpackSite
@@ -142,7 +140,7 @@ class ReleaseStack_ActivityLogTestJetpack : ReleaseStack_Base() {
         assertNotNull(rewindStatusForSite)
         rewindStatusForSite?.apply {
             assertNotNull(this.state)
-            assertEquals(this.state, UNAVAILABLE)
+            assertEquals(this.state, State.UNAVAILABLE)
         }
     }
 
@@ -233,10 +231,10 @@ class ReleaseStack_ActivityLogTestJetpack : ReleaseStack_Base() {
 
         val rewindId = "rewindId"
         val restoreId: Long = 123
-        val status = RUNNING
+        val status = Rewind.Status.RUNNING
         val progress = 30
         val rewind = Rewind(rewindId, restoreId, status, progress, null, null, null)
-        val model = RewindStatusModel(ACTIVE, Reason.NO_REASON, Date(), null, null, rewind)
+        val model = RewindStatusModel(State.ACTIVE, Reason.NO_REASON, Date(), null, null, rewind)
 
         activityLogSqlUtils.replaceRewindStatus(site, model)
 
