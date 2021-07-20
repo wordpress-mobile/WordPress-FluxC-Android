1fe697ed49a951bddc8c0188999b56ac964836b2
ThomazFB <thomazfcortez@gmail.com>
2021-07-19 21:11:00 -0300

    Create Product Addon Type API response

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonApiResponse.kt
@@ -1,7 +1,7 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.addons
 
 class ProductAddonApiResponse {
-    val type: String? = null
+    val type: AddOnType? = null
     val display: String? = null
     val name: String? = null
     val titleFormat: String? = null
@@ -17,4 +17,15 @@ class ProductAddonApiResponse {
     val min: String? = null
     val max: String? = null
     val options: String? = null
+}
+
+class AddOnType {
+    val multipleChoice: String? = null
+    val checkbox: String? = null
+    val customText: String? = null
+    val customTextArea: String? = null
+    val fileUpload: String? = null
+    val customPrice: String? = null
+    val inputMultiplier: String? = null
+    val heading: String? = null
 }
