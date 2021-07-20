128addae1c9c128ccb94682123c4aa0fa085f1dc
ThomazFB <thomazfcortez@gmail.com>
2021-07-19 21:51:44 -0300

    Refactor ProductAddonApiResponse to use enums to define internal types

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonApiResponse.kt
@@ -2,30 +2,56 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.addons
 
 class ProductAddonApiResponse {
     val type: AddOnType? = null
-    val display: String? = null
+    val display: AddOnDisplay? = null
     val name: String? = null
-    val titleFormat: String? = null
+    val titleFormat: AddOnTitleFormat? = null
     val descriptionEnabled: String? = null
     val description: String? = null
     val required: String? = null
     val position: String? = null
     val restrictions: String? = null
-    val restrictionsType: String? = null
+    val restrictionsType: AddOnRestrictionsType? = null
     val adjustPrice: String? = null
-    val priceType: String? = null
+    val priceType: AddOnPriceType? = null
     val price: String? = null
     val min: String? = null
     val max: String? = null
     val options: String? = null
-}
 
-class AddOnType {
-    val multipleChoice: String? = null
-    val checkbox: String? = null
-    val customText: String? = null
-    val customTextArea: String? = null
-    val fileUpload: String? = null
-    val customPrice: String? = null
-    val inputMultiplier: String? = null
-    val heading: String? = null
+    enum class AddOnType {
+        MultipleChoice,
+        Checkbox,
+        CustomText,
+        CustomTextArea,
+        FileUpload,
+        CustomPrice,
+        InputMultiplier,
+        Heading
+    }
+
+    enum class AddOnDisplay {
+        Dropdown,
+        RadioButton,
+        Images
+    }
+
+    enum class AddOnTitleFormat {
+        Label,
+        Heading,
+        Hide
+    }
+
+    enum class AddOnRestrictionsType {
+        AnyText,
+        OnlyLetters,
+        OnlyNumbers,
+        OnlyLettersNumbers,
+        Email
+    }
+
+    enum class AddOnPriceType {
+        FlatFee,
+        QuantityBased,
+        PercentageBased
+    }
 }
