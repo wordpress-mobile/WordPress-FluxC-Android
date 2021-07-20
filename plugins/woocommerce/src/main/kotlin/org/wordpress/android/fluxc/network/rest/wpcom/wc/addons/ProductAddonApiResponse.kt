7fbcb076c2133f99e68d56b35b352beabe35e16f
ThomazFB <thomazfcortez@gmail.com>
2021-07-19 21:04:20 -0300

    Create Product Addon base API response

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonApiResponse.kt
@@ -1,4 +1,20 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.addons
 
 class ProductAddonApiResponse {
+    val type: String? = null
+    val display: String? = null
+    val name: String? = null
+    val titleFormat: String? = null
+    val descriptionEnabled: String? = null
+    val description: String? = null
+    val required: String? = null
+    val position: String? = null
+    val restrictions: String? = null
+    val restrictionsType: String? = null
+    val adjustPrice: String? = null
+    val priceType: String? = null
+    val price: String? = null
+    val min: String? = null
+    val max: String? = null
+    val options: String? = null
 }
