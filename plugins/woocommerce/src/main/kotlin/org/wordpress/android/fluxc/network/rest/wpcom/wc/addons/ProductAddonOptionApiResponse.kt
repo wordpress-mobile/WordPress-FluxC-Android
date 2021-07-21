ce47634e8046ea73f121091f47b4b973a4722938
ThomazFB <thomazfcortez@gmail.com>
2021-07-21 00:33:13 -0300

    Add serialization types to ProductAddonOptionApiResponse

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonOptionApiResponse.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/addons/ProductAddonOptionApiResponse.kt
@@ -1,11 +1,13 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.addons
 
-import org.wordpress.android.fluxc.network.Response
+import com.google.gson.annotations.SerializedName
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.addons.ProductAddonApiResponse.AddOnPriceType
 
-class ProductAddonOptionApiResponse : Response {
+class ProductAddonOptionApiResponse {
+    @SerializedName("price_type")
+    val priceType: AddOnPriceType? = null
+
     val label: String? = null
     val price: String? = null
-    val priceType: AddOnPriceType? = null
-    val imageID: Long? = null
+    val image: String? = null
 }
