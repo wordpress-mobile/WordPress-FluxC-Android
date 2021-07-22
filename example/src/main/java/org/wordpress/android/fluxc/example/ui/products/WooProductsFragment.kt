0fe26648d7427f983c8b869ace6a5228cb393636
ThomazFB <thomazfcortez@gmail.com>
2021-07-22 18:44:26 -0300

    Add Product addon visualization to example app

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/products/WooProductsFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/products/WooProductsFragment.kt
@@ -8,6 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_woo_products.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
 import org.wordpress.android.fluxc.Dispatcher
@@ -85,6 +88,8 @@ class WooProductsFragment : Fragment() {
 
     private var enteredCategoryName: String? = null
     private val enteredTagNames: MutableList<String> = mutableListOf()
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -367,6 +372,27 @@ class WooProductsFragment : Fragment() {
                             prependToLog("Tag name is empty. Doing nothing..")
                         }
                     }
+                }
+            }
+        }
+
+        fetch_product_addons.setOnClickListener {
+            selectedSite?.let { site ->
+                showSingleLineDialog(activity, "Enter the remoteProductId of product to fetch the addons:") { editText ->
+                    pendingFetchSingleProductRemoteId = editText.text.toString().toLongOrNull()
+                    pendingFetchSingleProductRemoteId?.let { id ->
+                        prependToLog("Submitting request to fetch product by remoteProductID $id")
+                        coroutineScope.launch {
+                            wcProductStore.fetchProductListSynced(site, listOf(id))
+                                    .takeUnless { it.isNullOrEmpty() }
+                                    ?.first()?.addons
+                                    ?.forEachIndexed { index, addon ->
+                                        prependToLog(addon.description?.let { "description: $it" }.orEmpty())
+                                        prependToLog(addon.name?.let { "name: $it" }.orEmpty())
+                                        prependToLog("========== Product Add-on #${index} =========")
+                                    }
+                        }
+                    } ?: prependToLog("No valid remoteOrderId defined...doing nothing")
                 }
             }
         }
