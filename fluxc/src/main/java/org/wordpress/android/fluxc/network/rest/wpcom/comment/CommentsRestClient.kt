f516a6b9732fb3791526af6a2bde199865a2a77a
develric <develric@gmail.com>
2021-07-28 00:07:26 +0200

    Refactoring comments API clients.

--- fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/comment/CommentsRestClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/rest/wpcom/comment/CommentsRestClient.kt
@@ -3,22 +3,20 @@ package org.wordpress.android.fluxc.network.rest.wpcom.comment
 import android.content.Context
 import com.android.volley.RequestQueue
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.Payload
 import org.wordpress.android.fluxc.generated.endpoint.WPCOMREST
 import org.wordpress.android.fluxc.model.CommentStatus
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.comments.CommentsMapper
 import org.wordpress.android.fluxc.network.UserAgent
+import org.wordpress.android.fluxc.network.common.comments.CommentsApiPayload
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Error
 import org.wordpress.android.fluxc.network.rest.wpcom.WPComGsonRequestBuilder.Response.Success
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
 import org.wordpress.android.fluxc.network.rest.wpcom.comment.CommentWPComRestResponse.CommentsWPComRestResponse
-import org.wordpress.android.fluxc.network.rest.wpcom.common.LikesUtilsProvider
 import org.wordpress.android.fluxc.persistence.comments.CommentEntityList
 import org.wordpress.android.fluxc.persistence.comments.CommentsDao.CommentEntity
-import org.wordpress.android.fluxc.store.CommentStore.CommentError
 import org.wordpress.android.fluxc.utils.CommentErrorUtilsWrapper
 import javax.inject.Inject
 import javax.inject.Named
@@ -32,21 +30,9 @@ class CommentsRestClient @Inject constructor(
     accessToken: AccessToken,
     userAgent: UserAgent,
     private val wpComGsonRequestBuilder: WPComGsonRequestBuilder,
-    private val likesUtilsProvider: LikesUtilsProvider,
     private val commentErrorUtilsWrapper: CommentErrorUtilsWrapper,
     private val commentsMapper: CommentsMapper
 ) : BaseWPComRestClient(appContext, dispatcher, requestQueue, accessToken, userAgent) {
-    // TODOD: move to a common dedicated file (like in the mapper package maybe?)
-    data class CommentsApiPayload<T>(
-        val response: T? = null
-    ) : Payload<CommentError>() {
-        constructor(error: CommentError, response: T? = null) : this(response) {
-            this.error = error
-        }
-    }
-
-    //data class <>(val remoteSiteId: Long, )
-
     suspend fun fetchCommentsPage(
         site: SiteModel,
         number: Int,
@@ -73,10 +59,10 @@ class CommentsRestClient @Inject constructor(
             is Success -> {
                 CommentsApiPayload(response.data.comments.map { commentDto ->
                     commentsMapper.commentDtoToEntity(commentDto, site)
-                }/*, site.siteId*/)
+                })
             }
             is Error -> {
-                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error)/*, site.siteId*/)
+                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
             }
         }
     }
@@ -101,10 +87,10 @@ class CommentsRestClient @Inject constructor(
 
         return when (response) {
             is Success -> {
-                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site)/*, site.siteId*/)
+                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site))
             }
             is Error -> {
-                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error)/*, site.siteId*/)
+                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
             }
         }
     }
@@ -121,10 +107,10 @@ class CommentsRestClient @Inject constructor(
 
         return when (response) {
             is Success -> {
-                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site)/*, site.siteId*/)
+                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site))
             }
             is Error -> {
-                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error)/*, site.siteId*/)
+                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
             }
         }
     }
@@ -142,10 +128,10 @@ class CommentsRestClient @Inject constructor(
 
         return when (response) {
             is Success -> {
-                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site)/*, site.siteId*/)
+                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site))
             }
             is Error -> {
-                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error)/*, site.siteId*/)
+                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
             }
         }
     }
@@ -167,10 +153,10 @@ class CommentsRestClient @Inject constructor(
 
         return when (response) {
             is Success -> {
-                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site)/*, site.siteId*/)
+                CommentsApiPayload(commentsMapper.commentDtoToEntity(response.data, site))
             }
             is Error -> {
-                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error)/*, site.siteId*/)
+                CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
             }
         }
     }
@@ -200,7 +186,7 @@ class CommentsRestClient @Inject constructor(
         }
     }
 
-    suspend fun likeComment(site: SiteModel, remoteCommentId: Long/*, comment: CommentEntity*/, isLike: Boolean): CommentsApiPayload<CommentLikeWPComRestResponse> {
+    suspend fun likeComment(site: SiteModel, remoteCommentId: Long, isLike: Boolean): CommentsApiPayload<CommentLikeWPComRestResponse> {
         val url = if (isLike) {
             WPCOMREST.sites.site(site.siteId).comments.comment(remoteCommentId).likes.new_.urlV1_1
         } else {
@@ -224,7 +210,4 @@ class CommentsRestClient @Inject constructor(
             }
         }
     }
-
-
-
 }
--- fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/comment/CommentsXMLRPCClient.kt
+++ fluxc/src/main/java/org/wordpress/android/fluxc/network/xmlrpc/comment/CommentsXMLRPCClient.kt
@@ -1,7 +1,6 @@
 package org.wordpress.android.fluxc.network.xmlrpc.comment
 
 import com.android.volley.RequestQueue
-import org.apache.commons.text.StringEscapeUtils
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.endpoint.XMLRPC
 import org.wordpress.android.fluxc.model.CommentStatus
@@ -10,24 +9,20 @@ import org.wordpress.android.fluxc.model.CommentStatus.SPAM
 import org.wordpress.android.fluxc.model.CommentStatus.TRASH
 import org.wordpress.android.fluxc.model.CommentStatus.UNAPPROVED
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.comments.CommentsMapper
 import org.wordpress.android.fluxc.network.HTTPAuthManager
 import org.wordpress.android.fluxc.network.UserAgent
-import org.wordpress.android.fluxc.network.rest.wpcom.comment.CommentsRestClient.CommentsApiPayload
+import org.wordpress.android.fluxc.network.common.comments.CommentsApiPayload
 import org.wordpress.android.fluxc.network.xmlrpc.BaseXMLRPCClient
 import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCRequestBuilder
 import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCRequestBuilder.Response.Error
 import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCRequestBuilder.Response.Success
-import org.wordpress.android.fluxc.network.xmlrpc.XMLRPCUtils
 import org.wordpress.android.fluxc.persistence.comments.CommentEntityList
 import org.wordpress.android.fluxc.persistence.comments.CommentsDao.CommentEntity
 import org.wordpress.android.fluxc.store.CommentStore.CommentError
-import org.wordpress.android.fluxc.store.CommentStore.CommentErrorType
 import org.wordpress.android.fluxc.store.CommentStore.CommentErrorType.GENERIC_ERROR
 import org.wordpress.android.fluxc.utils.CommentErrorUtilsWrapper
-import org.wordpress.android.util.DateTimeUtils
 import java.util.ArrayList
-import java.util.Date
-import java.util.HashMap
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
@@ -39,7 +34,8 @@ class CommentsXMLRPCClient @Inject constructor(
     userAgent: UserAgent?,
     httpAuthManager: HTTPAuthManager?,
     private val commentErrorUtilsWrapper: CommentErrorUtilsWrapper,
-    private val xmlrpcRequestBuilder: XMLRPCRequestBuilder
+    private val xmlrpcRequestBuilder: XMLRPCRequestBuilder,
+    private val commentsMapper: CommentsMapper
 ) : BaseXMLRPCClient(dispatcher, requestQueue, userAgent, httpAuthManager) {
     suspend fun fetchCommentsPage(
         site: SiteModel,
@@ -68,12 +64,12 @@ class CommentsXMLRPCClient @Inject constructor(
                 url = site.xmlRpcUrl,
                 method = XMLRPC.GET_COMMENTS,
                 params = params,
-                clazz = Any::class.java, // TODOD: better check this!
+                clazz = Array<Any>::class.java
         )
 
         return when(response) {
             is Success -> {
-                CommentsApiPayload(commentsResponseToCommentList(response.data, site))
+                CommentsApiPayload(commentsMapper.commentXmlRpcDTOToEntityList(response.data, site))
             }
             is Error -> {
                 CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
@@ -101,7 +97,7 @@ class CommentsXMLRPCClient @Inject constructor(
                 url = site.xmlRpcUrl,
                 method = XMLRPC.EDIT_COMMENT,
                 params = params,
-                clazz = Any::class.java, // TODOD: better check this!
+                clazz = Any::class.java
         )
 
         return when(response) {
@@ -127,12 +123,12 @@ class CommentsXMLRPCClient @Inject constructor(
                 url = site.xmlRpcUrl,
                 method = XMLRPC.GET_COMMENT,
                 params = params,
-                clazz = Any::class.java, // TODOD: better check this!
+                clazz = Map::class.java
         )
 
         return when(response) {
             is Success -> {
-                CommentsApiPayload(commentResponseToComment(response.data, site))
+                CommentsApiPayload(commentsMapper.commentXmlRpcDTOToEntity(response.data, site))
             }
             is Error -> {
                 CommentsApiPayload(commentErrorUtilsWrapper.networkToCommentError(response.error))
@@ -140,7 +136,7 @@ class CommentsXMLRPCClient @Inject constructor(
         }
     }
 
-    suspend fun deleteComment(site: SiteModel, remoteCommentId: Long, comment: CommentEntity?): CommentsApiPayload<CommentEntity?> {
+    suspend fun deleteComment(site: SiteModel, remoteCommentId: Long): CommentsApiPayload<CommentEntity?> {
         val params: MutableList<Any> = ArrayList(4)
 
         params.add(site.selfHostedSiteId)
@@ -168,7 +164,7 @@ class CommentsXMLRPCClient @Inject constructor(
         }
     }
 
-    suspend fun createNewReply(site: SiteModel, comment: CommentEntity, reply: CommentEntity): CommentsApiPayload<CommentEntity>{
+    suspend fun createNewReply(site: SiteModel, comment: CommentEntity, reply: CommentEntity): CommentsApiPayload<CommentEntity> {
         val commentParams = mutableMapOf<String, Any?>(
                 "content" to reply.content,
                 "comment_parent" to comment.remoteCommentId,
@@ -194,17 +190,18 @@ class CommentsXMLRPCClient @Inject constructor(
                 "content" to comment.content,
         )
 
+        // TODOD: check how it is possible to create a post comment from the app for a self-hsoted!
         if (comment.remoteParentCommentId != 0L) {
-            commentParams["comment_parent"] = comment.remoteParentCommentId;
+            commentParams["comment_parent"] = comment.remoteParentCommentId
         }
         if (comment.authorName != null) {
-            commentParams["author"] = comment.authorName;
+            commentParams["author"] = comment.authorName
         }
         if (comment.authorUrl != null) {
-            commentParams["author_url"] = comment.authorUrl;
+            commentParams["author_url"] = comment.authorUrl
         }
         if (comment.authorEmail != null) {
-            commentParams["author_email"] = comment.authorEmail;
+            commentParams["author_email"] = comment.authorEmail
         }
 
         return newComment(site, remotePostId, comment, comment.remoteParentCommentId, commentParams)
@@ -224,7 +221,7 @@ class CommentsXMLRPCClient @Inject constructor(
                 url = site.xmlRpcUrl,
                 method = XMLRPC.NEW_COMMENT,
                 params = params,
-                clazz = Any::class.java, // TODOD: better check this!
+                clazz = Any::class.java
         )
 
         return when(response) {
@@ -251,72 +248,5 @@ class CommentsXMLRPCClient @Inject constructor(
             TRASH -> "trash"
             else -> "approve"
         }
-    }
-
-    private fun getCommentStatusFromXMLRPCStatusString(stringStatus: String): CommentStatus {
-        return when (stringStatus) {
-            "approve" -> APPROVED
-            "hold" -> UNAPPROVED
-            "spam" -> SPAM
-            "trash" -> TRASH
-            else -> APPROVED
-        }
-    }
-
-    // TODOD: move to mapper?
-    private fun commentsResponseToCommentList(response: Any?, site: SiteModel): List<CommentEntity> {
-        val comments: MutableList<CommentEntity> = ArrayList()
-        if (response !is Array<*>) {
-            return comments
-        }
-
-        response.forEach { commentObject ->
-            commentResponseToComment(commentObject, site)?.let {
-                comments.add(it)
-            }
-        }
-
-        return comments
-    }
-
-    // TODOD: move to mapper?
-    private fun commentResponseToComment(commentObject: Any?, site: SiteModel): CommentEntity? {
-        if (commentObject !is HashMap<*, *>) {
-            return null
-        }
-        val commentMap: HashMap<*, *> = commentObject
-
-        val datePublished = DateTimeUtils.iso8601UTCFromDate(XMLRPCUtils.safeGetMapValue(commentMap, "date_created_gmt", Date()))
-        // TODOD: use a wrapper for XMLRPCUtils?
-        val remoteParentCommentId = XMLRPCUtils.safeGetMapValue(commentMap, "parent", 0L)
-
-        return CommentEntity(
-                //id = 0,
-                remoteCommentId = XMLRPCUtils.safeGetMapValue(commentMap, "comment_id", 0L),
-                remotePostId = XMLRPCUtils.safeGetMapValue(commentMap, "post_id", 0L),
-                remoteParentCommentId = remoteParentCommentId,
-                localSiteId = site.id,
-                remoteSiteId = site.selfHostedSiteId,
-                authorUrl = XMLRPCUtils.safeGetMapValue(commentMap, "author_url", ""),
-                authorName = StringEscapeUtils.unescapeHtml4(XMLRPCUtils.safeGetMapValue(commentMap, "author", "")),
-                authorEmail = XMLRPCUtils.safeGetMapValue(commentMap, "author_email", ""),
-                // TODO: set authorProfileImageUrl - get the hash from the email address?
-                // TODOD: check if legacy code place a null in db or empty string!
-                authorProfileImageUrl = null,
-                postTitle = StringEscapeUtils.unescapeHtml4(
-                        XMLRPCUtils.safeGetMapValue(
-                                commentMap,
-                                "post_title", ""
-                        )
-                ),
-                status = getCommentStatusFromXMLRPCStatusString(XMLRPCUtils.safeGetMapValue(commentMap, "status", "approve")).toString(),
-                datePublished = datePublished,
-                publishedTimestamp = DateTimeUtils.timestampFromIso8601(datePublished),
-                content = XMLRPCUtils.safeGetMapValue(commentMap, "content", ""),
-                url = XMLRPCUtils.safeGetMapValue(commentMap, "link", ""),
-                hasParent = remoteParentCommentId > 0,
-                parentId = if (remoteParentCommentId > 0) remoteParentCommentId else 0,
-                iLike = false
-        )
     }
 }
