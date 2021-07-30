f924847c1d647f0f9c43d7898e1d8765a285e7bd
Oguz Kocer <oguz.kocer@automattic.com>
2021-07-29 23:53:24 -0400

    Fix processor version metadata name in publish-plugins-woocommerce.sh

--- .buildkite/publish-plugins-woocommerce.sh
+++ .buildkite/publish-plugins-woocommerce.sh
@@ -4,12 +4,12 @@ set -euo pipefail
 
 # Retrieve data from previous steps
 PUBLISHED_FLUXC_ANNOTATIONS_VERSION=$(buildkite-agent meta-data get "PUBLISHED_FLUXC_ANNOTATIONS_VERSION")
-PUBLISHED_FLUXC_PROCESSORS_VERSION=$(buildkite-agent meta-data get "PUBLISHED_FLUXC_PROCESSORS_VERSION")
+PUBLISHED_FLUXC_PROCESSOR_VERSION=$(buildkite-agent meta-data get "PUBLISHED_FLUXC_PROCESSOR_VERSION")
 PUBLISHED_FLUXC_VERSION=$(buildkite-agent meta-data get "PUBLISHED_FLUXC_VERSION")
 
 ./gradlew \
     -PfluxcAnnotationsVersion="$PUBLISHED_FLUXC_ANNOTATIONS_VERSION" \
-    -PfluxcProcessorVersion="$PUBLISHED_FLUXC_PROCESSORS_VERSION" \
+    -PfluxcProcessorVersion="$PUBLISHED_FLUXC_PROCESSOR_VERSION" \
     -PfluxcVersion="$PUBLISHED_FLUXC_VERSION" \
     :plugins:woocommerce:prepareToPublishToS3 $(prepare_to_publish_to_s3_params) \
     :plugins:woommerce:publish
