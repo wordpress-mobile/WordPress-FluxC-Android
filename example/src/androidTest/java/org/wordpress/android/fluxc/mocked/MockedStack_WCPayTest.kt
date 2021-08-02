e0fc2c716268a458f52ee033c61a28e759f26ca8
malinajirka <malinajirka@gmail.com>
2021-08-02 09:34:29 +0200

    Add support for restricted_soon status

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCPayTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCPayTest.kt
@@ -100,4 +100,13 @@ class MockedStack_WCPayTest : MockedStack_Base() {
 
         assertTrue(result.result?.status == WCPayAccountStatusEnum.NO_ACCOUNT)
     }
+
+    @Test
+    fun whenLoadAccountRestrictedSoonStatusThenRestrictedSoonStatusReturned() = runBlocking {
+        interceptor.respondWithError("wc-pay-load-account-response-restricted-soon-status.json", 200)
+
+        val result = payRestClient.loadAccount(SiteModel().apply { siteId = 123L })
+
+        assertTrue(result.result?.status == WCPayAccountStatusEnum.RESTRICTED_SOON)
+    }
 }
