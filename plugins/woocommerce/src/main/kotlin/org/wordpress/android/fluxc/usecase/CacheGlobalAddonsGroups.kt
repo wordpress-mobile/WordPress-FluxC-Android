41ea70f3ff39e7d08ae21d7d490239ae3218199a
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-08-16 09:30:12 +0200

    Divide CacheGlobalAddonsGroups into smaller private functions

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/usecase/CacheGlobalAddonsGroups.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/usecase/CacheGlobalAddonsGroups.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.fluxc.usecase
 
+import org.wordpress.android.fluxc.model.addons.WCProductAddonModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.addons.dto.AddOnGroupDto
 import org.wordpress.android.fluxc.persistence.dao.AddonsDao
 import org.wordpress.android.fluxc.persistence.mappers.toAddonEntity
@@ -16,14 +17,28 @@ internal class CacheGlobalAddonsGroups @Inject constructor(private val addonsDao
 
         globalAddonGroups.forEach { group ->
             val globalAddonGroupEntityId = addonsDao.insertGroup(group.toAddonGroupEntity(remoteSiteId))
-            group.addons.forEach { addon ->
-                val addonEntity = addon.toAddonEntity(globalAddonGroupEntityId)
-                val addonEntityId = addonsDao.insertAddons(addonEntity)
-                addon.options?.forEach { addonOption ->
-                    val addonOptionEntity = addonOption.toAddonOptionEntity(addonLocalId = addonEntityId)
-                    addonsDao.insertAddonOptions(addonOptionEntity)
-                }
-            }
+            insertAddonEntity(group.addons, globalAddonGroupEntityId)
+        }
+    }
+
+    private suspend fun insertAddonEntity(
+        addons: List<WCProductAddonModel>,
+        groupId: Long
+    ) {
+        addons.forEach { addon ->
+            val addonEntity = addon.toAddonEntity(groupId)
+            val addonEntityId = addonsDao.insertAddons(addonEntity)
+            insertAddonOptionEntity(addon.options, addonEntityId)
+        }
+    }
+
+    private suspend fun insertAddonOptionEntity(
+        options: List<WCProductAddonModel.ProductAddonOption>?,
+        addonEntityId: Long
+    ) {
+        options?.forEach { addonOption ->
+            val addonOptionEntity = addonOption.toAddonOptionEntity(addonLocalId = addonEntityId)
+            addonsDao.insertAddonOptions(addonOptionEntity)
         }
     }
 }
