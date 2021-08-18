a7320445dfb401ba595000c73e809df727a2098d
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-08-18 15:58:37 +0200

    Add logic for combining global and product specific add-ons

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WCAddonsStore.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WCAddonsStore.kt
@@ -1,12 +1,18 @@
 package org.wordpress.android.fluxc.store
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.WCProductModel
 import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.UNKNOWN
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType.GENERIC_ERROR
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.addons.AddOnsRestClient
 import org.wordpress.android.fluxc.persistence.dao.AddonsDao
+import org.wordpress.android.fluxc.persistence.entity.AddonWithOptions
+import org.wordpress.android.fluxc.persistence.entity.GlobalAddonGroupEntity
 import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
@@ -31,5 +37,33 @@ class WCAddonsStore @Inject internal constructor(
                 else -> WooResult(WooError(GENERIC_ERROR, UNKNOWN))
             }
         }
+    }
+
+    // TODO: 18/08/2021 @wzieba add tests for this method 
+    fun observeAddonsForProduct(siteRemoteId: Long, product: WCProductModel): Flow<List<AddonWithOptions>> {
+        return dao.observeGlobalAddonsForSite(siteRemoteId = siteRemoteId)
+                .map { globalGroups ->
+                    globalGroups.filter { globalGroup ->
+                        globalGroup.group.appliesToEveryProduct() || globalGroup.group.appliesToProduct(product)
+                    }.flatMap { it.addons }
+                }
+                .combine(
+                        dao.observeSingleProductAddons(
+                                siteRemoteId = siteRemoteId,
+                                productRemoteId = product.remoteProductId
+                        )
+                ) { fromGlobalAddons, fromSingleProductAddons ->
+                    fromGlobalAddons + fromSingleProductAddons
+                }
+    }
+
+    private fun GlobalAddonGroupEntity.appliesToEveryProduct(): Boolean {
+        return this.restrictedCategoriesIds.isEmpty()
+    }
+
+    private fun GlobalAddonGroupEntity.appliesToProduct(product: WCProductModel): Boolean {
+        val productCategoriesIds = product.getCategoryList().map { it.id }
+
+        return restrictedCategoriesIds.intersect(productCategoriesIds).isNotEmpty()
     }
 }
