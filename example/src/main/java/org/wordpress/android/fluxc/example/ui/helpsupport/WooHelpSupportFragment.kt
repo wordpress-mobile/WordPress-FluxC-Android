62103abce26978fdc78f7b9dce8cdfb3feb0fad6
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-15 18:34:06 +0700

    Add fetch functionality in Example app.

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/helpsupport/WooHelpSupportFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/helpsupport/WooHelpSupportFragment.kt
@@ -8,8 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_woo_help_support.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.example.R
+import org.wordpress.android.fluxc.example.prependToLog
 import org.wordpress.android.fluxc.example.ui.StoreSelectorDialog
 import org.wordpress.android.fluxc.example.ui.common.showStoreSelectorDialog
 import org.wordpress.android.fluxc.example.utils.toggleSiteDependentButtons
@@ -23,6 +28,8 @@ class WooHelpSupportFragment : Fragment() {
 
     private var selectedPos: Int = -1
     private var selectedSite: SiteModel? = null
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
@@ -48,7 +55,19 @@ class WooHelpSupportFragment : Fragment() {
 
         fetch_ssr.setOnClickListener {
             selectedSite?.let { site ->
-                TODO()
+                coroutineScope.launch {
+                    val result = withContext(Dispatchers.Default) {
+                        wooCommerceStore.fetchSSR(site)
+                    }
+
+                    result.error?.let {
+                        prependToLog("${it.type}: ${it.message}")
+                        return@launch
+                    }
+                    result.model?.let {
+                        prependToLog("Fetched data: $it")
+                    }
+                }
             }
         }
     }
