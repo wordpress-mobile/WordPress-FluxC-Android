4a288b00fb7a96e9798a4c7f7ec8bb1b0490ca7a
Hafiz Rahman <hfzrhmn@gmail.com>
2021-09-15 11:36:12 +0700

    Remove return value from insert as we don't need it.

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/SSRDao.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/SSRDao.kt
@@ -10,7 +10,7 @@ import org.wordpress.android.fluxc.persistence.entity.SSREntity
 @Dao
 abstract class SSRDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertSSR(ssrEntity: SSREntity): Long
+    abstract suspend fun insertSSR(ssrEntity: SSREntity)
 
     @Query("SELECT * FROM SSREntity WHERE localSiteId = :localSiteId LIMIT 1")
     abstract suspend fun getSSRbySite(localSiteId: Int): WCSSRModel
