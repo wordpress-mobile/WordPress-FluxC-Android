598c2edd1b22e7c4ccdd43eb9d42bc780f76dd77
malinajirka <malinajirka@gmail.com>
2021-09-17 13:44:16 +0200

    Fix lint

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/orders/WooOrdersFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/orders/WooOrdersFragment.kt
@@ -141,7 +141,7 @@ class WooOrdersFragment : Fragment(), WCAddOrderShipmentTrackingDialog.Listener 
                     coroutineScope.launch {
                         enteredRemoteId?.let { id ->
                             prependToLog("Submitting request to fetch order by remoteOrderID: $enteredRemoteId")
-                            wcOrderStore.fetchSingleOrder(site, id).takeUnless {   it.isError }?.let {
+                            wcOrderStore.fetchSingleOrder(site, id).takeUnless { it.isError }?.let {
                                 prependToLog("Single order fetched successfully!")
                             } ?: prependToLog("WARNING: Fetched order not found in the local database!")
                         } ?: prependToLog("No valid remoteOrderId defined...doing nothing")
