495fee284fd4da57ce269daaf82bd74be3c4e2c4
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-10-05 14:50:07 +0200

    Use StoreSelectingFragment in tax view

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/taxes/WooTaxFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/taxes/WooTaxFragment.kt
@@ -1,12 +1,9 @@
 package org.wordpress.android.fluxc.example.ui.taxes
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_woo_taxes.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -16,43 +13,21 @@ import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.example.R
 import org.wordpress.android.fluxc.example.prependToLog
-import org.wordpress.android.fluxc.example.ui.StoreSelectorDialog
-import org.wordpress.android.fluxc.example.ui.common.showStoreSelectorDialog
-import org.wordpress.android.fluxc.example.utils.toggleSiteDependentButtons
-import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.example.ui.StoreSelectingFragment
 import org.wordpress.android.fluxc.store.WCTaxStore
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
 
-class WooTaxFragment : Fragment() {
+class WooTaxFragment : StoreSelectingFragment() {
     @Inject internal lateinit var dispatcher: Dispatcher
     @Inject internal lateinit var wcTaxStore: WCTaxStore
     @Inject internal lateinit var wooCommerceStore: WooCommerceStore
-
-    private var selectedPos: Int = -1
-    private var selectedSite: SiteModel? = null
-
-    override fun onAttach(context: Context) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_woo_taxes, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        taxes_select_site.setOnClickListener {
-            showStoreSelectorDialog(selectedPos, object : StoreSelectorDialog.Listener {
-                override fun onSiteSelected(site: SiteModel, pos: Int) {
-                    selectedSite = site
-                    selectedPos = pos
-                    buttonContainer.toggleSiteDependentButtons(true)
-                    taxes_selected_site.text = site.name ?: site.displayName
-                }
-            })
-        }
 
         fetch_tax_class.setOnClickListener {
             selectedSite?.let { site ->
