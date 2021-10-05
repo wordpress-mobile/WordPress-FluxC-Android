0824b64228ce9b5d3a6c5da5d09a0e620248b27d
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-10-05 14:49:04 +0200

    Use StoreSelectingFragment in leaderboards view

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/leaderboards/WooLeaderboardsFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/leaderboards/WooLeaderboardsFragment.kt
@@ -1,12 +1,9 @@
 package org.wordpress.android.fluxc.example.ui.leaderboards
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_woo_leaderboards.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,8 +12,7 @@ import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.example.R
 import org.wordpress.android.fluxc.example.prependToLog
-import org.wordpress.android.fluxc.example.ui.StoreSelectorDialog
-import org.wordpress.android.fluxc.example.utils.toggleSiteDependentButtons
+import org.wordpress.android.fluxc.example.ui.StoreSelectingFragment
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.leaderboards.WCTopPerformerProductModel
 import org.wordpress.android.fluxc.store.WCLeaderboardsStore
@@ -28,28 +24,18 @@ import org.wordpress.android.fluxc.store.WCStatsStore.StatsGranularity.YEARS
 import org.wordpress.android.fluxc.store.WooCommerceStore
 import javax.inject.Inject
 
-class WooLeaderboardsFragment : Fragment(), StoreSelectorDialog.Listener {
+class WooLeaderboardsFragment : StoreSelectingFragment() {
     @Inject internal lateinit var dispatcher: Dispatcher
     @Inject internal lateinit var wooCommerceStore: WooCommerceStore
     @Inject internal lateinit var wcLeaderboardsStore: WCLeaderboardsStore
 
-    private var selectedPos: Int = -1
-    private var selectedSite: SiteModel? = null
-
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
-
-    override fun onAttach(context: Context) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
             inflater.inflate(R.layout.fragment_woo_leaderboards, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        leaderboards_select_site.setOnClickListener(::onLeaderboardsSelectSiteButtonClicked)
         setupFetchButtons()
         bindCacheRetrievalButtons()
     }
@@ -84,13 +70,6 @@ class WooLeaderboardsFragment : Fragment(), StoreSelectorDialog.Listener {
         }
     }
 
-    private fun onLeaderboardsSelectSiteButtonClicked(view: View) {
-        fragmentManager?.let { fm ->
-            StoreSelectorDialog.newInstance(this, selectedPos)
-                    .show(fm, "StoreSelectorDialog")
-        }
-    }
-
     private fun launchProductLeaderboardsRequest(unit: StatsGranularity) {
         coroutineScope.launch {
             try {
@@ -115,13 +94,6 @@ class WooLeaderboardsFragment : Fragment(), StoreSelectorDialog.Listener {
                 prependToLog("Couldn't fetch Products Leaderboards. Error: ${ex.message}")
             }
         }
-    }
-
-    override fun onSiteSelected(site: SiteModel, pos: Int) {
-        selectedSite = site
-        selectedPos = pos
-        buttonContainer.toggleSiteDependentButtons()
-        leaderboards_selected_site.text = site.name ?: site.displayName
     }
 
     private fun logLeaderboardResponse(model: List<WCTopPerformerProductModel>, unit: StatsGranularity) {
