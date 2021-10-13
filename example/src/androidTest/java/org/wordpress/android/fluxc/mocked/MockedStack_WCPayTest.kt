c77fd3650c11785b30c5f201672d46a02dcea61c
Andrey <andrey.kidinov@a8c.com>
2021-10-13 10:30:06 +0300

    Added a test

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCPayTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCPayTest.kt
@@ -11,6 +11,7 @@ import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.MISSING_O
 import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.PAYMENT_ALREADY_CAPTURED
 import org.wordpress.android.fluxc.model.pay.WCCapturePaymentErrorType.SERVER_ERROR
 import org.wordpress.android.fluxc.model.pay.WCPaymentAccountResult.WCPayAccountStatusEnum
+import org.wordpress.android.fluxc.model.pay.WCTerminalStoreLocationErrorType.GenericError
 import org.wordpress.android.fluxc.model.pay.WCTerminalStoreLocationErrorType.MissingAddress
 import org.wordpress.android.fluxc.module.ResponseMockingInterceptor
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.pay.PayRestClient
@@ -131,8 +132,8 @@ class MockedStack_WCPayTest : MockedStack_Base() {
     }
 
     @Test
-    fun whenGetStoreLocationForSite() = runBlocking {
-        interceptor.respondWithError("wc-pay-store-location-for-site-address-missing-error.json", 500)
+    fun whenGetStoreLocationForSiteErrorWithUrl() = runBlocking {
+        interceptor.respondWithError("wc-pay-store-location-for-site-address-missing-with-url-error.json", 500)
 
         val result = payRestClient.getStoreLocationForSite(SiteModel().apply { siteId = 123L })
 
@@ -141,5 +142,25 @@ class MockedStack_WCPayTest : MockedStack_Base() {
         val expectedUrl = "https://myusernametestsite2020151673500.wpcomstaging.com/" +
                 "wp-admin/admin.php?page=wc-settings&tab=general"
         assertEquals(expectedUrl, (result.error?.type as MissingAddress).addressEditingUrl)
+    }
+
+    @Test
+    fun whenGetStoreLocationForSiteErrorWithEmptyUrl() = runBlocking {
+        interceptor.respondWithError("wc-pay-store-location-for-site-address-missing-with-empty-url-error.json", 500)
+
+        val result = payRestClient.getStoreLocationForSite(SiteModel().apply { siteId = 123L })
+
+        assertTrue(result.isError)
+        assertTrue(result.error?.type is GenericError)
+    }
+
+    @Test
+    fun whenGetStoreLocationForSiteErrorWithoutUrl() = runBlocking {
+        interceptor.respondWithError("wc-pay-store-location-for-site-address-missing-without-url-error.json", 500)
+
+        val result = payRestClient.getStoreLocationForSite(SiteModel().apply { siteId = 123L })
+
+        assertTrue(result.isError)
+        assertTrue(result.error?.type is GenericError)
     }
 }
