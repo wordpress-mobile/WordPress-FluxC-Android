99c1d0c7726b7bb42522d998226f45c1ef0168b7
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-10-27 15:43:53 +0200

    Add id column to BackwardCompatibilityWCOrderModel

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/BackwardCompatibilityWCOrderModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/BackwardCompatibilityWCOrderModel.kt
@@ -1,11 +1,13 @@
 package org.wordpress.android.fluxc.model
 
 import com.yarolegovich.wellsql.core.Identifiable
+import com.yarolegovich.wellsql.core.annotation.Column
+import com.yarolegovich.wellsql.core.annotation.PrimaryKey
 import com.yarolegovich.wellsql.core.annotation.Table
 import org.wordpress.android.fluxc.persistence.WellSqlConfig
 
 @Table(addOn = WellSqlConfig.ADDON_WOOCOMMERCE, name = "WCOrderModel")
-class BackwardCompatibilityWCOrderModel : Identifiable {
+class BackwardCompatibilityWCOrderModel(@PrimaryKey @Column private val id: Int = 0) : Identifiable {
     override fun setId(id: Int) = Unit
     override fun getId(): Int = -1
 }
