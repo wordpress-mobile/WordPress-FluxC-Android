ef7df3045045a6fc3bc460678bff5a4d710551e9
malinajirka <malinajirka@gmail.com>
2021-12-23 08:38:11 +0100

    Add id to FeeLine

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/order/FeeLine.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/order/FeeLine.kt
@@ -4,9 +4,12 @@ import com.google.gson.annotations.SerializedName
 
 /**
  * Represents a fee line
- * We are reading only the name and the total, as the tax is already included in the order totalTax
+ * We are reading only the id, the name and the total, as the tax is already included in the order totalTax
  */
 class FeeLine {
+    @SerializedName("id")
+    val id: Long? = null
+
     @SerializedName("name")
     val name: String? = null
 
