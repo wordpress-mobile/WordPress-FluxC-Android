9c37e38135aedcc00a11056e20e76542f2c82b11
Nick Bradbury <nick.bradbury@gmail.com>
2022-01-28 12:21:23 -0500

    Added ratePercent to TaxLine

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/order/TaxLine.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/order/TaxLine.kt
@@ -9,14 +9,17 @@ class TaxLine {
     @SerializedName("id")
     val id: Long? = null
 
+    @SerializedName("rate_id")
+    val rateId: Long? = null
+
     @SerializedName("rate_code")
     val rateCode: String? = null
 
+    @SerializedName("rate_percent")
+    val ratePercent: Float? = null
+
     @SerializedName("label")
     val label: String? = null
-
-    @SerializedName("rate_id")
-    val rateId: Long? = null
 
     @SerializedName("compound")
     val compound: Boolean? = null
