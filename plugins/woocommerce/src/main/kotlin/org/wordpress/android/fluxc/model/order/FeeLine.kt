865786d1bb2aef4ae10a43aa4b1004a1e72a870f
Nick Bradbury <nick.bradbury@gmail.com>
2022-02-03 15:38:59 -0500

    Changed tax status to a string

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/order/FeeLine.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/order/FeeLine.kt
@@ -19,7 +19,7 @@ class FeeLine {
     val totalTax: String? = null
 
     @SerializedName("tax_status")
-    val taxStatus: FeeLineTaxStatus? = null
+    val taxStatus: String? = null
 }
 
 enum class FeeLineTaxStatus {
