b811a1c20e1642d6cb092a5f04659a40ec3757ae
Ondrej Ruttkay <ruttkay@gmail.com>
2022-03-09 20:58:54 +0100

    Change the product entity data columns types from strings enums and sealed classes

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/ProductEntity.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/ProductEntity.kt
@@ -2,6 +2,19 @@ package org.wordpress.android.fluxc.persistence.entity
 
 import androidx.room.Entity
 import org.wordpress.android.fluxc.model.WCProductModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductBackOrders
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductStatus
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductStockStatus
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductTaxStatus
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductType
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductVisibility
+import org.wordpress.android.fluxc.persistence.entity.ProductEntity.BackorderStatus
+import org.wordpress.android.fluxc.persistence.entity.ProductEntity.StockStatus
+import org.wordpress.android.fluxc.persistence.entity.ProductEntity.TaxStatus
+import org.wordpress.android.fluxc.persistence.entity.ProductEntity.Type
+import org.wordpress.android.util.DateTimeUtils
+import java.util.Date
+import java.util.Locale
 
 @Entity(
     tableName = "Products",
@@ -14,13 +27,13 @@ data class ProductEntity(
     val slug: String? = null,
     val permalink: String? = null,
 
-    val dateCreated: String? = null,
-    val dateModified: String? = null,
+    val dateCreated: Date? = null,
+    val dateModified: Date? = null,
 
-    val type: String? = null, // simple, grouped, external, variable
-    val status: String? = null,
+    val type: Type? = null,
+    val status: CoreProductStatus? = null,
     val isFeatured: Boolean? = null,
-    val catalogVisibility: String? = null, // visible, catalog, search, hidden
+    val catalogVisibility: CoreProductVisibility? = null,
     val description: String? = null,
     val shortDescription: String? = null,
     val sku: String? = null,
@@ -32,10 +45,8 @@ data class ProductEntity(
     val totalSales: Long? = null,
     val isPurchasable: Boolean? = null,
 
-    val dateOnSaleFrom: String? = null,
-    val dateOnSaleTo: String? = null,
-    val dateOnSaleFromGmt: String? = null,
-    val dateOnSaleToGmt: String? = null,
+    val dateOnSaleFromGmt: Date? = null,
+    val dateOnSaleToGmt: Date? = null,
 
     val isVirtual: Boolean? = null,
     val isDownloadable: Boolean? = null,
@@ -46,14 +57,14 @@ data class ProductEntity(
     val externalUrl: String? = null,
     val buttonText: String? = null,
 
-    val taxStatus: String? = null, // taxable, shipping, none
+    val taxStatus: TaxStatus? = null,
     val taxClass: String? = null,
 
     val isStockManaged: Boolean? = null,
     val stockQuantity: Double? = null,
-    val stockStatus: String? = null, // instock, outofstock, onbackorder
+    val stockStatus: StockStatus? = null,
 
-    val backorders: String? = null, // no, notify, yes
+    val backorders: BackorderStatus? = null,
     val areBackordersAllowed: Boolean? = null,
     val isBackordered: Boolean? = null,
 
@@ -69,7 +80,108 @@ data class ProductEntity(
     val parentId: Int? = null,
     val purchaseNote: String? = null,
     val menuOrder: Int? = null
-)
+) {
+    sealed class Type(open val value: String) {
+        object Simple : Type(CoreProductType.SIMPLE.value)
+        object Grouped : Type(CoreProductType.GROUPED.value)
+        object External : Type(CoreProductType.EXTERNAL.value)
+        object Variable : Type(CoreProductType.VARIABLE.value)
+        data class Custom(override val value: String) : Type(value)
+
+        companion object {
+            fun fromString(value: String): Type {
+                return when (value.toLowerCase(Locale.US)) {
+                    CoreProductType.GROUPED.value -> Grouped
+                    CoreProductType.EXTERNAL.value -> External
+                    CoreProductType.VARIABLE.value -> Variable
+                    CoreProductType.SIMPLE.value -> Simple
+                    else -> Custom(value)
+                }
+            }
+        }
+    }
+
+    sealed class TaxStatus {
+        object Taxable : TaxStatus()
+        object Shipping : TaxStatus()
+        object None : TaxStatus()
+        data class Custom(val value: String) : TaxStatus()
+
+        companion object {
+            fun fromString(value: String?): TaxStatus {
+                return when (value) {
+                    CoreProductTaxStatus.TAXABLE.value -> Taxable
+                    CoreProductTaxStatus.SHIPPING.value -> Shipping
+                    CoreProductTaxStatus.NONE.value, null, "" -> None
+                    else -> Custom(value)
+                }
+            }
+        }
+
+        override fun toString(): String {
+            return when (this) {
+                Taxable -> CoreProductTaxStatus.TAXABLE.value
+                Shipping -> CoreProductTaxStatus.SHIPPING.value
+                is Custom -> value
+                else -> CoreProductTaxStatus.NONE.value
+            }
+        }
+    }
+
+    sealed class StockStatus {
+        object InStock : StockStatus()
+        object OutOfStock : StockStatus()
+        object OnBackorder : StockStatus()
+        data class Custom(val value: String) : StockStatus()
+
+        companion object {
+            fun fromString(value: String?): StockStatus {
+                return when (value) {
+                    CoreProductStockStatus.IN_STOCK.value, null, "" -> InStock
+                    CoreProductStockStatus.OUT_OF_STOCK.value -> OutOfStock
+                    CoreProductStockStatus.ON_BACK_ORDER.value -> OnBackorder
+                    else -> Custom(value)
+                }
+            }
+        }
+
+        override fun toString(): String {
+            return when (this) {
+                OnBackorder -> CoreProductStockStatus.ON_BACK_ORDER.value
+                OutOfStock -> CoreProductStockStatus.OUT_OF_STOCK.value
+                is Custom -> value
+                else -> CoreProductStockStatus.IN_STOCK.value
+            }
+        }
+    }
+
+    sealed class BackorderStatus {
+        object No : BackorderStatus()
+        object Yes : BackorderStatus()
+        object Notify : BackorderStatus()
+        data class Custom(val value: String) : BackorderStatus()
+
+        companion object {
+            fun fromString(value: String?): BackorderStatus {
+                return when (value) {
+                    CoreProductBackOrders.NO.value, null, "" -> No
+                    CoreProductBackOrders.YES.value -> Yes
+                    CoreProductBackOrders.NOTIFY.value -> Notify
+                    else -> Custom(value)
+                }
+            }
+        }
+
+        override fun toString(): String {
+            return when (this) {
+                Yes -> CoreProductBackOrders.YES.value
+                Notify -> CoreProductBackOrders.NOTIFY.value
+                is Custom -> value
+                else -> CoreProductBackOrders.NO.value
+            }
+        }
+    }
+}
 
 fun WCProductModel.toDataModel(siteId: Long): ProductEntity =
     ProductEntity(
@@ -78,12 +190,12 @@ fun WCProductModel.toDataModel(siteId: Long): ProductEntity =
         name,
         slug,
         permalink,
-        dateCreated,
-        dateModified,
-        type,
-        status,
+        DateTimeUtils.dateUTCFromIso8601(dateCreated),
+        DateTimeUtils.dateUTCFromIso8601(dateModified),
+        Type.fromString(type),
+        CoreProductStatus.fromValue(status),
         featured,
-        catalogVisibility,
+        CoreProductVisibility.fromValue(catalogVisibility),
         description,
         shortDescription,
         sku,
@@ -93,10 +205,8 @@ fun WCProductModel.toDataModel(siteId: Long): ProductEntity =
         onSale,
         totalSales,
         purchasable,
-        dateOnSaleFrom,
-        dateOnSaleTo,
-        dateOnSaleFromGmt,
-        dateOnSaleToGmt,
+        DateTimeUtils.dateUTCFromIso8601(dateOnSaleFromGmt),
+        DateTimeUtils.dateUTCFromIso8601(dateOnSaleToGmt),
         virtual,
         downloadable,
         downloadLimit,
@@ -104,12 +214,12 @@ fun WCProductModel.toDataModel(siteId: Long): ProductEntity =
         soldIndividually,
         externalUrl,
         buttonText,
-        taxStatus,
+        TaxStatus.fromString(taxStatus),
         taxClass,
         manageStock,
         stockQuantity,
-        stockStatus,
-        backorders,
+        StockStatus.fromString(stockStatus),
+        BackorderStatus.fromString(backorders),
         backordersAllowed,
         backordered,
         shippingRequired,
