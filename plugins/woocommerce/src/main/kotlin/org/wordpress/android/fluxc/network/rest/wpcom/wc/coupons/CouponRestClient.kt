fadf06fbd6ea1bc925009df0c73a0d62bdb795ea
Ondrej Ruttkay <ruttkay@gmail.com>
2022-03-15 22:03:21 +0100

    Add the create coupon REST client call

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/coupons/CouponRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/coupons/CouponRestClient.kt
@@ -5,14 +5,19 @@ import com.android.volley.RequestQueue
 import org.wordpress.android.fluxc.Dispatcher
 import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.coupon.UpdateCouponRequest
+import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.UNKNOWN
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
 import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder
 import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder.JetpackResponse.JetpackError
 import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder.JetpackResponse.JetpackSuccess
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType.GENERIC_ERROR
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.toWooError
+import org.wordpress.android.fluxc.persistence.entity.CouponEntity
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
@@ -53,6 +58,35 @@ class CouponRestClient @Inject constructor(
         }
     }
 
+    suspend fun createCoupon(
+        site: SiteModel,
+        request: UpdateCouponRequest
+    ): WooPayload<CouponEntity> {
+        val url = WOOCOMMERCE.orders.pathV3
+        val params = request.toNetworkRequest()
+
+        val response = jetpackTunnelGsonRequestBuilder.syncPostRequest(
+            this,
+            site,
+            url,
+            params,
+            CouponDto::class.java
+        )
+
+        return when (response) {
+            is JetpackError -> WooPayload(response.error.toWooError())
+            is JetpackSuccess -> response.data?.let { couponDto ->
+                WooPayload(couponDto.toDataModel(site.siteId))
+            } ?: WooPayload(
+                error = WooError(
+                    type = GENERIC_ERROR,
+                    original = UNKNOWN,
+                    message = "Success response with empty data"
+                )
+            )
+        }
+    }
+
     suspend fun deleteCoupon(
         site: SiteModel,
         couponId: Long,
@@ -71,6 +105,32 @@ class CouponRestClient @Inject constructor(
         return when (response) {
             is JetpackError -> WooPayload(response.error.toWooError())
             is JetpackSuccess -> WooPayload(Unit)
+        }
+    }
+
+
+    private fun UpdateCouponRequest.toNetworkRequest(): Map<String, Any> {
+        return mutableMapOf<String, Any>().apply {
+            code?.let { put("code", it) }
+            discountType?.let { put("discount_type", it) }
+            description?.let { put("description", it) }
+            dateExpires?.let { put("date_expires", it) }
+            dateExpiresGmt?.let { put("date_expires_gmt", it) }
+            usageCount?.let { put("usage_count", it) }
+            minimumAmount?.let { put("minimum_amount", it) }
+            maximumAmount?.let { put("maximum_amount", it) }
+            productIds?.let { put("product_ids", it) }
+            excludedProductIds?.let { put("excluded_product_ids", it) }
+            isShippingFree?.let { put("free_shipping", it) }
+            productCategoryIds?.let { put("product_categories", it) }
+            excludedProductCategoryIds?.let { put("excluded_product_categories", it) }
+            usageLimit?.let { put("usage_limit", it) }
+            usageLimitPerUser?.let { put("usage_limit_per_user", it) }
+            limitUsageToXItems?.let { put("limit_usage_to_x_items", it) }
+            restrictedEmails?.let { put("email_restrictions", it) }
+            isForIndividualUse?.let { put("individual_use", it) }
+            areSaleItemsExcluded?.let { put("exclude_sale_items", it) }
+            usedBy?.let { put("used_by", it) }
         }
     }
 }
