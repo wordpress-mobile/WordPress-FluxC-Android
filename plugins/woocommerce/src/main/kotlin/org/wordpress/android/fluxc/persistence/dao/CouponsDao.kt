011deb8ae48e88ce64345355dc2d584857d230ea
Ondrej Ruttkay <ruttkay@gmail.com>
2022-03-30 17:48:30 +0200

    Replace the existing coupon upsert methods explicity insert or update that prevents cascading deletes of related entities

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/CouponsDao.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/CouponsDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 import org.wordpress.android.fluxc.persistence.entity.CouponAndProductCategoryEntity
 import org.wordpress.android.fluxc.persistence.entity.CouponAndProductEntity
@@ -22,17 +23,63 @@ abstract class CouponsDao {
     @Query("SELECT * FROM Coupons WHERE siteId = :siteId AND id = :couponId")
     abstract fun observeSingleCoupon(siteId: Long, couponId: Long): Flow<CouponWithEmails?>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertOrUpdateCoupon(entity: CouponEntity)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun insertCoupon(entity: CouponEntity): Long
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertOrUpdateCouponAndProductCategory(
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun insertCouponAndProductCategory(
         entity: CouponAndProductCategoryEntity
-    )
+    ): Long
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertOrUpdateCouponAndProduct(entity: CouponAndProductEntity)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun insertCouponAndProduct(entity: CouponAndProductEntity): Long
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertOrUpdateCouponEmail(entity: CouponEmailEntity)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun insertCouponEmail(entity: CouponEmailEntity): Long
+
+    @Update
+    abstract suspend fun updateCoupon(entity: CouponEntity)
+
+    @Update
+    abstract suspend fun updateCouponEmail(entity: CouponEmailEntity)
+
+    @Update
+    abstract suspend fun updateCouponAndProductCategory(entity: CouponAndProductCategoryEntity)
+
+    @Update
+    abstract suspend fun updateCouponAndProduct(entity: CouponAndProductEntity)
+
+    @Transaction
+    open suspend fun insertOrUpdateCoupon(entity: CouponEntity) {
+        val id: Long = insertCoupon(entity)
+        if (id == -1L) {
+            updateCoupon(entity)
+        }
+    }
+
+    @Transaction
+    open suspend fun insertOrUpdateCouponAndProductCategory(
+        entity: CouponAndProductCategoryEntity
+    ) {
+        val id: Long = insertCouponAndProductCategory(entity)
+        if (id == -1L) {
+            updateCouponAndProductCategory(entity)
+        }
+    }
+
+    @Transaction
+    open suspend fun insertOrUpdateCouponAndProduct(entity: CouponAndProductEntity) {
+        val id: Long = insertCouponAndProduct(entity)
+        if (id == -1L) {
+            updateCouponAndProduct(entity)
+        }
+    }
+
+    @Transaction
+    open suspend fun insertOrUpdateCouponEmail(entity: CouponEmailEntity) {
+        val id: Long = insertCouponEmail(entity)
+        if (id == -1L) {
+            updateCouponEmail(entity)
+        }
+    }
 }
