f03149fc297ffbf789a67a393779f38bae71d6a7
JorgeMucientes <jorge.mucientes@automattic.com>
2022-04-04 12:55:50 +0200

    Remove redundant default values

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/InboxNoteEntity.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/InboxNoteEntity.kt
@@ -27,9 +27,9 @@ data class InboxNoteEntity(
     val content: String,
     val dateCreated: String,
     val status: LocalInboxNoteStatus,
-    val source: String? = null,
-    val type: String? = null,
-    val dateReminder: String? = null
+    val source: String?,
+    val type: String?,
+    val dateReminder: String?
 ) {
     enum class LocalInboxNoteStatus {
         Unactioned,
