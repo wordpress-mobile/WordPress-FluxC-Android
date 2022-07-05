1a573c25bf5a6934151e338af6d41ffb2b40c982
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-05 12:30:56 -0400

    Include CHARGE_ID_KEY and SHIPPING_PHONE_KEY in metadata

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/StripOrder.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/StripOrder.kt
@@ -22,8 +22,9 @@ internal class StripOrder @Inject constructor(private val gson: Gson) {
                 metaData = gson.toJson(
                         fatModel.getMetaDataList()
                                 .filter {
-                                            // it.key == CHARGE_ID_KEY || it.key == SHIPPING_PHONE_KEY
-                                    !it.key.startsWith("_")
+                                    it.key == CHARGE_ID_KEY ||
+                                        it.key == SHIPPING_PHONE_KEY ||
+                                        it.key.startsWith("_").not()
                                 }
                 )
         )
