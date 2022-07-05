c15edc3a9f00791a747cb818ba78b1b476c89684
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-05 14:46:16 -0400

    Removed unused fun

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/OrderMetaDataEntity.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/entity/OrderMetaDataEntity.kt
@@ -2,13 +2,8 @@ package org.wordpress.android.fluxc.persistence.entity
 
 import androidx.room.Entity
 import androidx.room.Index
-import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
-import com.google.gson.reflect.TypeToken
 import org.wordpress.android.fluxc.model.LocalOrRemoteId
-import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
-import org.wordpress.android.fluxc.model.WCMetaData
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderDto
 
 /**
  * The OrderMetaDataEntity table is used to store viewable order meta data separately from the order
@@ -29,23 +24,3 @@ data class OrderMetaDataEntity(
     @SerializedName("display_key") val displayKey: String?,
     @SerializedName("display_value") val displayValue: String?
 )
-
-fun fromOrderDto(orderDto: OrderDto, localSiteId: LocalId): List<OrderMetaDataEntity> {
-    val responseType = object : TypeToken<List<WCMetaData>>() {}.type
-    val metaData = Gson().fromJson(orderDto.meta_data, responseType) as? List<WCMetaData> ?: emptyList()
-    return ArrayList<OrderMetaDataEntity>().also { list ->
-        metaData.forEach { meta ->
-            list.add(
-                OrderMetaDataEntity(
-                    id = meta.id,
-                    localSiteId = localSiteId,
-                    orderId = orderDto.id ?: 0,
-                    key = meta.key,
-                    value = meta.value.toString(),
-                    displayKey = meta.displayKey,
-                    displayValue = meta.displayKey
-                )
-            )
-        }
-    }
-}
