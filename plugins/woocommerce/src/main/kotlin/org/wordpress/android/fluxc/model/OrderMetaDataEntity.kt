d921d7399e853830e90691a42b47719cc3557823
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-05 12:41:04 -0400

    Added OrderMetaDataEntity

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/OrderMetaDataEntity.kt
@@ -0,0 +1,22 @@
+package org.wordpress.android.fluxc.model
+
+import androidx.room.Entity
+import androidx.room.Index
+import com.google.gson.annotations.SerializedName
+
+@Entity(
+    tableName = "OrderEntity",
+    indices = [Index(
+        value = ["localSiteId", "orderId"]
+    )],
+    primaryKeys = ["localSiteId", "orderId", "id"]
+)
+data class OrderMetaDataEntity(
+    @SerializedName("id") val id: Long,
+    @SerializedName("localSiteId") val localSiteId: Long,
+    @SerializedName("orderId") val orderId: Long,
+    @SerializedName("key") val key: String,
+    @SerializedName("value") val value: Any,
+    @SerializedName("display_key") val displayKey: String?,
+    @SerializedName("display_value") val displayValue: Any?
+)
