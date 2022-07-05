e3baa85e54a63ba59461ef870cf65dc06c7fa57a
Nick Bradbury <nick.bradbury@gmail.com>
2022-07-05 12:15:58 -0400

    Include all metadata that doesn't start with an underscore

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/StripOrder.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/StripOrder.kt
@@ -22,7 +22,8 @@ internal class StripOrder @Inject constructor(private val gson: Gson) {
                 metaData = gson.toJson(
                         fatModel.getMetaDataList()
                                 .filter {
-                                            it.key == CHARGE_ID_KEY || it.key == SHIPPING_PHONE_KEY
+                                            // it.key == CHARGE_ID_KEY || it.key == SHIPPING_PHONE_KEY
+                                    !it.key.startsWith("_")
                                 }
                 )
         )
