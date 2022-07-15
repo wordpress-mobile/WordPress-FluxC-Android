76921d15f4490fe77b0caf1bb95e7895cac51755
ThomazFB <thomazfcortez@gmail.com>
2022-07-15 02:39:59 -0300

    Create first fully configured test in OrderMetaDataHandlerTest

--- plugins/woocommerce/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderMetaDataHandlerTest.kt
+++ plugins/woocommerce/src/test/java/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderMetaDataHandlerTest.kt
@@ -3,7 +3,10 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import org.junit.Before
+import org.junit.Test
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
 import org.wordpress.android.fluxc.model.WCMetaData
 import org.wordpress.android.fluxc.persistence.dao.OrderMetaDataDao
 
@@ -11,11 +14,10 @@ class OrderMetaDataHandlerTest {
     private lateinit var sut: OrderMetaDataHandler
     private lateinit var orderDtoMock: OrderDto
     private lateinit var gsonMock: Gson
-    private val orderMetaDataDaoMock
-        get() = mock<OrderMetaDataDao>()
+    private val orderMetaDataDaoMock = mock<OrderMetaDataDao>()
 
     @Before
-    private fun setUp() {
+    fun setUp() {
         configureMocks()
         sut = OrderMetaDataHandler(gsonMock, orderMetaDataDaoMock)
     }
@@ -31,4 +33,40 @@ class OrderMetaDataHandlerTest {
             )
         }
     }
+
+    @Test
+    fun `when metadata contains internal keys, should remove all of them`() {
+        // Given
+        val rawMetadata = generateMetadata(amount = 100) {
+            WCMetaData(
+                id = it.toLong(),
+                key = "_$it key",
+                value = "$it value",
+                displayKey = null,
+                displayValue = null
+            )
+        }
+        gsonMock = mock {
+            val responseType = object : TypeToken<List<WCMetaData>>() {}.type
+            on { fromJson<List<WCMetaData>?>(orderDtoMock.meta_data, responseType) }.thenReturn(
+                rawMetadata
+            )
+        }
+        sut = OrderMetaDataHandler(gsonMock, orderMetaDataDaoMock)
+
+        // When
+        sut(orderDtoMock, LocalId(1))
+
+        // Then
+        verify(orderMetaDataDaoMock).updateOrderMetaData(
+            orderId = 1,
+            localSiteId = LocalId(1),
+            metaData = emptyList()
+        )
+    }
+
+    private fun generateMetadata(
+        amount: Int,
+        constructor: (Int) -> WCMetaData
+    ) = (1..amount).map { constructor(it) }
 }
