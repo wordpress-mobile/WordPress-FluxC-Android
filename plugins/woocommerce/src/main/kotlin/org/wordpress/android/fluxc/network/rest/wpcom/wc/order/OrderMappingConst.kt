af0af4e8ca78047b3c33d221358b71cb8bddf5d1
ThomazFB <thomazfcortez@gmail.com>
2022-08-12 22:12:33 -0300

    Revert semantic of isInternalAttribute property

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderMappingConst.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/OrderMappingConst.kt
@@ -5,13 +5,11 @@ import org.wordpress.android.fluxc.model.WCMetaData
 object OrderMappingConst {
     const val CHARGE_ID_KEY = "_charge_id"
     const val SHIPPING_PHONE_KEY = "_shipping_phone"
-
     /**
-     * Verify if the Metadata key is a internal store attribute
-     * true if the `key` starts with the `_` character
-     * null if the key is null and it's not possible to verify if it's internal or not
-     * false otherwise
+     * Verify if the Metadata key is not null or a internal store attribute
+     * @return false if the `key` is null or starts with the `_` character
+     * @return true otherwise
      */
-    internal val WCMetaData.isInternalAttribute
-        get() = key?.startsWith('_')
+    internal val WCMetaData.isDisplayableAttribute
+        get() = key?.startsWith('_') ?: false
 }
--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/StripOrder.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/StripOrder.kt
@@ -5,7 +5,7 @@ import org.wordpress.android.fluxc.model.OrderEntity
 import org.wordpress.android.fluxc.model.order.LineItem
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderMappingConst.CHARGE_ID_KEY
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderMappingConst.SHIPPING_PHONE_KEY
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderMappingConst.isInternalAttribute
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderMappingConst.isDisplayableAttribute
 import javax.inject.Inject
 
 internal class StripOrder @Inject constructor(private val gson: Gson) {
@@ -14,7 +14,7 @@ internal class StripOrder @Inject constructor(private val gson: Gson) {
                 lineItems = gson.toJson(fatModel.getLineItemList().map { lineItemDto: LineItem ->
                     lineItemDto.copy(
                         metaData = lineItemDto.metaData
-                            ?.filter { it.isInternalAttribute?.not() ?: false }
+                            ?.filter { it.isDisplayableAttribute }
                     )
                 }),
                 shippingLines = gson.toJson(fatModel.getShippingLineList()),
