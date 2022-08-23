2bed6199118bbd97ec6954430e05b1fab954be9d
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2022-08-23 09:05:48 -0400

    Add mocked json file for the GatewayRestClient

--- example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCGatewayTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/mocked/MockedStack_WCGatewayTest.kt
@@ -1,13 +1,18 @@
 package org.wordpress.android.fluxc.mocked
 
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.module.ResponseMockingInterceptor
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.gateways.GatewayRestClient
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.gateways.GatewayRestClient.GatewayId.CASH_ON_DELIVERY
 import javax.inject.Inject
 
+
 class MockedStack_WCGatewayTest: MockedStack_Base() {
-    @Inject internal lateinit var gatewayRestClient: GatewayRestClient
+    @Inject internal lateinit var restClient: GatewayRestClient
     @Inject internal lateinit var interceptor: ResponseMockingInterceptor
 
     @Throws(Exception::class)
@@ -17,19 +22,13 @@ class MockedStack_WCGatewayTest: MockedStack_Base() {
     }
 
     @Test
-    fun givenSiteHasCODEnabledThenSuccessReturned() = runBlocking {
-        interceptor.respondWith("")
+    fun givenSiteHasCodEnabledThenSuccessReturned() = runBlocking {
+        interceptor.respondWith("wc-pay-fetch-cod-enabled-response-success.json")
+
+        val result = restClient.updatePaymentGateway(
+            SiteModel().apply { siteId = 123L },
+            CASH_ON_DELIVERY,
+            enabled = true
+        )
     }
 }
-
-
-
-//@Test
-//    fun givenSiteHasWCPayWhenFetchConnectionTokenInvokedThenTokenReturned() = runBlocking {
-//        interceptor.respondWith("wc-pay-fetch-connection-token-response-success.json")
-//
-//        val result = restClient.fetchConnectionToken(WOOCOMMERCE_PAYMENTS, SiteModel().apply { siteId = 123L })
-//
-//        assertTrue(result.result?.token?.isNotEmpty() == true)
-//        assertTrue(result.result?.isTestMode == true)
-//    }
