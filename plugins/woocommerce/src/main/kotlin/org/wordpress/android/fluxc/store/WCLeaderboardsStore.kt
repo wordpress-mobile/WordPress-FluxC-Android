ef2b62681f744c787d1c2ed9707f2ad150ec233f
JorgeMucientes <jorge.mucientes@automattic.com>
2022-09-09 18:34:02 +0200

    Remove old functions to fetch top performer products

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WCLeaderboardsStore.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/WCLeaderboardsStore.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.leaderboards.WCProductLeaderboardsMapper
-import org.wordpress.android.fluxc.model.leaderboards.WCTopPerformerProductModel
 import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.UNKNOWN
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType.GENERIC_ERROR
@@ -12,8 +11,6 @@ import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsApiResponse.Type.PRODUCTS
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.leaderboards.LeaderboardsRestClient
-import org.wordpress.android.fluxc.persistence.WCLeaderboardsSqlUtils.getCurrentLeaderboards
-import org.wordpress.android.fluxc.persistence.WCLeaderboardsSqlUtils.insertNewLeaderboards
 import org.wordpress.android.fluxc.persistence.dao.TopPerformerProductsDao
 import org.wordpress.android.fluxc.persistence.entity.TopPerformerProductEntity
 import org.wordpress.android.fluxc.store.WCStatsStore.StatsGranularity
@@ -31,61 +28,6 @@ class WCLeaderboardsStore @Inject constructor(
     private val coroutineEngine: CoroutineEngine,
     private val topPerformersDao: TopPerformerProductsDao,
 ) {
-    suspend fun fetchProductLeaderboards(
-        site: SiteModel,
-        unit: StatsGranularity = DAYS,
-        queryTimeRange: LongRange? = null,
-        quantity: Int? = null,
-        addProductsPath: Boolean = false,
-        forceRefresh: Boolean = false
-    ): WooResult<List<WCTopPerformerProductModel>> =
-        coroutineEngine.withDefaultContext(AppLog.T.API, this, "fetchLeaderboards") {
-            fetchAllLeaderboards(site, unit, queryTimeRange, quantity, addProductsPath, forceRefresh)
-                .model
-                ?.firstOrNull { it.type == PRODUCTS }
-                ?.run { mapper.map(this, site, productStore, unit) }
-                ?.let {
-                    insertNewLeaderboards(it, site.id, unit)
-                    getCurrentLeaderboards(site.id, unit)
-                }
-                ?.distinctBy { it.product.remoteProductId }
-                ?.let { WooResult(it) }
-                ?: getCurrentLeaderboards(site.id, unit)
-                    .takeIf { it.isNotEmpty() }
-                    ?.let { WooResult(it) }
-                ?: WooResult(WooError(GENERIC_ERROR, UNKNOWN))
-        }
-
-    fun fetchCachedProductLeaderboards(
-        site: SiteModel,
-        unit: StatsGranularity
-    ) = WooResult(getCurrentLeaderboards(site.id, unit))
-
-    private suspend fun fetchAllLeaderboards(
-        site: SiteModel,
-        unit: StatsGranularity? = null,
-        queryTimeRange: LongRange? = null,
-        quantity: Int? = null,
-        addProductsPath: Boolean = false,
-        forceRefresh: Boolean
-    ): WooResult<List<LeaderboardsApiResponse>> =
-        with(
-            restClient.fetchLeaderboards(
-                site,
-                unit,
-                queryTimeRange,
-                quantity,
-                addProductsPath,
-                forceRefresh
-            )
-        ) {
-            return when {
-                isError -> WooResult(error)
-                result != null -> WooResult(result.toList())
-                else -> WooResult(WooError(GENERIC_ERROR, UNKNOWN))
-            }
-        }
-
     fun observeTopPerformerProducts(
         siteId: Long,
         granularity: StatsGranularity
@@ -97,9 +39,10 @@ class WCLeaderboardsStore @Inject constructor(
     fun getCachedTopPerformerProducts(
         siteId: Long,
         granularity: StatsGranularity
-    ): List<TopPerformerProductEntity> = topPerformersDao.getTopPerformerProductsFor(siteId, granularity.toString())
+    ): List<TopPerformerProductEntity> =
+        topPerformersDao.getTopPerformerProductsFor(siteId, granularity.toString())
 
-    suspend fun fetchProductLeaderboardsNew(
+    suspend fun fetchTopPerformerProducts(
         site: SiteModel,
         granularity: StatsGranularity = DAYS,
         queryTimeRange: LongRange? = null,
@@ -134,5 +77,30 @@ class WCLeaderboardsStore @Inject constructor(
                     )
                     WooResult(it)
                 } ?: WooResult(WooError(GENERIC_ERROR, UNKNOWN))
+        }
+
+    private suspend fun fetchAllLeaderboards(
+        site: SiteModel,
+        unit: StatsGranularity? = null,
+        queryTimeRange: LongRange? = null,
+        quantity: Int? = null,
+        addProductsPath: Boolean = false,
+        forceRefresh: Boolean
+    ): WooResult<List<LeaderboardsApiResponse>> =
+        with(
+            restClient.fetchLeaderboards(
+                site,
+                unit,
+                queryTimeRange,
+                quantity,
+                addProductsPath,
+                forceRefresh
+            )
+        ) {
+            return when {
+                isError -> WooResult(error)
+                result != null -> WooResult(result.toList())
+                else -> WooResult(WooError(GENERIC_ERROR, UNKNOWN))
+            }
         }
 }
