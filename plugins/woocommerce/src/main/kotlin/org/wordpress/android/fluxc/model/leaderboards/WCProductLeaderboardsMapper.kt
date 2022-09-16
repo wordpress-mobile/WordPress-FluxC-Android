5584ac9e82ee3c25f789812bd48528464afec568
JorgeMucientes <jorge.mucientes@automattic.com>
2022-09-16 19:29:40 +0200

    Remove unused methods from WCLeaderboardsStore

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/leaderboards/WCProductLeaderboardsMapper.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/leaderboards/WCProductLeaderboardsMapper.kt
@@ -13,22 +13,6 @@ import org.wordpress.android.fluxc.store.WCStatsStore.StatsGranularity
 import javax.inject.Inject
 
 class WCProductLeaderboardsMapper @Inject constructor() {
-    private val gson = Gson()
-
-    suspend fun map(
-        response: LeaderboardsApiResponse,
-        site: SiteModel,
-        productStore: WCProductStore,
-        unit: StatsGranularity
-    ) = response.products
-        ?.takeIf { it.isNotEmpty() }
-        ?.mapNotNull { it.productId }
-        ?.asProductList(site, productStore)
-        ?.mapNotNull { product ->
-            response.products
-                ?.find { it.productId == product.remoteProductId }
-                ?.let { product.toWCTopPerformerProductModel(it, site, unit) }
-        }.orEmpty()
 
     suspend fun mapTopPerformerProductsEntity(
         response: LeaderboardsApiResponse,
@@ -75,19 +59,6 @@ class WCProductLeaderboardsMapper @Inject constructor() {
             addAll(locallyFetchedProducts)
         }.toList()
     }
-
-    private fun WCProductModel.toWCTopPerformerProductModel(
-        productItem: LeaderboardProductItem,
-        site: SiteModel,
-        unit: StatsGranularity
-    ) = WCTopPerformerProductModel(
-            gson.toJson(this),
-            productItem.currency.toString(),
-            productItem.quantity?.toIntOrNull() ?: 0,
-            productItem.total?.toDoubleOrNull() ?: 0.0,
-            site.id,
-            unit.toString()
-    )
 
     private fun WCProductModel.toTopPerformerProductEntity(
         productItem: LeaderboardProductItem,
