b0cd7c8bfff37baf30b829ce7e87a89b5bd40d8d
Anirudh Uppunda <abhat38@gmail.com>
2022-10-20 17:12:00 +0530

    Add JITM store class

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/JitmStore.kt
@@ -0,0 +1,23 @@
+package org.wordpress.android.fluxc.store
+
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.jitm.JitmRestClient
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.payments.inperson.JITMApiResponse
+import org.wordpress.android.fluxc.tools.CoroutineEngine
+import org.wordpress.android.util.AppLog
+import javax.inject.Inject
+
+class JitmStore @Inject constructor(
+    private val coroutineEngine: CoroutineEngine,
+    private val restClient: JitmRestClient
+) {
+    suspend fun fetchJitmMessage(
+        site: SiteModel,
+        messagePath: String
+    ): WooResult<Array<JITMApiResponse>> {
+        return coroutineEngine.withDefaultContext(AppLog.T.API, this, "jitm") {
+            restClient.fetchJitmMessage(site, messagePath).asWooResult()
+        }
+    }
+}
