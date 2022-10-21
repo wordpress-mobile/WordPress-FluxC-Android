3ad1a5a704729e22b6b34a129e23c58b311ddb0f
Anirudh Uppunda <abhat38@gmail.com>
2022-10-21 17:14:24 +0530

    Add jitm dismiss logic in JitmStore.kt

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/JitmStore.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/JitmStore.kt
@@ -2,8 +2,8 @@ package org.wordpress.android.fluxc.store
 
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooResult
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.jitm.JITMApiResponse
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.jitm.JitmRestClient
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.payments.inperson.JITMApiResponse
 import org.wordpress.android.fluxc.tools.CoroutineEngine
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
@@ -20,6 +20,19 @@ class JitmStore @Inject constructor(
     ): WooResult<Array<JITMApiResponse>> {
         return coroutineEngine.withDefaultContext(AppLog.T.API, this, "jitm") {
             restClient.fetchJitmMessage(site, messagePath).asWooResult()
+        }
+    }
+
+    suspend fun dismissJitmMessage(site: SiteModel, jitmId: String): Boolean {
+        return coroutineEngine.withDefaultContext(AppLog.T.API, this, "jitm-dismiss") {
+            val response = restClient.dismissJitmMessage(site, jitmId).asWooResult()
+            return@withDefaultContext when {
+                response.isError -> false
+                response.model != null -> {
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
