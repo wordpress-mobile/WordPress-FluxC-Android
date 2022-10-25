0b0abf75f1cad2073d7cad4284a4ff22a9bcb2e0
Rodrigo Souza <rodrigo.otavio89@gmail.com>
2022-10-25 20:03:07 -0300

    Merge branch 'trunk' into bugfix/issue-2536-add-support-to-orders-batch-update

--- example/src/test/java/org/wordpress/android/fluxc/wc/leaderboards/WCLeaderboardsStoreTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/leaderboards/WCLeaderboardsStoreTest.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.fluxc.wc.leaderboards
 
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.times
@@ -62,8 +63,8 @@ class WCLeaderboardsStoreTest {
 
     @Test
     fun `fetch top performer products with empty result should return WooError`() = test {
-        setup()
         givenFetchLeaderBoardsReturns(emptyArray())
+        setup()
 
         val result = storeUnderTest.fetchTopPerformerProducts(stubSite)
 
@@ -179,8 +180,17 @@ class WCLeaderboardsStoreTest {
     }
 
     private suspend fun givenFetchLeaderBoardsReturns(response: Array<LeaderboardsApiResponse>?) {
-        whenever(restClient.fetchLeaderboards(stubSite, DAYS, null, null, forceRefresh = false))
-            .thenReturn(WooPayload(response))
+        whenever(
+            restClient.fetchLeaderboards(
+                site = any(),
+                unit = anyOrNull(),
+                startDate = anyOrNull(),
+                endDate = anyOrNull(),
+                quantity = anyOrNull(),
+                addProductsPath = any(),
+                forceRefresh = any(),
+            )
+        ).thenReturn(WooPayload(response))
     }
 
     private fun createStoreUnderTest() {
