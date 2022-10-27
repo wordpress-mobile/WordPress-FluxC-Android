250e346e98666298a804a9a93f649d399fe18e63
Anirudh Uppunda <abhat38@gmail.com>
2022-10-27 18:43:25 +0530

    delete JitmDismissApiResponse.kt

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/jitm/JitmDismissApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package org.wordpress.android.fluxc.network.rest.wpcom.wc.jitm
-
-import com.google.gson.annotations.SerializedName
-
-data class JitmDismissApiResponse(
-    @SerializedName("data") val data: Boolean
-)
