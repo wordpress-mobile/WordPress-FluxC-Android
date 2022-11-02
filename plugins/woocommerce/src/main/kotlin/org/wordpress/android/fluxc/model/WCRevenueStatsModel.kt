06e1fc7f8c6c514e919c7970c3196c95d7a8c0ed
Alejo <alejandro.torres.veiga@automattic.com>
2022-11-02 17:35:39 -0300

    add net revenue and average order's value to interval subtotals

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCRevenueStatsModel.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCRevenueStatsModel.kt
@@ -40,6 +40,10 @@ data class WCRevenueStatsModel(@PrimaryKey @Column private var id: Int = 0) : Id
         val ordersCount: Long? = null
         @SerializedName("total_sales")
         val totalSales: Double? = null
+        @SerializedName("net_revenue")
+        val netRevenue: Double? = null
+        @SerializedName("avg_order_value")
+        val avgOrderValue: Double? = null
     }
 
     /**
