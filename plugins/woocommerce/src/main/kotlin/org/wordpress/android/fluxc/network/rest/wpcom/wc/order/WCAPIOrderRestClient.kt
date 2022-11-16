8c4988fd23af314d34d30d7a4ee4d8db62cb5229
Hafiz Rahman <hfzrhmn@gmail.com>
2022-11-16 17:02:36 +0700

    Add WC API specific rest clients for Order and System

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/order/WCAPIOrderRestClient.kt
@@ -0,0 +1,86 @@
+@file:Suppress("DEPRECATION_ERROR")
+package org.wordpress.android.fluxc.network.rest.wpcom.wc.order
+
+import com.android.volley.RequestQueue
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
+import org.wordpress.android.fluxc.model.OrderEntity
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.model.order.UpdateOrderRequest
+import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType
+import org.wordpress.android.fluxc.network.UserAgent
+import org.wordpress.android.fluxc.network.rest.wcapi.BaseWCAPIRestClient
+import org.wordpress.android.fluxc.network.rest.wcapi.WCAPIGsonRequestBuilder
+import org.wordpress.android.fluxc.network.rest.wcapi.WCAPIResponse.Success
+import org.wordpress.android.fluxc.network.rest.wcapi.WCAPIResponse.Error
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.OrderDtoMapper.Companion.toDto
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Singleton
+class WCAPIOrderRestClient @Inject constructor(
+        dispatcher: Dispatcher,
+        @Named("regular") requestQueue: RequestQueue,
+        private val wcAPIGsonRequestBuilder: WCAPIGsonRequestBuilder,
+        private val orderDtoMapper: OrderDtoMapper,
+        userAgent: UserAgent
+) : BaseWCAPIRestClient(dispatcher, requestQueue, userAgent) {
+
+    suspend fun createOrder(
+            site: SiteModel,
+            request: UpdateOrderRequest
+    ): WooPayload<OrderEntity> {
+        val url = site.url + "/wp-json" + WOOCOMMERCE.orders.pathV3
+        val basicAuthKey = "ENTER_AUTH_KEY"
+
+        val params = request.toNetworkRequest()
+
+        val response = wcAPIGsonRequestBuilder.syncPostRequest(
+                this,
+                url,
+                params,
+                OrderDto::class.java,
+                basicAuthKey
+        )
+
+        return when (response) {
+            is Success -> {
+                response.data?.let { orderDto ->
+                    WooPayload(orderDtoMapper.toDatabaseEntity(orderDto, site.localId()).first)
+                } ?: WooPayload(
+                        error = WooError(
+                                type = WooErrorType.GENERIC_ERROR,
+                                original = GenericErrorType.UNKNOWN,
+                                message = "Success response with empty data"
+                        )
+                )
+            }
+            is Error -> {
+                WooPayload(
+                        WooError(
+                                WooErrorType.GENERIC_ERROR,
+                                GenericErrorType.UNKNOWN,
+                                response.error.message)
+                )
+            }
+        }
+    }
+
+
+    private fun UpdateOrderRequest.toNetworkRequest(): Map<String, Any> {
+        return mutableMapOf<String, Any>().apply {
+            status?.let { put("status", it.statusKey) }
+            lineItems?.let { put("line_items", it) }
+            shippingAddress?.toDto()?.let { put("shipping", it) }
+            billingAddress?.toDto()?.let { put("billing", it) }
+            feeLines?.let { put("fee_lines", it) }
+            shippingLines?.let { put("shipping_lines", it) }
+            customerNote?.let { put("customer_note", it) }
+        }
+    }
+
+}
---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/system/WCAPISystemRestClient.kt
@@ -0,0 +1,68 @@
+package org.wordpress.android.fluxc.network.rest.wpcom.wc.system
+
+import com.android.volley.RequestQueue
+import com.google.gson.JsonElement
+import com.google.gson.annotations.SerializedName
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.BaseRequest
+import org.wordpress.android.fluxc.network.UserAgent
+import org.wordpress.android.fluxc.network.rest.wcapi.BaseWCAPIRestClient
+import org.wordpress.android.fluxc.network.rest.wcapi.WCAPIGsonRequestBuilder
+import org.wordpress.android.fluxc.network.rest.wcapi.WCAPIResponse.Success
+import org.wordpress.android.fluxc.network.rest.wcapi.WCAPIResponse.Error
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Singleton
+class WCAPISystemRestClient @Inject constructor(
+        dispatcher: Dispatcher,
+        private val wcAPIGsonRequestBuilder: WCAPIGsonRequestBuilder,
+        @Named("regular") requestQueue: RequestQueue,
+        userAgent: UserAgent
+) : BaseWCAPIRestClient(dispatcher, requestQueue, userAgent) {
+
+    suspend fun fetchSSR(site: SiteModel): WooPayload<SSRResponse> {
+        val url = site.url + "/wp-json" + WOOCOMMERCE.system_status.pathV3
+        val basicAuthKey = "ENTER_AUTH_KEY"
+
+        val response = wcAPIGsonRequestBuilder.syncGetRequest(
+            restClient = this,
+            url = url,
+            params = mapOf("_fields" to "environment,database,active_plugins,theme,settings,security,pages"),
+            body = emptyMap(),
+            clazz = SSRResponse::class.java,
+            enableCaching = true,
+            cacheTimeToLive = BaseRequest.DEFAULT_CACHE_LIFETIME,
+            basicAuthKey = basicAuthKey
+        )
+        return when (response) {
+            is Success -> {
+                WooPayload(response.data)
+            }
+            is Error -> {
+                WooPayload(
+                        WooError(
+                        WooErrorType.GENERIC_ERROR,
+                        BaseRequest.GenericErrorType.UNKNOWN,
+                        response.error.message)
+                )
+            }
+        }
+    }
+
+    data class SSRResponse(
+            val environment: JsonElement? = null,
+            val database: JsonElement? = null,
+            @SerializedName("active_plugins") val activePlugins: JsonElement? = null,
+            val theme: JsonElement? = null,
+            val settings: JsonElement? = null,
+            val security: JsonElement? = null,
+            val pages: JsonElement? = null
+    )
+}
