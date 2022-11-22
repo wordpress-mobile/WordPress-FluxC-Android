fafc3e86c156f572e706968dbd7ec65a5d569fdf
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-11-22 17:03:37 +0200

    Analysis: Remove unnecessary commons lang random string utils
    The 'RandomStringUtils.randomAlphanumeric/Alphabetic/Number(...)' can be
    safely replaced with a Kotlin equivalent solution.
    
    As such, this commit also removes the unnecessary org apache commons
    lang Android test dependency for the 'example' module.
    
    FYI: Since this is a random string utility, it is difficult to unit
    test that. However, I did manually unit test those for correctness,
    compared those to the Apache Commons Lang3 'RandomStringUtils' and
    verified that the results are the expected ones. Also, the connected
    tests can verify correctness of this change as well.

--- example/src/main/java/org/wordpress/android/fluxc/example/utils/RandomStringUtils.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/utils/RandomStringUtils.kt
@@ -1,20 +1,44 @@
 package org.wordpress.android.fluxc.example.utils
 
-import org.apache.commons.lang3.RandomStringUtils
-
 object RandomStringUtils {
     @JvmStatic
     fun randomAlphanumeric(count: Int): String {
-        return RandomStringUtils.randomAlphanumeric(count)
+        return randomString(
+            count = count,
+            letters = true,
+            numbers = true
+        )
     }
 
     @JvmStatic
     fun randomAlphabetic(count: Int): String {
-        return RandomStringUtils.randomAlphabetic(count)
+        return randomString(
+            count = count,
+            letters = true,
+            numbers = false
+        )
     }
 
     @JvmStatic
     fun randomNumeric(count: Int): String {
-        return RandomStringUtils.randomNumeric(count)
+        return randomString(
+            count = count,
+            letters = false,
+            numbers = true
+        )
+    }
+
+    private fun randomString(count: Int, letters: Boolean, numbers: Boolean): String {
+        val alphabetic: List<Char> = ('A'..'Z') + ('a'..'z')
+        val numeric = ('0'..'9').toList()
+        val allowedChars: List<Char> = when {
+            letters && numbers -> alphabetic + numeric
+            letters -> alphabetic
+            numbers -> numeric
+            else -> throw UnsupportedOperationException("Neither letters nor numbers")
+        }
+        return (1..count)
+            .map { allowedChars.random() }
+            .joinToString("")
     }
 }
