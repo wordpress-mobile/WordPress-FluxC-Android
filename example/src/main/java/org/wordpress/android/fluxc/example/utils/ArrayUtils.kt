5e95cd079e1ba1b84db25edd11cf3102d9f6b6cd
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2022-11-22 16:21:02 +0200

    Analysis: Remove unnecessary commons lang array utils contains
    The 'ArrayUtils.contains(...)' can be safely replaced with a Kotlin
    equivalent solution.
    
    FYI: The tests within the 'ArrayUtilsTest' test suite verify correctness
    of this change.
    
    PS: Part of the same change was applied on 'WPAndroid' (see PR/commit
    below). As such, and after these changes are confirmed by manual
    testing, it could be possible to move this 'ArrayUtils' into the
    'WordPress-Utils-Android' repo and made this as the universal source of
    truth, just like it already happens for other such utilities.
    
    WPAndroid PR: https://github.com/wordpress-mobile/WordPress-Android/
    pull/17501
    WPAndroid Commit: https://github.com/wordpress-mobile/WordPress-Android/
    pull/17501/commits/762fb23046fdde78b92bd90687a56bf5736d915b

--- example/src/main/java/org/wordpress/android/fluxc/example/utils/ArrayUtils.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/utils/ArrayUtils.kt
@@ -1,10 +1,15 @@
 package org.wordpress.android.fluxc.example.utils
 
-import org.apache.commons.lang3.ArrayUtils
-
 object ArrayUtils {
+    private const val INDEX_NOT_FOUND = -1
+
     @JvmStatic
     fun contains(stringArray: Array<String>, string: String): Boolean {
-        return ArrayUtils.contains(stringArray, string)
+        return indexOf(stringArray, string) != INDEX_NOT_FOUND
+    }
+
+    @JvmStatic
+    fun indexOf(stringArray: Array<String>, string: String): Int {
+        return stringArray.indexOf(string)
     }
 }
