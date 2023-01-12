5ac2738b3557681f0b756413ca29877eb55b30a4
Hafiz Rahman <hfzrhmn@gmail.com>
2023-01-12 16:46:07 +0700

    Refactor createPackages()

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/shippinglabels/ShippingLabelRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/shippinglabels/ShippingLabelRestClient.kt
@@ -19,12 +19,8 @@ import org.wordpress.android.fluxc.model.shippinglabels.WCShippingPackageCustoms
 import org.wordpress.android.fluxc.network.UserAgent
 import org.wordpress.android.fluxc.network.rest.wpcom.BaseWPComRestClient
 import org.wordpress.android.fluxc.network.rest.wpcom.auth.AccessToken
-import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder
-import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder.JetpackResponse.JetpackError
-import org.wordpress.android.fluxc.network.rest.wpcom.jetpacktunnel.JetpackTunnelGsonRequestBuilder.JetpackResponse.JetpackSuccess
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooNetwork
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
-import org.wordpress.android.fluxc.network.rest.wpcom.wc.toWooError
 import org.wordpress.android.fluxc.network.utils.toMap
 import org.wordpress.android.fluxc.utils.toWooPayload
 import java.math.BigDecimal
@@ -38,7 +34,6 @@ import kotlin.collections.toMap as asMap
 @Singleton
 class ShippingLabelRestClient @Inject constructor(
     dispatcher: Dispatcher,
-    private val jetpackTunnelGsonRequestBuilder: JetpackTunnelGsonRequestBuilder,
     appContext: Context?,
     @Named("regular") requestQueue: RequestQueue,
     accessToken: AccessToken,
@@ -299,22 +294,12 @@ class ShippingLabelRestClient @Inject constructor(
                 "predefined" to predefinedParam
         )
 
-        val response = jetpackTunnelGsonRequestBuilder.syncPostRequest(
-                this,
-                site,
-                url,
-                params,
-                JsonObject::class.java
-        )
-
-        return when (response) {
-            is JetpackSuccess -> {
-                WooPayload(response.data!!["success"].asBoolean)
-            }
-            is JetpackError -> {
-                WooPayload(response.error.toWooError())
-            }
-        }
+        return wooNetwork.executePostGsonRequest(
+            site = site,
+            path = url,
+            clazz = JsonObject::class.java,
+            body = params
+        ).toWooPayload { it["success"].asBoolean }
     }
 
     data class PrintShippingLabelApiResponse(
