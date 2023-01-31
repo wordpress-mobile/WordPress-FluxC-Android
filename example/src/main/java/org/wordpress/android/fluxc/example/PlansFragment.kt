1bca3dc13d9898c139e2cc78fbe5dc710c8f59e5
Ondrej Ruttkay <ruttkay@gmail.com>
2023-01-31 20:23:03 +0100

    Add a test UI functionality for fetching site plans

--- example/src/main/java/org/wordpress/android/fluxc/example/PlansFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/PlansFragment.kt
@@ -5,33 +5,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_plans.*
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.example.ui.StoreSelectingFragment
 import org.wordpress.android.fluxc.store.PlansStore
+import org.wordpress.android.fluxc.store.SiteStore
 import javax.inject.Inject
 
-class PlansFragment : Fragment() {
+class PlansFragment : StoreSelectingFragment() {
     @Inject internal lateinit var dispatcher: Dispatcher
-    @Inject internal lateinit var store: PlansStore
+    @Inject internal lateinit var plansStore: PlansStore
+    @Inject internal lateinit var siteStore: SiteStore
 
     override fun onAttach(context: Context) {
         AndroidSupportInjection.inject(this)
         super.onAttach(context)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_plans, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
+        inflater.inflate(R.layout.fragment_plans, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         fetch_plans.setOnClickListener {
             lifecycleScope.launch {
-                val plansResult = store.fetchPlans()
+                val plansResult = plansStore.fetchPlans()
                 when {
                     plansResult.isError -> {
                         prependToLog("Error fetching plans: ${plansResult.error}")
@@ -45,6 +51,28 @@ class PlansFragment : Fragment() {
                     }
                 }
             }
+        }
+
+        fetch_site_plan.setOnClickListener {
+            selectedSite?.let { site ->
+                lifecycleScope.launch {
+                    val plansResult = siteStore.fetchSitePlans(site)
+                    when {
+                        plansResult.isError -> {
+                            prependToLog("Error fetching site plan: ${plansResult.error.type}")
+                        }
+                        else -> {
+                            val plan = plansResult.plans?.firstOrNull { it.isCurrentPlan }
+                            if (plan != null) {
+                                prependToLog("Current site plan: ${plan.productName}" +
+                                    "; has domain credit: ${plan.hasDomainCredit}")
+                            } else {
+                               prependToLog("The site has no active plan")
+                            }
+                        }
+                    }
+                }
+            } ?: prependToLog("Select a site first")
         }
     }
 }
