e72e305b6fd0b0fe10b1056a3a9530693d2fde9b
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-02-20 15:39:27 +0100

    Add new onboarding tasks path correctly

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/onboarding/OnboardingRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/onboarding/OnboardingRestClient.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.onboarding
 
-import org.wordpress.android.fluxc.generated.endpoint.WPCOMREST
+import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooNetwork
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
@@ -11,7 +11,7 @@ class OnboardingRestClient @Inject constructor(
     private val wooNetwork: WooNetwork
 ) {
     suspend fun fetchOnboardingTasks(site: SiteModel): WooPayload<Array<TaskGroupDto>> {
-        val url = WPCOMREST.wc_admin.onboarding.tasks.endpoint
+        val url = WOOCOMMERCE.onboarding.tasks.pathWcAdmin
         return wooNetwork.executeGetGsonRequest(
             site,
             url,
