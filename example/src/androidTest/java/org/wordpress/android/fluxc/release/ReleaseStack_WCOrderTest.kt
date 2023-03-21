8b7640756b413cbbb67dc961b41f235c4e7296ef
pachlava <pachlava@gmail.com>
2023-03-21 13:53:29 +0200

    Ignored `testFetchShipmentTrackingsForOrder_pluginNotInstalled`

--- example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_WCOrderTest.kt
+++ example/src/androidTest/java/org/wordpress/android/fluxc/release/ReleaseStack_WCOrderTest.kt
@@ -255,6 +255,7 @@ class ReleaseStack_WCOrderTest : ReleaseStack_WCBase() {
      */
     @Throws(InterruptedException::class)
     @Test
+    @Ignore("WIP see https://github.com/wordpress-mobile/WordPress-FluxC-Android/pull/2673")
     fun testFetchShipmentTrackingsForOrder_pluginNotInstalled() = runBlocking {
         val result = orderStore.fetchOrderShipmentTrackings(orderModel.orderId, sSite)
         assertTrue(result.isError)
