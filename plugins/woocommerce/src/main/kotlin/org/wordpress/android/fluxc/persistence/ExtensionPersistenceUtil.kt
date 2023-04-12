70d7a97187544decfe785768aeab72656f4b105b
Alejo <alejandro.torres.veiga@automattic.com>
2023-04-11 22:25:55 -0600

    add bundle to core product types

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/ExtensionPersistenceUtil.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/ExtensionPersistenceUtil.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.fluxc.persistence
 
 import org.wordpress.android.fluxc.model.LocalOrRemoteId
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.CoreProductType
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.product.ProductApiResponse
 import org.wordpress.android.fluxc.persistence.dao.BundledProductsDao
 import org.wordpress.android.fluxc.persistence.mappers.BundledProductMapper
@@ -13,7 +14,7 @@ class ExtensionPersistenceUtil @Inject constructor(
 ) {
     suspend fun persistExtensionData(localSiteId: Int, productApiResponse: ProductApiResponse) {
         when (productApiResponse.type) {
-            "bundle" -> {
+            CoreProductType.BUNDLE.value -> {
                 val productId = productApiResponse.id
                 val bundledProducts = BundledProductMapper.toDatabaseEntityList(
                     localSiteId = localSiteId,
