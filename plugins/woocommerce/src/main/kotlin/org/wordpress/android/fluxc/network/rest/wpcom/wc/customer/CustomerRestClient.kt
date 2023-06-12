a7ec878415b72d47e505cdcf25c6151398e1e22d
kidinov <andrey.kidinov@a8c.com>
2023-06-12 14:21:33 +0400

    Add new methods to fetch and create customers, and sort search results in different ways, including by date_last_active, by implementing CustomerSorting enum and updating CustomerRestClient and WCCustomerStore implementation accordingly. Also add a method to fetch customers from analytics endpoint.

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/customer/CustomerRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/customer/CustomerRestClient.kt
@@ -4,6 +4,8 @@ import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooNetwork
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.customer.CustomerSorting.DATE_LAST_ACTIVE_ASC
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.customer.CustomerSorting.DATE_LAST_ACTIVE_DESC
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.customer.CustomerSorting.INCLUDE_ASC
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.customer.CustomerSorting.INCLUDE_DESC
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.customer.CustomerSorting.NAME_ASC
@@ -22,7 +24,10 @@ class CustomerRestClient @Inject constructor(private val wooNetwork: WooNetwork)
      *
      * @param [remoteCustomerId] Unique server id of the customer to fetch
      */
-    suspend fun fetchSingleCustomer(site: SiteModel, remoteCustomerId: Long): WooPayload<CustomerDTO> {
+    suspend fun fetchSingleCustomer(
+        site: SiteModel,
+        remoteCustomerId: Long
+    ): WooPayload<CustomerDTO> {
         val url = WOOCOMMERCE.customers.id(remoteCustomerId).pathV3
 
         val response = wooNetwork.executeGetGsonRequest(
@@ -56,10 +61,11 @@ class CustomerRestClient @Inject constructor(private val wooNetwork: WooNetwork)
             NAME_ASC, NAME_DESC -> "name"
             INCLUDE_ASC, INCLUDE_DESC -> "include"
             REGISTERED_DATE_ASC, REGISTERED_DATE_DESC -> "registered_date"
+            DATE_LAST_ACTIVE_ASC, DATE_LAST_ACTIVE_DESC -> "date_last_active"
         }
         val sortOrder = when (sortType) {
-            NAME_ASC, INCLUDE_ASC, REGISTERED_DATE_ASC -> "asc"
-            INCLUDE_DESC, NAME_DESC, REGISTERED_DATE_DESC -> "desc"
+            NAME_ASC, INCLUDE_ASC, REGISTERED_DATE_ASC, DATE_LAST_ACTIVE_ASC -> "asc"
+            INCLUDE_DESC, NAME_DESC, REGISTERED_DATE_DESC, DATE_LAST_ACTIVE_DESC -> "desc"
         }
 
         val params = mutableMapOf(
@@ -103,6 +109,51 @@ class CustomerRestClient @Inject constructor(private val wooNetwork: WooNetwork)
             path = url,
             body = customer.toMap(),
             clazz = CustomerDTO::class.java
+        )
+
+        return response.toWooPayload()
+    }
+
+    /**
+     * Makes a GET call to `wc-analytics/reports/customers` to fetch customers
+     *
+     */
+    suspend fun fetchCustomersFromAnalytics(
+        site: SiteModel,
+        pageSize: Int,
+        page: Int,
+        sortType: CustomerSorting = NAME_ASC,
+        searchQuery: String? = null,
+        searchBy: String? = null,
+    ): WooPayload<Array<CustomerDTO>> {
+        val url = WOOCOMMERCE.wc_analytics.reports.customers.pathV3
+
+        val orderBy = when (sortType) {
+            NAME_ASC, NAME_DESC -> "name"
+            INCLUDE_ASC, INCLUDE_DESC -> "include"
+            REGISTERED_DATE_ASC, REGISTERED_DATE_DESC -> "registered_date"
+            DATE_LAST_ACTIVE_ASC, DATE_LAST_ACTIVE_DESC -> "date_last_active"
+        }
+        val sortOrder = when (sortType) {
+            NAME_ASC, INCLUDE_ASC, REGISTERED_DATE_ASC, DATE_LAST_ACTIVE_ASC -> "asc"
+            INCLUDE_DESC, NAME_DESC, REGISTERED_DATE_DESC, DATE_LAST_ACTIVE_DESC -> "desc"
+        }
+
+        val params = mutableMapOf(
+            "per_page" to pageSize.toString(),
+            "orderby" to orderBy,
+            "order" to sortOrder,
+            "page" to page.toString(),
+        ).run {
+            putIfNotEmpty("search" to searchQuery)
+            putIfNotEmpty("searchBy" to searchBy)
+        }
+
+        val response = wooNetwork.executeGetGsonRequest(
+            site = site,
+            path = url,
+            params = params,
+            clazz = Array<CustomerDTO>::class.java
         )
 
         return response.toWooPayload()
