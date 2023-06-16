720d30a36cfcce826952a6c02b907a7a891aa00f
Alejo <alejandro.torres.veiga@automattic.com>
2023-06-16 14:34:54 -0600

    remove tests using log

--- plugins/woocommerce/src/test/java/org/wordpress/android/fluxc/model/WCProductModelTest.kt
+++ plugins/woocommerce/src/test/java/org/wordpress/android/fluxc/model/WCProductModelTest.kt
@@ -2,13 +2,8 @@ package org.wordpress.android.fluxc.model
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
-import com.google.gson.JsonPrimitive
 import org.assertj.core.api.Assertions
 import org.junit.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.doNothing
-import org.mockito.kotlin.mock
-import org.wordpress.android.fluxc.utils.AppLogWrapper
 
 class WCProductModelTest {
     @Test
@@ -43,38 +38,5 @@ class WCProductModelTest {
         val result = sut.getAttributeList()
 
         Assertions.assertThat(result).isNotEmpty
-    }
-
-    @Test
-    fun `json attributes as a primitive doesn't crash`() {
-        val appLogWrapper: AppLogWrapper = mock()
-        doNothing().`when`(appLogWrapper).e(any(), any())
-
-        val attribute = JsonPrimitive(true)
-
-        val attributes = JsonArray().apply { add(attribute) }
-
-        val sut = WCProductModel(appLogWrapper = appLogWrapper).apply {
-            this.attributes = attributes.toString()
-        }
-
-        val result = sut.getAttributeList()
-
-        // If we can't process product's attributes then we return an empty list
-        Assertions.assertThat(result).isEmpty()
-    }
-
-    @Test
-    fun `json attributes empty doesn't crash`() {
-        val appLogWrapper: AppLogWrapper = mock()
-        doNothing().`when`(appLogWrapper).e(any(), any())
-
-        val sut = WCProductModel(appLogWrapper = appLogWrapper).apply {
-            this.attributes = ""
-        }
-        val result = sut.getAttributeList()
-
-        // If we can't process product's attributes then we return an empty list
-        Assertions.assertThat(result).isEmpty()
     }
 }
