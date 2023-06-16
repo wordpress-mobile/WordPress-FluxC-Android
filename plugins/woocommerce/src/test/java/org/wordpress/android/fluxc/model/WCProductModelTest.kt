227973115ec946df215723368975caad63c347ef
Alejo <alejandro.torres.veiga@automattic.com>
2023-06-16 07:41:39 -0600

    failure parsing attributes unit tests

--- plugins/woocommerce/src/test/java/org/wordpress/android/fluxc/model/WCProductModelTest.kt
+++ plugins/woocommerce/src/test/java/org/wordpress/android/fluxc/model/WCProductModelTest.kt
@@ -2,8 +2,13 @@ package org.wordpress.android.fluxc.model
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import org.assertj.core.api.Assertions
 import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doNothing
+import org.mockito.kotlin.mock
+import org.wordpress.android.fluxc.utils.AppLogWrapper
 
 class WCProductModelTest {
     @Test
@@ -40,30 +45,36 @@ class WCProductModelTest {
         Assertions.assertThat(result).isNotEmpty
     }
 
-/*    @Test
+    @Test
     fun `json attributes as a primitive doesn't crash`() {
+        val appLogWrapper: AppLogWrapper = mock()
+        doNothing().`when`(appLogWrapper).e(any(), any())
+
         val attribute = JsonPrimitive(true)
 
-        val attributes = JsonArray().apply {
-            add(attribute)
-        }
+        val attributes = JsonArray().apply { add(attribute) }
 
-        val sut = WCProductModel().apply {
+        val sut = WCProductModel(appLogWrapper = appLogWrapper).apply {
             this.attributes = attributes.toString()
         }
 
         val result = sut.getAttributeList()
 
-        Assertions.assertThat(result).isNotEmpty
+        // If we can't process product's attributes then we return an empty list
+        Assertions.assertThat(result).isEmpty()
     }
 
     @Test
     fun `json attributes empty doesn't crash`() {
-        val sut = WCProductModel().apply {
+        val appLogWrapper: AppLogWrapper = mock()
+        doNothing().`when`(appLogWrapper).e(any(), any())
+
+        val sut = WCProductModel(appLogWrapper = appLogWrapper).apply {
             this.attributes = ""
         }
         val result = sut.getAttributeList()
 
-        Assertions.assertThat(result).isNotEmpty
-    }*/
+        // If we can't process product's attributes then we return an empty list
+        Assertions.assertThat(result).isEmpty()
+    }
 }
