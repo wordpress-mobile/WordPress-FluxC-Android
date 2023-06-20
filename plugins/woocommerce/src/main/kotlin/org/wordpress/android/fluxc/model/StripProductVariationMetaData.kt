4e45c0b80152cc4f45cf9dc066322718013d1dbc
Alejo <alejandro.torres.veiga@automattic.com>
2023-06-20 10:55:10 -0600

    refactor StripProductVariationMetaData

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/StripProductVariationMetaData.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/StripProductVariationMetaData.kt
@@ -3,18 +3,19 @@ package org.wordpress.android.fluxc.model
 import com.google.gson.Gson
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import org.wordpress.android.fluxc.utils.isElementNullOrEmpty
 import javax.inject.Inject
 
 class StripProductVariationMetaData @Inject internal constructor(private val gson: Gson) {
     operator fun invoke(metadata: String?): String? {
-        if (metadata == null) return null
+        if (metadata.isNullOrEmpty()) return null
 
         return gson.fromJson(metadata, JsonArray::class.java)
             .mapNotNull { it as? JsonObject }
             .asSequence()
             .filter { jsonObject ->
-                jsonObject[WCMetaData.KEY]?.asString.orEmpty() in SUPPORTED_KEYS &&
-                    jsonObject[WCMetaData.VALUE]?.asString.orEmpty().isNotBlank()
+                val isNullOrEmpty = jsonObject[WCMetaData.VALUE].isElementNullOrEmpty()
+                jsonObject[WCMetaData.KEY]?.asString.orEmpty() in SUPPORTED_KEYS && isNullOrEmpty.not()
             }.toList()
             .takeIf { it.isNotEmpty() }
             ?.let { gson.toJson(it) }
