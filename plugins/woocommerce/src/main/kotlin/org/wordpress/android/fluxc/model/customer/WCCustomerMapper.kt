116188c51261b30af02a910922deb97e1e9f579f
kidinov <andrey.kidinov@a8c.com>
2023-07-06 15:10:04 +0200

    Fix assertion value for remoteCustomerId in WCCustomerMapperTest and update property assignment in WCCustomerMapper.

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/customer/WCCustomerMapper.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/customer/WCCustomerMapper.kt
@@ -88,7 +88,7 @@ class WCCustomerMapper @Inject constructor() {
     fun mapToModel(site: SiteModel, dto: CustomerFromAnalyticsDTO): WCCustomerModel {
         return WCCustomerModel().apply {
             localSiteId = site.id
-            remoteCustomerId = dto.userId ?: 0
+            remoteCustomerId = dto.id ?: 0
             email = dto.email ?: ""
             firstName = dto.name.firstNameFromName()
             lastName = dto.name.lastNameFromName()
