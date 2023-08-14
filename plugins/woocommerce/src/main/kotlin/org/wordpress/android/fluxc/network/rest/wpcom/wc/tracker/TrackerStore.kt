bb118e01ac818c4e39c93a8cf79a350f0e0c9889
jorgemucientesfayos <jorge.mucientes@automattic.com>
2023-08-14 15:46:17 +0200

    Remove ISO8601 date formatting

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/tracker/TrackerStore.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/tracker/TrackerStore.kt
@@ -2,38 +2,16 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.tracker
 
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
-import org.wordpress.android.fluxc.utils.AppLogWrapper
-import org.wordpress.android.util.AppLog.T.STATS
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class TrackerStore @Inject internal constructor(
-    private val restClient: TrackerRestClient,
-    private val appLogWrapper: AppLogWrapper
-) {
-    private companion object {
-        val ISO8601_FORMAT = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ROOT)
-            .apply {
-                timeZone = TimeZone.getTimeZone("UTC")
-            }
-    }
-
+class TrackerStore @Inject internal constructor(private val restClient: TrackerRestClient) {
     suspend fun sendTelemetry(
         appVersion: String,
         site: SiteModel,
-        installationDate: Date? = null
+        installationDateIso8601: String? = null
     ): WooPayload<Unit> {
-        val formattedInstallationDate = installationDate?.let { ISO8601_FORMAT.format(it) }
-        appLogWrapper.d(
-            STATS,
-            "Sending Telemetry. Values: appVersion=$appVersion, site=${site.siteId}, " +
-                    "installationDate=$formattedInstallationDate"
-        )
-        return restClient.sendTelemetry(appVersion, site, formattedInstallationDate)
+        return restClient.sendTelemetry(appVersion, site, installationDateIso8601)
     }
 }
