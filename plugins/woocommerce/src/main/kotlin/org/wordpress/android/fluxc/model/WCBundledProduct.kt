206efbd19eae739313a660d0e07ab079085ce30d
Alejo <alejandro.torres.veiga@automattic.com>
2023-08-22 17:47:33 -0600

    include bundle rules

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCBundledProduct.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/WCBundledProduct.kt
@@ -2,10 +2,15 @@ package org.wordpress.android.fluxc.model
 
 import com.google.gson.annotations.SerializedName
 
+@Suppress("LongParameterList")
 class WCBundledProduct(
     @SerializedName("bundled_item_id") val id: Long,
     @SerializedName("product_id") val bundledProductId: Long,
     @SerializedName("menu_order") val menuOrder: Int,
     @SerializedName("title") val title: String,
-    @SerializedName("stock_status") val stockStatus: String
+    @SerializedName("stock_status") val stockStatus: String,
+    @SerializedName("quantity_min") val quantityMin: Long,
+    @SerializedName("quantity_max") val quantityMax: Long,
+    @SerializedName("quantity_default") val quantityDefault: Long = 0,
+    @SerializedName("optional") val isOptional: Boolean = false,
 )
