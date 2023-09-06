b8ad2310333ece0105dcb8f52a3644cf88d2ffe1
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-09-06 10:02:45 -0400

    Add paginated results

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/taxes/WCTaxRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/taxes/WCTaxRestClient.kt
@@ -2,8 +2,10 @@ package org.wordpress.android.fluxc.network.rest.wpcom.wc.taxes
 
 import org.wordpress.android.fluxc.generated.endpoint.WOOCOMMERCE
 import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.rest.wpapi.WPAPIResponse
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooNetwork
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.toWooError
 import org.wordpress.android.fluxc.utils.toWooPayload
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -23,15 +25,34 @@ class WCTaxRestClient @Inject constructor(private val wooNetwork: WooNetwork) {
         return response.toWooPayload()
     }
 
-    suspend fun fetchTaxRateList(site: SiteModel): WooPayload<Array<TaxRateApiResponse>> {
+    suspend fun fetchTaxRateList(
+        site: SiteModel,
+        page: Int,
+        pageSize: Int,
+        searchQuery: String? = null
+    ): WooPayload<Array<TaxRateApiResponse>> {
         val url = WOOCOMMERCE.taxes.pathV3
 
         val response = wooNetwork.executeGetGsonRequest(
             site,
             url,
-            Array<TaxRateApiResponse>::class.java
+            Array<TaxRateApiResponse>::class.java,
+            mutableMapOf<String, String>().apply {
+                put("page", page.toString())
+                put("per_page", pageSize.toString())
+                searchQuery?.let {
+                    put("search", searchQuery)
+                }
+            }
         )
-        return response.toWooPayload()
+        return when (response) {
+            is WPAPIResponse.Success -> {
+                WooPayload(response.data)
+            }
+            is WPAPIResponse.Error -> {
+                WooPayload(response.error.toWooError())
+            }
+        }
     }
 
     data class TaxClassApiResponse(
