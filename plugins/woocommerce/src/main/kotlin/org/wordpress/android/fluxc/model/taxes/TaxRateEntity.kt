f48c3ee3b3153ebda49b774fb7b45382703aa978
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-09-11 16:56:55 -0400

    Add TaxRateEntity

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/taxes/TaxRateEntity.kt
@@ -0,0 +1,15 @@
+package org.wordpress.android.fluxc.model.taxes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
+
+@Entity(
+    tableName = "TaxRate",
+    primaryKeys = ["id", "localSiteId"],
+    )
+data class TaxRateEntity (
+    val id: RemoteId,
+    val localSiteId: LocalId,
+)
