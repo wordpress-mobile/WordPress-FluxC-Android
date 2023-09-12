d66e79c6a692d3d96197dd0ca644813c41248531
Rossana <30724184+rossanafmenezes@users.noreply.github.com>
2023-09-12 13:22:01 -0400

    Add get single tax method to dao

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/TaxRateDao.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/TaxRateDao.kt
@@ -4,16 +4,23 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.LocalId
+import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
 import org.wordpress.android.fluxc.model.taxes.TaxRateEntity
 
 @Dao
 interface TaxRateDao {
     @Query("SELECT * FROM TaxRate WHERE localSiteId = :localSiteId")
-    suspend fun getTaxRates(localSiteId: Long): List<TaxRateEntity>
+    suspend fun getTaxRates(localSiteId: LocalId): List<TaxRateEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrUpdate(taxRate: TaxRateEntity): Long
 
     @Query("DELETE FROM TaxRate WHERE localSiteId = :localSiteId")
-    suspend fun deleteAll(localSiteId: Long)
+    suspend fun deleteAll(localSiteId: LocalId)
+
+    @Transaction
+    @Query("SELECT * FROM TaxRate WHERE localSiteId = :localSiteId AND id IN (:taxRateId)")
+    suspend fun getTaxRate(localSiteId: LocalId, taxRateId: RemoteId): TaxRateEntity?
 }
