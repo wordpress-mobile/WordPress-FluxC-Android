1ed8b71dcf7076973543d2eb380388621dd57450
ThomazFB <thomazfcortez@gmail.com>
2023-10-17 13:20:38 -0300

    Add endpoint values to PasskeyRestClient

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/passkey/PasskeyRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/passkey/PasskeyRestClient.kt
@@ -1,4 +1,9 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.passkey
 
 class PasskeyRestClient {
+    companion object {
+        const val baseURLWithAction = "wp-login.php?action"
+        const val challengeEndpoint = "webauthn-challenge-endpoint"
+        const val authEndpoint = "webauthn-authentication-endpoint"
+    }
 }
