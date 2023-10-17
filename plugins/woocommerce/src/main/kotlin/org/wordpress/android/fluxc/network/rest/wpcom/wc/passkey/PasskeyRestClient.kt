71cf4aff7df3d914eef2cbbf327b26c4715598cf
ThomazFB <thomazfcortez@gmail.com>
2023-10-17 19:20:04 -0300

    Define basic challenge request method

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/passkey/PasskeyRestClient.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/passkey/PasskeyRestClient.kt
@@ -1,6 +1,29 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.passkey
 
+import org.wordpress.android.fluxc.model.SiteModel
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooPayload
+
 class PasskeyRestClient {
+    fun requestWebauthnChallenge(
+        userId: Long,
+        twoStepNonce: String
+    ): WooPayload<WebauthnChallengeInfo> {
+        val parameters = mapOf(
+            "user_id" to userId.toString(),
+            "two_step_nonce" to twoStepNonce,
+            "auth_type" to "webauthn"
+        )
+
+        return WooPayload(
+            WebauthnChallengeInfo(
+                challenge = "challenge",
+                rpId = "rpId",
+                twoStepNonce = "twoStepNonce",
+                allowedCredentials = listOf("allowedCredentials")
+            )
+        )
+    }
+
     companion object {
         const val baseURLWithAction = "wp-login.php?action"
         const val challengeEndpoint = "webauthn-challenge-endpoint"
