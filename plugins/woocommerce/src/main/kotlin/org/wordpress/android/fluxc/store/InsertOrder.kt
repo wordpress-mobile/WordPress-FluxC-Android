7dfe489c93f4f4cde47cce95e9181068046bec9a
samiuelson <samiuelson@gmail.com>
2024-02-22 11:39:28 +0100

    Revert "Minor code optimization"
    This reverts commit 0b3be16ffc2adc67230ecf780e2c304fb95cc66b.

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/InsertOrder.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/store/InsertOrder.kt
@@ -1,25 +1,29 @@
 package org.wordpress.android.fluxc.store
 
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.ListActionBuilder
 import org.wordpress.android.fluxc.model.LocalOrRemoteId
 import org.wordpress.android.fluxc.model.OrderEntity
+import org.wordpress.android.fluxc.model.WCOrderListDescriptor
 import org.wordpress.android.fluxc.persistence.TransactionExecutor
-import org.wordpress.android.fluxc.persistence.dao.OrdersDaoDecorator
 import org.wordpress.android.fluxc.persistence.dao.OrderMetaDataDao
+import org.wordpress.android.fluxc.persistence.dao.OrdersDaoDecorator
 import org.wordpress.android.fluxc.persistence.entity.OrderMetaDataEntity
 import javax.inject.Inject
 
 class InsertOrder @Inject internal constructor(
+    private val dispatcher: Dispatcher,
     private val ordersDaoDecorator: OrdersDaoDecorator,
     private val ordersMetaDataDao: OrderMetaDataDao,
     private val transactionExecutor: TransactionExecutor
 ) {
     suspend operator fun invoke(
+        localSiteId: LocalOrRemoteId.LocalId,
         vararg ordersPack: Pair<OrderEntity, List<OrderMetaDataEntity>>
     ) {
-        val listSize = ordersPack.size
         transactionExecutor.executeInTransaction {
-            ordersPack.forEachIndexed { index, (order, metaData) ->
-                ordersDaoDecorator.insertOrUpdateOrder(order, suppressListRefresh = index != listSize - 1 )
+            ordersPack.forEach { (order, metaData) ->
+                ordersDaoDecorator.insertOrUpdateOrder(order, suppressListRefresh = true)
                 ordersMetaDataDao.updateOrderMetaData(
                     order.orderId,
                     order.localSiteId,
@@ -27,5 +31,9 @@ class InsertOrder @Inject internal constructor(
                 )
             }
         }
+        val listTypeIdentifier = WCOrderListDescriptor.calculateTypeIdentifier(
+            localSiteId = localSiteId.value
+        )
+        dispatcher.dispatch(ListActionBuilder.newListDataInvalidatedAction(listTypeIdentifier))
     }
 }
