b0f748ef14fede236795e85f8b0f0c0129b21cba
malinajirka <malinajirka@gmail.com>
2024-02-22 06:55:00 +0100

    Introduce decorator for OrdersDao - auto-updates ListStore component

---
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/OrdersDaoDecorator.kt
@@ -0,0 +1,79 @@
+package org.wordpress.android.fluxc.persistence.dao
+
+import kotlinx.coroutines.flow.Flow
+import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.generated.ListActionBuilder
+import org.wordpress.android.fluxc.model.LocalOrRemoteId
+import org.wordpress.android.fluxc.model.OrderEntity
+import org.wordpress.android.fluxc.model.WCOrderListDescriptor
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.CoreOrderStatus
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class OrdersDaoDecorator @Inject constructor(
+    private val ordersDao: OrdersDao,
+) {
+    suspend fun updateLocalOrder(
+        orderId: Long,
+        localSiteId: LocalOrRemoteId.LocalId,
+        updateOrder: OrderEntity.() -> OrderEntity
+    ) {
+        getOrder(orderId, localSiteId)
+            ?.let(updateOrder)
+            ?.let { insertOrUpdateOrder(it) }
+    }
+
+    @Suppress("unused")
+    suspend fun getAllOrders(): List<OrderEntity> = ordersDao.getAllOrders()
+
+    fun insertOrUpdateOrder(order: OrderEntity) {
+        ordersDao.insertOrUpdateOrder(order)
+    }
+
+    suspend fun getOrder(orderId: Long, localSiteId: LocalOrRemoteId.LocalId): OrderEntity? =
+        ordersDao.getOrder(orderId, localSiteId)
+
+    @Suppress("unused")
+    fun observeOrder(orderId: Long, localSiteId: LocalOrRemoteId.LocalId): Flow<OrderEntity?> =
+        ordersDao.observeOrder(orderId, localSiteId)
+
+    suspend fun getPaidOrdersForSiteDesc(
+        localSiteId: LocalOrRemoteId.LocalId,
+        status: List<String> = listOf(CoreOrderStatus.COMPLETED.value)
+    ): List<OrderEntity> = ordersDao.getPaidOrdersForSiteDesc(localSiteId, status)
+
+    suspend fun getOrdersForSite(
+        localSiteId: LocalOrRemoteId.LocalId, status: List<String>
+    ): List<OrderEntity> = ordersDao.getOrdersForSite(localSiteId, status)
+
+    suspend fun getOrdersForSite(localSiteId: LocalOrRemoteId.LocalId): List<OrderEntity> =
+        ordersDao.getOrdersForSite(localSiteId)
+
+    fun observeOrdersForSite(localSiteId: LocalOrRemoteId.LocalId): Flow<List<OrderEntity>> =
+        ordersDao.observeOrdersForSite(localSiteId)
+
+    fun observeOrdersForSite(
+        localSiteId: LocalOrRemoteId.LocalId, status: List<String>
+    ): Flow<List<OrderEntity>> = ordersDao.observeOrdersForSite(localSiteId, status)
+
+    fun getOrdersForSiteByRemoteIds(
+        localSiteId: LocalOrRemoteId.LocalId, orderIds: List<Long>
+    ): List<OrderEntity> = ordersDao.getOrdersForSiteByRemoteIds(localSiteId, orderIds)
+
+    fun deleteOrdersForSite(localSiteId: LocalOrRemoteId.LocalId) {
+        ordersDao.deleteOrdersForSite(localSiteId)
+    }
+
+    fun getOrderCountForSite(localSiteId: LocalOrRemoteId.LocalId): Int =
+        ordersDao.getOrderCountForSite(localSiteId)
+
+    fun observeOrderCountForSite(localSiteId: LocalOrRemoteId.LocalId, status: List<String>): Flow<Int> =
+        ordersDao.observeOrderCountForSite(localSiteId, status)
+
+    suspend fun deleteOrder(localSiteId: LocalOrRemoteId.LocalId, orderId: Long) {
+        ordersDao.deleteOrder(localSiteId, orderId)
+    }
+}
+
+
