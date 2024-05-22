f8b5c72005a1603975f30338aeae7eb8689b1b42
Annmarie Ziegler <annmarie.ziegler@gmail.com>
2024-05-22 14:39:20 -0400

    Remove unused import

--- example/src/main/java/org/wordpress/android/fluxc/example/JetpackAIFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/JetpackAIFragment.kt
@@ -2,7 +2,6 @@ package org.wordpress.android.fluxc.example
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
