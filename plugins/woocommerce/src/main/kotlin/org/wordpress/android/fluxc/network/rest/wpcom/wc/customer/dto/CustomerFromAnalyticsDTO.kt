45b5609979ec3e09d4bb4d3a3b84095d9fa8315c
Alejo <alejandro.torres.veiga@automattic.com>
2024-06-24 21:09:06 -0600

    map dto to model

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/customer/dto/CustomerFromAnalyticsDTO.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/network/rest/wpcom/wc/customer/dto/CustomerFromAnalyticsDTO.kt
@@ -1,6 +1,7 @@
 package org.wordpress.android.fluxc.network.rest.wpcom.wc.customer.dto
 
 import com.google.gson.annotations.SerializedName
+import org.wordpress.android.fluxc.model.customer.WCCustomerFromAnalytics
 
 data class CustomerFromAnalyticsDTO(
     @SerializedName("avg_order_value")
@@ -38,3 +39,25 @@ data class CustomerFromAnalyticsDTO(
     @SerializedName("username")
     val username: String? = null
 )
+
+fun CustomerFromAnalyticsDTO.toAppModel(): WCCustomerFromAnalytics {
+    return WCCustomerFromAnalytics(
+        avgOrderValue = this.avgOrderValue ?: 0.0,
+        city = this.city.orEmpty(),
+        country = this.country.orEmpty(),
+        dateLastActive = this.dateLastActive.orEmpty(),
+        dateLastActiveGmt = this.dateLastActiveGmt.orEmpty(),
+        dateLastOrder = this.dateLastOrder.orEmpty(),
+        dateRegistered = this.dateRegistered.orEmpty(),
+        dateRegisteredGmt = this.dateRegisteredGmt.orEmpty(),
+        email = this.email.orEmpty(),
+        id = this.id ?: 0L,
+        name = this.name.orEmpty(),
+        ordersCount = this.ordersCount ?: 0,
+        postcode = this.postcode.orEmpty(),
+        state = this.state.orEmpty(),
+        totalSpend = this.totalSpend ?: 0.0,
+        userId = this.userId ?: 0L,
+        username = this.username.orEmpty()
+    )
+}
