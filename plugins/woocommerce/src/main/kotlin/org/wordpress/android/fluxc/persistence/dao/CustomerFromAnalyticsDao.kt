ddb6f3355be9dc545269c4f8774a39d1de3ce863
Alejo <alejandro.torres.veiga@automattic.com>
2024-06-26 10:09:37 -0600

    refactor to interface

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/CustomerFromAnalyticsDao.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/persistence/dao/CustomerFromAnalyticsDao.kt
@@ -8,15 +8,15 @@ import org.wordpress.android.fluxc.model.LocalOrRemoteId
 import org.wordpress.android.fluxc.persistence.entity.CustomerFromAnalyticsEntity
 
 @Dao
-abstract class CustomerFromAnalyticsDao {
+interface CustomerFromAnalyticsDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertCustomersFromAnalytics(customers: List<CustomerFromAnalyticsEntity>)
+    suspend fun insertCustomersFromAnalytics(customers: List<CustomerFromAnalyticsEntity>)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    abstract suspend fun insertCustomerFromAnalytics(customers: CustomerFromAnalyticsEntity)
+    suspend fun insertCustomerFromAnalytics(customers: CustomerFromAnalyticsEntity)
 
     @Query("SELECT * FROM CustomerFromAnalytics WHERE localSiteId = :localSiteId AND id = :analyticCustomerId")
-    abstract suspend fun getCustomerByAnalyticCustomerId(
+    suspend fun getCustomerByAnalyticCustomerId(
         localSiteId: LocalOrRemoteId.LocalId,
         analyticCustomerId: Long
     ): CustomerFromAnalyticsEntity?
