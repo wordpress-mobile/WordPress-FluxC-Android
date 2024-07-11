9ab3e620e43aca3365bd144c7336f75f882c4400
ThomazFB <thomazfcortez@gmail.com>
2024-07-10 21:24:58 -0300

    Introduce reusable Status fromString function

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/google/WCGoogleAdsCampaignMapper.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/google/WCGoogleAdsCampaignMapper.kt
@@ -1,5 +1,6 @@
 package org.wordpress.android.fluxc.model.google
 
+import org.wordpress.android.fluxc.model.google.WCGoogleAdsCampaign.Status
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.google.WCGoogleAdsCampaignDTO
 import org.wordpress.android.util.AppLog
 import javax.inject.Inject
@@ -9,18 +10,7 @@ class WCGoogleAdsCampaignMapper @Inject constructor() {
         return WCGoogleAdsCampaign(
             id = dto.id,
             name = dto.name,
-            status = dto.status?.let {
-                try {
-                    WCGoogleAdsCampaign.Status.valueOf(it.uppercase())
-                } catch (e: IllegalArgumentException) {
-                    AppLog.w(
-                        AppLog.T.API,
-                        "Unknown campaign status returned: `$it`, defaulting to DISABLED " +
-                        e.message
-                    )
-                    WCGoogleAdsCampaign.Status.DISABLED
-                }
-            },
+            status = dto.status?.let { Status.fromString(it) },
             type = dto.rawType,
             amount = dto.amount,
             countryISOCode = dto.country,
