da0ea089f6a5e6c152edc8e7f18a14569b144ef0
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-07-17 16:04:07 +0300

    Refactor: Replace domains fragment synthetics with binding

--- example/src/main/java/org/wordpress/android/fluxc/example/DomainsFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/DomainsFragment.kt
@@ -7,9 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import dagger.android.support.AndroidSupportInjection
-import kotlinx.android.synthetic.main.fragment_domains.*
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.Dispatcher
+import org.wordpress.android.fluxc.example.databinding.FragmentDomainsBinding
 import org.wordpress.android.fluxc.example.ui.StoreSelectingFragment
 import org.wordpress.android.fluxc.example.utils.showSingleLineDialog
 import org.wordpress.android.fluxc.network.rest.wpapi.WPAPIResponse.Error
@@ -26,64 +26,68 @@ class DomainsFragment : StoreSelectingFragment() {
         super.onAttach(context)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_domains, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentDomainsBinding.inflate(inflater, container, false).root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        fetch_domains.setOnClickListener {
-            selectedSite?.let { site ->
-                lifecycleScope.launch {
-                    val result = store.fetchSiteDomains(site)
-                    when {
-                        result.isError -> {
-                            prependToLog("Error fetching plans: ${result.error.type}")
-                        }
-                        else -> {
-                            val plans = result.domains
-                                ?.joinToString(separator = "\n") {
-                                    "${it.domain} (Renewal: ${it.autoRenewalDate}), primary: ${it.primaryDomain}"
-                                }
-                            prependToLog("Domains:\n$plans")
+        with(FragmentDomainsBinding.bind(view)) {
+            fetchDomains.setOnClickListener {
+                selectedSite?.let { site ->
+                    lifecycleScope.launch {
+                        val result = store.fetchSiteDomains(site)
+                        when {
+                            result.isError -> {
+                                prependToLog("Error fetching plans: ${result.error.type}")
+                            }
+                            else -> {
+                                val plans = result.domains
+                                    ?.joinToString(separator = "\n") {
+                                        "${it.domain} (Renewal: ${it.autoRenewalDate}), primary: ${it.primaryDomain}"
+                                    }
+                                prependToLog("Domains:\n$plans")
+                            }
                         }
                     }
-                }
-            } ?: prependToLog("Select a site first")
-        }
+                } ?: prependToLog("Select a site first")
+            }
 
-        fetch_domain_price.setOnClickListener {
-            showSingleLineDialog(activity, "Enter a domain name:") { editText ->
-                lifecycleScope.launch {
-                    val domain = editText.text.toString()
-                    when (val result = store.fetchDomainPrice(editText.text.toString())) {
-                        is Error -> {
-                            prependToLog("Error fetching domain price: ${result.error.type}")
-                        }
-                        is Success -> {
-                            prependToLog("$domain price: ${result.data?.cost}")
+            fetchDomainPrice.setOnClickListener {
+                showSingleLineDialog(activity, "Enter a domain name:") { editText ->
+                    lifecycleScope.launch {
+                        val domain = editText.text.toString()
+                        when (val result = store.fetchDomainPrice(editText.text.toString())) {
+                            is Error -> {
+                                prependToLog("Error fetching domain price: ${result.error.type}")
+                            }
+                            is Success -> {
+                                prependToLog("$domain price: ${result.data?.cost}")
+                            }
                         }
                     }
                 }
             }
-        }
 
-        fetch_all_domains.setOnClickListener {
-            lifecycleScope.launch {
-                // fetching wpcom too for debugging purposes
-                val result = store.fetchAllDomains(noWpCom = false, resolveStatus = true)
-                when {
-                    result.isError -> {
-                        prependToLog("Error fetching all domains: ${result.error.message}")
-                    }
-                    else -> {
-                        prependToLog("All domains count: ${result.domains?.size}")
-                        val domains = result.domains
-                            ?.joinToString(separator = "\n") {
-                                "${it.domain} (type: ${it.type}, expiry: ${it.expiry}, " +
-                                    "status: ${it.domainStatus?.statusType.toString()})"
-                            }
-                        prependToLog("Domains:\n$domains")
+            fetchAllDomains.setOnClickListener {
+                lifecycleScope.launch {
+                    // fetching wpcom too for debugging purposes
+                    val result = store.fetchAllDomains(noWpCom = false, resolveStatus = true)
+                    when {
+                        result.isError -> {
+                            prependToLog("Error fetching all domains: ${result.error.message}")
+                        }
+                        else -> {
+                            prependToLog("All domains count: ${result.domains?.size}")
+                            val domains = result.domains
+                                ?.joinToString(separator = "\n") {
+                                    "${it.domain} (type: ${it.type}, expiry: ${it.expiry}, " +
+                                        "status: ${it.domainStatus?.statusType.toString()})"
+                                }
+                            prependToLog("Domains:\n$domains")
+                        }
                     }
                 }
             }
