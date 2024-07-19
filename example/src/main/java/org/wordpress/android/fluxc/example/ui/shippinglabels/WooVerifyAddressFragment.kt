acdc66282fa73b548a3dc822f184464d103fa3a4
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-07-19 16:12:24 +0300

    Refactor: Replace woo verify address fragment synthetics with binding
    FYI: This refactor also fixes a bug where a view id from
    'fragment_woo_shippinglabels' was used instead of using the
    correct with, with the same view id, from the actual targeted
    'fragment_woo_verify_address' fragment.

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/shippinglabels/WooVerifyAddressFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/shippinglabels/WooVerifyAddressFragment.kt
@@ -7,13 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.android.support.AndroidSupportInjection
-import kotlinx.android.synthetic.main.fragment_woo_shippinglabels.verify_address
-import kotlinx.android.synthetic.main.fragment_woo_verify_address.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.example.R
+import org.wordpress.android.fluxc.example.databinding.FragmentWooVerifyAddressBinding
 import org.wordpress.android.fluxc.example.prependToLog
 import org.wordpress.android.fluxc.model.SiteModel
 import org.wordpress.android.fluxc.model.shippinglabels.WCAddressVerificationResult.InvalidAddress
@@ -48,46 +46,50 @@ class WooVerifyAddressFragment : Fragment() {
         super.onAttach(context)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_woo_verify_address, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentWooVerifyAddressBinding.inflate(inflater, container, false).root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        verify_address.setOnClickListener {
-            if (address_country.getText().isBlank()) {
-                prependToLog("Validation error: Country is required (2-letter acronym) ")
-            } else {
-                coroutineScope.launch {
-                    val result = wcShippingLabelStore.verifyAddress(
+        with(FragmentWooVerifyAddressBinding.bind(view)) {
+            verifyAddress.setOnClickListener {
+                if (addressCountry.getText().isBlank()) {
+                    prependToLog("Validation error: Country is required (2-letter acronym) ")
+                } else {
+                    coroutineScope.launch {
+                        val result = wcShippingLabelStore.verifyAddress(
                             selectedSite,
                             ShippingLabelAddress(
-                                    address_company.getText(),
-                                    address_name.getText(),
-                                    address_phone.getText(),
-                                    address_country.getText(),
-                                    address_state.getText(),
-                                    address_address1.getText(),
-                                    address_address2.getText(),
-                                    address_city.getText(),
-                                    address_zip.getText()
+                                addressCompany.getText(),
+                                addressName.getText(),
+                                addressPhone.getText(),
+                                addressCountry.getText(),
+                                addressState.getText(),
+                                addressAddress1.getText(),
+                                addressAddress2.getText(),
+                                addressCity.getText(),
+                                addressZip.getText()
                             ),
                             if (destination.isChecked) DESTINATION else ORIGIN
-                    )
-                    when {
-                        result.isError -> {
-                            prependToLog("${result.error.message}")
-                        }
-                        result.model is Valid -> {
-                            val model = result.model as Valid
-                            prependToLog("Suggested address: ${model.suggestedAddress}\n" +
+                        )
+                        when {
+                            result.isError -> {
+                                prependToLog("${result.error.message}")
+                            }
+                            result.model is Valid -> {
+                                val model = result.model as Valid
+                                prependToLog("Suggested address: ${model.suggestedAddress}\n" +
                                     "Trivial Change: ${model.isTrivialNormalization}")
-                        }
-                        result.model is InvalidAddress -> {
-                            prependToLog("Address error: ${(result.model as InvalidAddress).message}")
-                        }
-                        result.model is InvalidRequest -> {
-                            prependToLog("Request error: ${(result.model as InvalidRequest).message}")
+                            }
+                            result.model is InvalidAddress -> {
+                                prependToLog("Address error: ${(result.model as InvalidAddress).message}")
+                            }
+                            result.model is InvalidRequest -> {
+                                prependToLog("Request error: ${(result.model as InvalidRequest).message}")
+                            }
                         }
                     }
                 }
