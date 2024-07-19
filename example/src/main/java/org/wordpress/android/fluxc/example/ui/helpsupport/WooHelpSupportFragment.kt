91927c657943b4ae9af5730370cdaf194339d0f6
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-07-19 13:17:49 +0300

    Refactor: Replace woo help support fragment synthetics with binding

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/helpsupport/WooHelpSupportFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/helpsupport/WooHelpSupportFragment.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_woo_help_support.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.example.R
+import org.wordpress.android.fluxc.example.databinding.FragmentWooHelpSupportBinding
 import org.wordpress.android.fluxc.example.prependToLog
 import org.wordpress.android.fluxc.example.ui.StoreSelectingFragment
 import org.wordpress.android.fluxc.store.WooCommerceStore
@@ -22,24 +21,29 @@ class WooHelpSupportFragment : StoreSelectingFragment() {
 
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_woo_help_support, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentWooHelpSupportBinding.inflate(inflater, container, false).root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fetch_ssr.setOnClickListener {
-            selectedSite?.let { site ->
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooCommerceStore.fetchSSR(site)
-                    }
+        with(FragmentWooHelpSupportBinding.bind(view)) {
+            fetchSsr.setOnClickListener {
+                selectedSite?.let { site ->
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooCommerceStore.fetchSSR(site)
+                        }
 
-                    result.error?.let {
-                        prependToLog("${it.type}: ${it.message}")
-                        return@launch
-                    }
-                    result.model?.let {
-                        prependToLog("Fetched data: $it")
+                        result.error?.let {
+                            prependToLog("${it.type}: ${it.message}")
+                            return@launch
+                        }
+                        result.model?.let {
+                            prependToLog("Fetched data: $it")
+                        }
                     }
                 }
             }
