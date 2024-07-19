d3c3bd840e811510dbcf95ce02a58e448183a4a8
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-07-19 16:20:55 +0300

    Refactor: Replace woo tax fragment synthetics with binding

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/taxes/WooTaxFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/taxes/WooTaxFragment.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.fragment_woo_taxes.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.Dispatcher
-import org.wordpress.android.fluxc.example.R
+import org.wordpress.android.fluxc.example.databinding.FragmentWooTaxesBinding
 import org.wordpress.android.fluxc.example.prependToLog
 import org.wordpress.android.fluxc.example.ui.StoreSelectingFragment
 import org.wordpress.android.fluxc.store.WCTaxStore
@@ -22,30 +21,34 @@ class WooTaxFragment : StoreSelectingFragment() {
     @Inject internal lateinit var wcTaxStore: WCTaxStore
     @Inject internal lateinit var wooCommerceStore: WooCommerceStore
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_woo_taxes, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentWooTaxesBinding.inflate(inflater, container, false).root
 
     @Suppress("TooGenericExceptionCaught")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        fetch_tax_class.setOnClickListener {
-            selectedSite?.let { site ->
-                prependToLog("Submitting request to fetch tax classes for site ${site.id}")
-                lifecycleScope.launch(Dispatchers.IO) {
-                    try {
-                        val response = wcTaxStore.fetchTaxClassList(site)
-                        withContext(Dispatchers.Main) {
-                            response.error?.let {
-                                prependToLog("${it.type}: ${it.message}")
+        with(FragmentWooTaxesBinding.bind(view)) {
+            fetchTaxClass.setOnClickListener {
+                selectedSite?.let { site ->
+                    prependToLog("Submitting request to fetch tax classes for site ${site.id}")
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        try {
+                            val response = wcTaxStore.fetchTaxClassList(site)
+                            withContext(Dispatchers.Main) {
+                                response.error?.let {
+                                    prependToLog("${it.type}: ${it.message}")
+                                }
+                                response.model?.let {
+                                    prependToLog("Site has ${it.size} tax classes")
+                                }
                             }
-                            response.model?.let {
-                                prependToLog("Site has ${it.size} tax classes")
+                        } catch (e: Exception) {
+                            withContext(Dispatchers.Main) {
+                                prependToLog("Error: ${e.message}")
                             }
-                        }
-                    } catch (e: Exception) {
-                        withContext(Dispatchers.Main) {
-                            prependToLog("Error: ${e.message}")
                         }
                     }
                 }
