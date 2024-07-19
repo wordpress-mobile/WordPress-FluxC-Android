247fdb8cdb6109895ef70e636948a92b4b3c82bf
Petros Paraskevopoulos <petros.paraskevopoulos@automattic.com>
2024-07-19 16:48:32 +0300

    Refactor: Replace woo commerce fragment synthetics with binding

--- example/src/main/java/org/wordpress/android/fluxc/example/ui/WooCommerceFragment.kt
+++ example/src/main/java/org/wordpress/android/fluxc/example/ui/WooCommerceFragment.kt
@@ -5,13 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
-import kotlinx.android.synthetic.main.fragment_woocommerce.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.wordpress.android.fluxc.example.LogUtils
-import org.wordpress.android.fluxc.example.R
+import org.wordpress.android.fluxc.example.databinding.FragmentWoocommerceBinding
 import org.wordpress.android.fluxc.example.prependToLog
 import org.wordpress.android.fluxc.example.replaceFragment
 import org.wordpress.android.fluxc.example.ui.coupons.WooCouponsFragment
@@ -50,8 +49,7 @@ class WooCommerceFragment : StoreSelectingFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =
-        inflater.inflate(R.layout.fragment_woocommerce, container, false)
+    ): View = FragmentWoocommerceBinding.inflate(inflater, container, false).root
 
     @Suppress("LongMethod", "ComplexMethod")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -62,216 +60,218 @@ class WooCommerceFragment : StoreSelectingFragment() {
             wooCommerceStore.fetchWooCommerceSites()
         }
 
-        log_sites.setOnClickListener {
-            coroutineScope.launch {
-                prependToLog("Fetching WooCommerce sites")
-                val result = wooCommerceStore.fetchWooCommerceSites()
-                if (result.isError) {
-                    prependToLog("Fetching WooCommerce sites failed, error message: ${result.error.message}")
-                } else {
-                    for (site in result.model!!) {
-                        prependToLog(site.name + ": " + if (site.isWpComStore) "WP.com store" else "Self-hosted store")
-                        AppLog.i(T.API, LogUtils.toString(site))
+        with(FragmentWoocommerceBinding.bind(view)) {
+            logSites.setOnClickListener {
+                coroutineScope.launch {
+                    prependToLog("Fetching WooCommerce sites")
+                    val result = wooCommerceStore.fetchWooCommerceSites()
+                    if (result.isError) {
+                        prependToLog("Fetching WooCommerce sites failed, error message: ${result.error.message}")
+                    } else {
+                        for (site in result.model!!) {
+                            prependToLog(site.name + ": " + if (site.isWpComStore) "WP.com store" else "Self-hosted store")
+                            AppLog.i(T.API, LogUtils.toString(site))
+                        }
                     }
                 }
             }
-        }
 
-        log_woo_api_versions.setOnClickListener {
-            selectedSite?.let { selectedSite ->
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooCommerceStore.fetchSupportedApiVersion(selectedSite)
-                    }
-                    result.error?.let {
-                        prependToLog("Error in onApiVersionFetched: ${it.type} - ${it.message}")
-                    }
-                    result.model?.let {
-                        val formattedVersion = it.apiVersion?.substringAfterLast("/")
-                        prependToLog("Max Woo version for ${selectedSite.name}: $formattedVersion")
-                    }
-                }
-            }
-        }
-
-        fetch_settings.setOnClickListener {
-            selectedSite?.let { site ->
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooCommerceStore.fetchSiteGeneralSettings(site)
-                    }
-                    result.error?.let {
-                        prependToLog("Error fetching settings: ${it.type} - ${it.message}")
-                    }
-                    result.model?.let {
-                        prependToLog(
-                            "Updated site settings for ${site.name}:\n" +
-                                it.toString()
-                        )
-                    }
-                }
-            } ?: showNoWCSitesToast()
-        }
-
-        fetch_product_settings.setOnClickListener {
-            selectedSite?.let { site ->
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooCommerceStore.fetchSiteProductSettings(site)
-                    }
-                    result.error?.let {
-                        prependToLog("Error in onWCProductSettingsChanged: ${it.type} - ${it.message}")
-                    }
-                    result.model?.let {
-                        prependToLog(
-                            "Updated product settings for ${site.name}: " +
-                                "weight unit = ${it.weightUnit}, dimension unit = ${it.dimensionUnit}"
-                        )
-                    } ?: prependToLog("Error getting product settings from db")
-                }
-            } ?: showNoWCSitesToast()
-        }
-
-        get_user_role.setOnClickListener {
-            selectedSite?.let { site ->
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooUserStore.fetchUserRole(site)
-                    }
-                    result.error?.let {
-                        prependToLog("${it.type}: ${it.message}")
-                    }
-                    result.model?.let {
-                        prependToLog("Current user is: ${it.roles}")
+            logWooApiVersions.setOnClickListener {
+                selectedSite?.let { selectedSite ->
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooCommerceStore.fetchSupportedApiVersion(selectedSite)
+                        }
+                        result.error?.let {
+                            prependToLog("Error in onApiVersionFetched: ${it.type} - ${it.message}")
+                        }
+                        result.model?.let {
+                            val formattedVersion = it.apiVersion?.substringAfterLast("/")
+                            prependToLog("Max Woo version for ${selectedSite.name}: $formattedVersion")
+                        }
                     }
                 }
             }
-        }
 
-        orders.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooOrdersFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        products.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooProductsFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        stats_revenue.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooRevenueStatsFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        refunds.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooRefundsFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        gateways.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooGatewaysFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        taxes.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooTaxFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        shipping_labels.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooShippingLabelFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        leaderboards.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooLeaderboardsFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        countries.setOnClickListener {
-            launchCountriesRequest()
-        }
-
-        attributes.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooProductAttributeFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        customers.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooCustomersFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        store_creation.setOnClickListener {
-            replaceFragment(WooStoreCreationFragment())
-        }
-
-        coupons.setOnClickListener {
-            replaceFragment(WooCouponsFragment())
-        }
-
-        help_support.setOnClickListener {
-            selectedSite?.let {
-                replaceFragment(WooHelpSupportFragment())
-            } ?: showNoWCSitesToast()
-        }
-
-        store_onboarding.setOnClickListener {
-            replaceFragment(WooOnboardingFragment())
-        }
-
-        woo_admin.setOnClickListener {
-            replaceFragment(WooAdminFragment())
-        }
-
-        woo_google_ads_status.setOnClickListener {
-            selectedSite?.let { selectedSite ->
-                prependToLog("Fetching Google Ads connection status...")
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooGoogleStore.isGoogleAdsAccountConnected(selectedSite)
-                    }
-                    result.error?.let {
-                        prependToLog("Error in fetchGoogleAdsConnectionStatus: ${it.type} - ${it.message}")
-                    }
-                    result.model?.let {
-                        prependToLog("Google Ads connection status: $it")
-                    } ?: prependToLog("Couldn't fetch Google Ads connection status.")
-                }
-            } ?: showNoWCSitesToast()
-        }
-
-        woo_google_ads_campaigns.setOnClickListener {
-            selectedSite?.let { selectedSite ->
-                prependToLog("Fetching Google Ads campaigns...")
-                coroutineScope.launch {
-                    val result = withContext(Dispatchers.Default) {
-                        wooGoogleStore.fetchGoogleAdsCampaigns(selectedSite)
-                    }
-                    result.error?.let {
-                        prependToLog("Error in fetchGoogleAdsCampaigns: ${it.type} - ${it.message}")
-                    }
-                    result.model?.let { campaigns ->
-                        prependToLog("Fetched ${campaigns.size} Google Ads campaigns")
-                        campaigns.forEach { campaign ->
-                            prependToLog("Campaign ID: ${campaign.id}, " +
-                                "Name: ${campaign.name}, " +
-                                "Status: ${campaign.status}"
+            fetchSettings.setOnClickListener {
+                selectedSite?.let { site ->
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooCommerceStore.fetchSiteGeneralSettings(site)
+                        }
+                        result.error?.let {
+                            prependToLog("Error fetching settings: ${it.type} - ${it.message}")
+                        }
+                        result.model?.let {
+                            prependToLog(
+                                "Updated site settings for ${site.name}:\n" +
+                                    it.toString()
                             )
                         }
-                    } ?: prependToLog("Couldn't fetch Google Ads campaigns.")
+                    }
+                } ?: showNoWCSitesToast()
+            }
+
+            fetchProductSettings.setOnClickListener {
+                selectedSite?.let { site ->
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooCommerceStore.fetchSiteProductSettings(site)
+                        }
+                        result.error?.let {
+                            prependToLog("Error in onWCProductSettingsChanged: ${it.type} - ${it.message}")
+                        }
+                        result.model?.let {
+                            prependToLog(
+                                "Updated product settings for ${site.name}: " +
+                                    "weight unit = ${it.weightUnit}, dimension unit = ${it.dimensionUnit}"
+                            )
+                        } ?: prependToLog("Error getting product settings from db")
+                    }
+                } ?: showNoWCSitesToast()
+            }
+
+            getUserRole.setOnClickListener {
+                selectedSite?.let { site ->
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooUserStore.fetchUserRole(site)
+                        }
+                        result.error?.let {
+                            prependToLog("${it.type}: ${it.message}")
+                        }
+                        result.model?.let {
+                            prependToLog("Current user is: ${it.roles}")
+                        }
+                    }
                 }
-            } ?: showNoWCSitesToast()
+            }
+
+            orders.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooOrdersFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            products.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooProductsFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            statsRevenue.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooRevenueStatsFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            refunds.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooRefundsFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            gateways.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooGatewaysFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            taxes.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooTaxFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            shippingLabels.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooShippingLabelFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            leaderboards.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooLeaderboardsFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            countries.setOnClickListener {
+                launchCountriesRequest()
+            }
+
+            attributes.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooProductAttributeFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            customers.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooCustomersFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            storeCreation.setOnClickListener {
+                replaceFragment(WooStoreCreationFragment())
+            }
+
+            coupons.setOnClickListener {
+                replaceFragment(WooCouponsFragment())
+            }
+
+            helpSupport.setOnClickListener {
+                selectedSite?.let {
+                    replaceFragment(WooHelpSupportFragment())
+                } ?: showNoWCSitesToast()
+            }
+
+            storeOnboarding.setOnClickListener {
+                replaceFragment(WooOnboardingFragment())
+            }
+
+            wooAdmin.setOnClickListener {
+                replaceFragment(WooAdminFragment())
+            }
+
+            wooGoogleAdsStatus.setOnClickListener {
+                selectedSite?.let { selectedSite ->
+                    prependToLog("Fetching Google Ads connection status...")
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooGoogleStore.isGoogleAdsAccountConnected(selectedSite)
+                        }
+                        result.error?.let {
+                            prependToLog("Error in fetchGoogleAdsConnectionStatus: ${it.type} - ${it.message}")
+                        }
+                        result.model?.let {
+                            prependToLog("Google Ads connection status: $it")
+                        } ?: prependToLog("Couldn't fetch Google Ads connection status.")
+                    }
+                } ?: showNoWCSitesToast()
+            }
+
+            wooGoogleAdsCampaigns.setOnClickListener {
+                selectedSite?.let { selectedSite ->
+                    prependToLog("Fetching Google Ads campaigns...")
+                    coroutineScope.launch {
+                        val result = withContext(Dispatchers.Default) {
+                            wooGoogleStore.fetchGoogleAdsCampaigns(selectedSite)
+                        }
+                        result.error?.let {
+                            prependToLog("Error in fetchGoogleAdsCampaigns: ${it.type} - ${it.message}")
+                        }
+                        result.model?.let { campaigns ->
+                            prependToLog("Fetched ${campaigns.size} Google Ads campaigns")
+                            campaigns.forEach { campaign ->
+                                prependToLog("Campaign ID: ${campaign.id}, " +
+                                    "Name: ${campaign.name}, " +
+                                    "Status: ${campaign.status}"
+                                )
+                            }
+                        } ?: prependToLog("Couldn't fetch Google Ads campaigns.")
+                    }
+                } ?: showNoWCSitesToast()
+            }
         }
     }
 
