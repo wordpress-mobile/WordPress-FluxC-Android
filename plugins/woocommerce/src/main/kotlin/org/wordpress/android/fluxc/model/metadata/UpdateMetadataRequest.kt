202e100f2ec03e95f9c145b3092945e99c54dcd0
Hicham Boushaba <hicham.boushaba@gmail.com>
2024-10-08 15:15:06 +0100

    Move logic of JSON conversion to make it reusable

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/metadata/UpdateMetadataRequest.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/metadata/UpdateMetadataRequest.kt
@@ -1,5 +1,9 @@
 package org.wordpress.android.fluxc.model.metadata
 
+import com.google.gson.JsonArray
+import com.google.gson.JsonNull
+import com.google.gson.JsonObject
+
 data class UpdateMetadataRequest(
     val parentItemId: Long,
     val parentItemType: MetaDataParentItemType,
@@ -12,6 +16,28 @@ data class UpdateMetadataRequest(
         // we require that all inserted metadata have an ID of 0.
         require(insertedMetadata.all { it.id == 0L }) {
             "Inserted metadata must have an ID of 0"
+        }
+    }
+
+    internal fun toJsonArray() = JsonArray().apply {
+        insertedMetadata.forEach {
+            add(
+                JsonObject().apply {
+                    addProperty(WCMetaData.KEY, it.key)
+                    add(WCMetaData.VALUE, it.value.jsonValue)
+                }
+            )
+        }
+        updatedMetadata.forEach {
+            add(it.toJson())
+        }
+        deletedMetadataIds.forEach {
+            add(
+                JsonObject().apply {
+                    addProperty(WCMetaData.ID, it)
+                    add(WCMetaData.VALUE, JsonNull.INSTANCE)
+                }
+            )
         }
     }
 }
