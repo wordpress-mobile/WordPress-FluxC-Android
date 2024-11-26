7b6d579c09ada3aec910fcce61e4a4206287695b
AnirudhBhat <abhat38@gmail.com>
2024-11-26 13:47:35 +0530

    Fix checkstyle error

--- plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/customer/WCCustomerMapper.kt
+++ plugins/woocommerce/src/main/kotlin/org/wordpress/android/fluxc/model/customer/WCCustomerMapper.kt
@@ -122,6 +122,8 @@ class WCCustomerMapper @Inject constructor() {
         this?.trim()?.replace("\\s+".toRegex(), " ")?.substringBefore(" ") ?: ""
 
     private fun String?.lastNameFromName(): String =
-        this?.trim()?.replace("\\s+".toRegex(), " ")?.substringAfter(" ", "") ?: ""
+        this?.trim()?.replace("\\s+".toRegex(), " ")?.substringAfter(
+            " ", ""
+        ) ?: ""
 
 }
