2455c15f0dc8f3b9c5bfa084d4cd8a21b2d8d31c
AnirudhBhat <abhat38@gmail.com>
2024-11-26 13:35:45 +0530

    Add test to verify multiple in between space in customer name will still give us proper last name

--- example/src/test/java/org/wordpress/android/fluxc/model/customer/WCCustomerMapperTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/model/customer/WCCustomerMapperTest.kt
@@ -438,4 +438,19 @@ class WCCustomerMapperTest {
         // then
         assertEquals("firstname", result.firstName)
     }
+
+    @Test
+    fun `given customer name has multiple in between space, then last name returns proper string`() {
+        // given
+        val siteId = 23
+        val site = SiteModel().apply { id = siteId }
+
+        val customerDTO = CustomerFromAnalyticsDTO(name = "  firstname   and  a   very  long last name")
+
+        // when
+        val result = mapper.mapToModel(site, customerDTO)
+
+        // then
+        assertEquals("and a very long last name", result.lastName)
+    }
 }
