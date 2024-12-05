57339f291b150a7ca9d8f144653db6922aa6fb09
AnirudhBhat <abhat38@gmail.com>
2024-12-05 10:06:15 +0530

    Add test to verify that Simple type is returned

--- example/src/test/java/org/wordpress/android/fluxc/wc/product/WCProductStoreTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/product/WCProductStoreTest.kt
@@ -27,12 +27,12 @@ import org.wordpress.android.fluxc.model.AccountModel
 import org.wordpress.android.fluxc.model.LocalOrRemoteId.RemoteId
 import org.wordpress.android.fluxc.model.ProductWithMetaData
 import org.wordpress.android.fluxc.model.SiteModel
-import org.wordpress.android.fluxc.model.metadata.WCMetaData
 import org.wordpress.android.fluxc.model.WCProductCategoryModel
 import org.wordpress.android.fluxc.model.WCProductModel
 import org.wordpress.android.fluxc.model.WCProductReviewModel
 import org.wordpress.android.fluxc.model.WCProductVariationModel
 import org.wordpress.android.fluxc.model.WCProductVariationModel.ProductVariantOption
+import org.wordpress.android.fluxc.model.metadata.WCMetaData
 import org.wordpress.android.fluxc.network.BaseRequest.GenericErrorType.NETWORK_ERROR
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooError
 import org.wordpress.android.fluxc.network.rest.wpcom.wc.WooErrorType.GENERIC_ERROR
@@ -829,5 +829,10 @@ class WCProductStoreTest {
         assertThat(storedProduct).isNotNull
         assertThat(storedProduct?.product).isEqualTo(product)
         assertThat(storedProduct?.metaData).isEqualTo(metadata)
+    }
+
+    @Test
+    fun `given include_type simple, then return type Simple` () {
+        assertThat(WCProductStore.IncludeType.fromValue("simple")).isEqualTo(WCProductStore.IncludeType.Simple)
     }
 }
