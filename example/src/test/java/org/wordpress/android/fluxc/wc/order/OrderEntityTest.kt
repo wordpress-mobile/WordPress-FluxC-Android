0a0d3ebed6418d18848c81828d2d799c83e3b055
Hafiz Rahman <hfzrhmn@gmail.com>
2024-12-12 16:06:47 +0700

    Add unit test for batch order response's deserializer.

--- example/src/test/java/org/wordpress/android/fluxc/wc/order/OrderEntityTest.kt
+++ example/src/test/java/org/wordpress/android/fluxc/wc/order/OrderEntityTest.kt
@@ -1,10 +1,12 @@
 package org.wordpress.android.fluxc.wc.order
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
 import org.junit.Test
 import org.wordpress.android.fluxc.UnitTestUtils
 import org.wordpress.android.fluxc.model.order.ShippingLine
+import org.wordpress.android.fluxc.network.rest.wpcom.wc.order.BatchOrderApiResponse
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
@@ -142,5 +144,32 @@ class OrderEntityTest {
 
         assertEquals("Flat Rate Shipping", shippingLinesList[0].methodTitle)
         assertEquals("Local Pickup Shipping", shippingLinesList[1].methodTitle)
+    }
+
+    @Test
+    fun testDeserializeBatchOrderResponse() {
+        val testGson = GsonBuilder()
+            .registerTypeAdapter(
+                BatchOrderApiResponse.OrderResponse::class.java,
+                BatchOrderApiResponse.OrderResponseDeserializer())
+            .create()
+
+        val batchOrderJson = UnitTestUtils.getStringFromResourceFile(
+            this.javaClass, "wc/orders-batch.json"
+        )
+
+        val response = testGson.fromJson(batchOrderJson, BatchOrderApiResponse::class.java)
+        val orders = response.update
+
+        assertEquals(2, orders.size)
+
+        val firstOrder = orders[0] as BatchOrderApiResponse.OrderResponse.Success
+        assertEquals(1032L, firstOrder.order.id)
+        assertEquals("224.00", firstOrder.order.total)
+
+        val secondOrder = orders[1] as BatchOrderApiResponse.OrderResponse.Error
+        assertEquals(525L, secondOrder.id)
+        assertEquals("woocommerce_rest_shop_order_invalid_id", secondOrder.error.code)
+        assertEquals(400, secondOrder.error.data.status)
     }
 }
